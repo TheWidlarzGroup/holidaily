f18880b59751a4151535e75938888179c73f09e9
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-03-25 11:08:29 +0100

    Create base SliderContent component. Pass title and text from slider screen

--- src/screens/slider/Slider.tsx
+++ src/screens/slider/Slider.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Text, StyleSheet } from 'react-native'
+import { StyleSheet, ScrollView } from 'react-native'
+
+import SliderContent from '../../components/SliderContent'
+
+const sliderContent = [
+  {
+    title: 'Welcome to Holidaily!',
+    text: 'All your team days-off in one place.',
+  },
+  {
+    title: 'Real-time vacation checking',
+    text: 'Check how many leaves have left.',
+  },
+  {
+    title: 'Request time off',
+    text: 'Are you planning vacations or some personal time? Simply request it.',
+  },
+  {
+    title: 'Get notified',
+    text: 'You’ll get notifications once the vacation is approved or rejected.',
+  },
+]
 
 const styles = StyleSheet.create({
   container: {
@@ -11,7 +32,12 @@ const styles = StyleSheet.create({
 
 const Slider = () => (
   <SafeAreaView style={styles.container}>
-    <Text>Slider Screen</Text>
+    <ScrollView>
+      <SliderContent title={sliderContent[0].title} text={sliderContent[0].text} />
+      {/* <SliderContent title={sliderContent[1].title} text={sliderContent[1].text} />
+      <SliderContent title={sliderContent[2].title} text={sliderContent[2].text} />
+      <SliderContent title={sliderContent[3].title} text={sliderContent[3].text} /> */}
+    </ScrollView>
   </SafeAreaView>
 )
 
