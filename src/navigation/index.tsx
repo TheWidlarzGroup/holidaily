f102290a382d61ea58659380812ed0acde4084b5
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-03-25 10:35:11 +0100

    Create slider screen, set to initial route

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { AppRoutes } from './types'
+import Slider from '../screens/slider/Slider'
 import Home from '../screens/home/Home'
 import TestScreen from '../screens/home/TestScreen'
 
@@ -9,7 +10,8 @@ const AppStack = createStackNavigator<AppRoutes>()
 
 const AppStackContainer = () => (
   <NavigationContainer>
-    <AppStack.Navigator headerMode="none" initialRouteName="Home">
+    <AppStack.Navigator headerMode="none" initialRouteName="Slider">
+      <AppStack.Screen name="Slider" component={Slider} />
       <AppStack.Screen name="Home" component={Home} />
       <AppStack.Screen name="TestScreen" component={TestScreen} />
     </AppStack.Navigator>
--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,6 +1,7 @@
 import { StackNavigationProp } from '@react-navigation/stack'
 
 export type AppRoutes = {
+  Slider: undefined
   Home: undefined
   TestScreen: undefined
 }
