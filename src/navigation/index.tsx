845c77c61dbec657d8bd79d78c17f316d064cf85
iceu-bb <bart.bajda@thewidlarzgroup.com>
2021-03-29 09:24:59 +0200

    Add Restyle Theming

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { AppRoutes } from './types'
-import Home from '../screens/home/Home'
-import TestScreen from '../screens/home/TestScreen'
+import { Home } from '../screens/home/Home'
+import { TestScreen } from '../screens/home/TestScreen'
 
 const AppStack = createStackNavigator<AppRoutes>()
 
---
+++ src/utils/theme/colors.ts
@@ -0,0 +1,16 @@
+export const palette = {
+  orangeLight: '#FFB051',
+  orangeMedium: '#FFB051',
+  orangeDark: '#E86E1F',
+  black: '#000',
+  white: '#fff',
+}
+
+export const colors = {
+  primary: palette.orangeLight,
+  black: palette.black,
+  white: palette.white,
+
+  //switch Theme
+  mainBackground: palette.orangeMedium,
+}
