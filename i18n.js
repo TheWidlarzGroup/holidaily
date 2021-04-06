41066d20cd3344e3c1d699765cc5528e285e9ed3
mateki0 <mateki0@interia.pl>
2021-04-06 17:11:42 +0200

    translations moved to separate file, language detection added

--- i18n.js
+++ i18n.js
@@ -1,28 +1,24 @@
-import i18n from 'i18next'
+import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import { NativeModules, Platform } from 'react-native'
+
+const locale =
+  Platform.OS === 'ios'
+    ? NativeModules.SetttingManager.settings.AppleLocale
+    : NativeModules.I18nManager.localeIdentifier
+
+const sliderTranslation = require('./translations/slider.json')
 
 const resources = {
   pl: {
-    slider: {
-      'Welcome to React': 'Witaj w react i react-i18next',
-      'Welcome to Holidaily!': 'Witaj w Holidaily!',
-      'All your team days-off in one place.': 'Wszystkie urlopy twojego zespołu w jednym miejscu.',
-      'Real-time vacation checking': 'Sprawdzanie wakacji w czasie rzeczywistym',
-      'Check how many leaves have left.': 'Sprawdź jak dużo xxx zostało.',
-      'Request time off': 'Poproś o czas wolny',
-      'Are you planning vacations or some personal time? Simply request it.':
-        'Planujesz wakacje lub czas dla siebie ? Po prostu o nie poproś.',
-      'Get notified': 'Otrzymuj powiadomienia',
-      'You’ll get notifications once the vacation is approved or rejected.':
-        'Dostaniesz powiadomienie kiedy wniosek zostanie zaakceptowany lub odrzucony',
-      Next: 'Dalej',
-    },
+    slider: sliderTranslation,
   },
 }
 
-i18n.use(initReactI18next).init({
+i18next.use(initReactI18next).init({
   resources,
-  lng: 'pl',
+  lng: locale.slice(0, 2),
+  fallbackLng: ['en', 'pl'],
   keySeparator: false,
 
   interpolation: {
