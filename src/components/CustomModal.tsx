c920feafce201ea2711e3a00b977d1709c2d6311
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2021-04-12 20:18:10 +0200

    custom modal implemented  (#46)
    * custom modal implemented with first dialog box
    
    * dialog box name changed
    
    * added second dialog box
    
    * 2nd box padding from theme
    
    * dialog box names changed again
    
    * transparent button variant and console log removed
    
    * backdrop background color changed
    
    * text moved to translation files, square dimension moved out
    
    * i18 typo
    
    * created separate component for register modal
    
    * names and modal prop changed
    
    * func renamed, modal moved to screens directory

---
+++ src/components/CustomModal.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import { StyleProp, ViewStyle } from 'react-native'
+import Modal, { ModalProps } from 'react-native-modal'
+import { Box } from '../utils/theme/index'
+
+type CustomModalProps = {
+  style: StyleProp<ViewStyle>
+}
+
+export const CustomModal: FC<CustomModalProps & Partial<ModalProps>> = ({
+  style,
+  children,
+  ...props
+}) => (
+  <Box>
+    <Modal {...props}>
+      <Box style={style}>{children}</Box>
+    </Modal>
+  </Box>
+)
