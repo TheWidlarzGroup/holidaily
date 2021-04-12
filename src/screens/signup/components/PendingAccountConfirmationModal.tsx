3fc2b8252e9d3d4795e3760ee85527b66a67ab63
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2021-04-12 20:18:10 +0200

    custom modal implemented  (#46)
    * custom modal implemented with first dialog box
    
    * dialog box name changed
    
    * added second dialog box
    
    * 2nd box padding from theme
    
    * dialog box names changed again
    
    * transparent button variant and console log removed
    
    * backdrop background color changed
    
    * text moved to translation files, square dimension moved out
    
    * i18 typo
    
    * created separate component for register modal
    
    * names and modal prop changed
    
    * func renamed, modal moved to screens directory

---
+++ src/screens/signup/components/PendingAccountConfirmationModal.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react'
+import { StyleSheet } from 'react-native'
+import { ModalProps } from 'react-native-modal'
+
+import { CustomModal } from '../../../components/CustomModal'
+import { FirstRegisterDialogBox } from '../../../components/FirstRegisterDialogBox'
+import { theme } from '../../../utils/theme/index'
+import { colors } from '../../../utils/theme/colors'
+
+type PendingAccountConfModalProps = Pick<ModalProps, 'isVisible'> & { onClose: () => void }
+
+export const PendingAccountConfirmationModal: FC<PendingAccountConfModalProps> = ({
+  isVisible,
+  onClose,
+}) => (
+  <CustomModal
+    isVisible={isVisible}
+    onBackButtonPress={onClose}
+    onBackdropPress={onClose}
+    backdropColor={colors.white}
+    animationInTiming={600}
+    animationOutTiming={400}
+    backdropTransitionInTiming={600}
+    backdropTransitionOutTiming={400}
+    backdropOpacity={0.8}
+    style={styles.modal}
+    hideModalContentWhileAnimating>
+    {/* TODO matthew: When register and mutations will be ready
+        !isConfirmed ? <FirstRegisterDialogBox /> : <SecondRegisterDialogBox /> */}
+    <FirstRegisterDialogBox />
+  </CustomModal>
+)
+
+const styles = StyleSheet.create({
+  modal: {
+    marginHorizontal: theme.spacing.l,
+  },
+})
