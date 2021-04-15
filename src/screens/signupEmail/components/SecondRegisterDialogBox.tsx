53b1033ccfa98158a57eba45df162378c21e1634
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2021-04-15 21:06:51 +0200

    Account confirmation (#64)
    * initialized confirmation
    
    * confirmed account screen added
    
    * changed confirm hook, handle confirm errors
    
    * console logs deleted
    
    * confirmationAccount func set as async
    
    * register dialog box navigation path changed
    
    * moved show modal to modal itself
    
    * try catch added

--- src/screens/signupEmail/components/SecondRegisterDialogBox.tsx
+++ src/screens/signupEmail/components/SecondRegisterDialogBox.tsx
@@ -1,43 +1,55 @@
-import React, { FC } from 'react'
-import { StyleSheet } from 'react-native'
+import React, { FC, useCallback } from 'react'
+import { TouchableOpacity } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import { Box, Text, theme } from 'utils/theme/index'
 import { CustomButton } from 'components/CustomButton'
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigationType } from 'navigation/types'
 
-export const SecondRegisterDialogBox: FC = () => {
+type SecondRegisterDialogBoxTypes = {
+  hideModal: () => void
+}
+
+export const SecondRegisterDialogBox: FC<SecondRegisterDialogBoxTypes> = ({ hideModal }) => {
   const { t } = useTranslation('modal')
+
+  const navigation = useNavigation<AppNavigationType<'ConfirmedAccount'>>()
+
+  const navigateToLogin = useCallback(() => {
+    hideModal()
+    navigation.navigate('Login')
+  }, [navigation])
 
   return (
     <Box backgroundColor="primary" alignItems="center" padding="lplus" borderRadius="mplus">
       <Text variant="boldBlack18">{t('dialogBox2Title')}</Text>
       <Box
         backgroundColor="tertiary"
-        style={styles.orangeBox}
         marginVertical="xxl"
         borderRadius="xm"
+        width={59}
+        height={59}
       />
       <Text variant="body1">{t('dialogBox2SubTitle')}</Text>
       <Box marginTop="l" marginBottom="m">
-        <CustomButton
-          label={t('dialog2FirstButton')}
-          variant="blackBgButton"
-          paddingVertical={theme.spacing.xs}
-        />
+        <TouchableOpacity activeOpacity={1} onPress={navigateToLogin}>
+          <CustomButton
+            label={t('dialog2FirstButton')}
+            variant="blackBgButton"
+            paddingVertical={theme.spacing.xs}
+          />
+        </TouchableOpacity>
       </Box>
       <Box paddingBottom="m">
-        <CustomButton
-          label={t('dialog2SecondButton')}
-          variant="secondary"
-          paddingVertical={theme.spacing.xs}
-        />
+        <TouchableOpacity activeOpacity={1} onPress={navigateToLogin}>
+          <CustomButton
+            label={t('dialog2SecondButton')}
+            variant="secondary"
+            paddingVertical={theme.spacing.xs}
+          />
+        </TouchableOpacity>
       </Box>
     </Box>
   )
 }
 
-const styles = StyleSheet.create({
-  orangeBox: {
-    height: 59,
-    width: 59,
-  },
-})
