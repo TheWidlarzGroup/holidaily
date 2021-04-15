df58d7538b4da1b133a65471388701df4e5d4c57
mateki0 <mateki0@interia.pl>
2021-04-15 14:59:31 +0200

    console logs deleted

--- src/screens/signupEmail/components/PendingAccountConfirmationModal.tsx
+++ src/screens/signupEmail/components/PendingAccountConfirmationModal.tsx
@@ -17,30 +17,23 @@ export const PendingAccountConfirmationModal: FC<PendingAccountConfModalProps> =
   isVisible,
   hideModal,
   isConfirmed,
-}) => {
-  console.log(isConfirmed)
-  console.log(isVisible)
-  return (
-    <CustomModal
-      isVisible={isVisible}
-      onBackButtonPress={hideModal}
-      onBackdropPress={hideModal}
-      backdropColor={colors.white}
-      animationInTiming={500}
-      animationOutTiming={500}
-      backdropTransitionInTiming={500}
-      backdropTransitionOutTiming={500}
-      backdropOpacity={0.8}
-      style={styles.modal}
-      hideModalContentWhileAnimating>
-      {!isConfirmed ? (
-        <FirstRegisterDialogBox />
-      ) : (
-        <SecondRegisterDialogBox hideModal={hideModal} />
-      )}
-    </CustomModal>
-  )
-}
+}) => (
+  <CustomModal
+    isVisible={isVisible}
+    onBackButtonPress={hideModal}
+    onBackdropPress={hideModal}
+    backdropColor={colors.white}
+    animationInTiming={500}
+    animationOutTiming={500}
+    backdropTransitionInTiming={500}
+    backdropTransitionOutTiming={500}
+    backdropOpacity={0.8}
+    style={styles.modal}
+    hideModalContentWhileAnimating>
+    {!isConfirmed ? <FirstRegisterDialogBox /> : <SecondRegisterDialogBox hideModal={hideModal} />}
+  </CustomModal>
+)
+
 const styles = StyleSheet.create({
   modal: {
     marginHorizontal: theme.spacing.l,
