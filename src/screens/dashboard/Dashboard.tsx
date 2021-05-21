18890940734317063bbaab1ce7e17cb34814cbe3
mateki0 <mateki0@interia.pl>
2021-05-21 18:08:13 +0200

    unused useeffect deleted

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 import { Box, Text } from 'utils/theme'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 
