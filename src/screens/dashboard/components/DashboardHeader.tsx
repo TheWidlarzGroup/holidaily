dd8789e2bfb44b5efd8ed8d11f8ea1ac272b6d5d
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-06-05 00:09:59 +0200

    Dashbord teams list created

--- src/screens/dashboard/components/DashboardHeader.tsx
+++ src/screens/dashboard/components/DashboardHeader.tsx
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: theme.spacing.m,
   },
   userContainer: {
     flexDirection: 'row',
