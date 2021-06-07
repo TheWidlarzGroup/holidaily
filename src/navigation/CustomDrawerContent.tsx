f27578e39394bf94cc13c695d5919527949373df
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-06-07 19:47:08 +0200

    Create drawer header with macked data

--- src/navigation/CustomDrawerContent.tsx
+++ src/navigation/CustomDrawerContent.tsx
@@ -1,19 +1,14 @@
-import React, { FC } from 'react'
-import { StyleSheet } from 'react-native'
-import {
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerContentComponentProps,
-} from '@react-navigation/drawer'
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+import { DrawerContentScrollView, DrawerContentComponentProps } from '@react-navigation/drawer'
 
-import { CustomButton } from 'components/CustomButton'
-import { Box } from 'utils/theme'
+import { Box, Text, theme } from 'utils/theme'
 import { useUserContext } from 'hooks/useUserContext'
 import { emptyUser } from 'contexts/UserProvider'
-import { useTranslation } from 'react-i18next'
+import UserIconPlaceholder from 'assets/icons/icon-profile.svg'
+import { MOCK_DATA } from 'navigation/MockData'
 
-export const CustomDrawerContent: FC<DrawerContentComponentProps> = (props) => {
-  const { t } = useTranslation('navigation')
+export const CustomDrawerContent = (props: DrawerContentComponentProps) => {
   const { updateUser } = useUserContext()
 
   const handleLogout = () => {
@@ -23,11 +18,21 @@ export const CustomDrawerContent: FC<DrawerContentComponentProps> = (props) => {
   return (
     <DrawerContentScrollView {...props} contentContainerStyle={styles.container}>
       <Box>
-        <DrawerItemList {...props} />
-      </Box>
+        <Box margin="m">
+          {MOCK_DATA.profilePicture ? (
+            <Image
+              source={{ uri: MOCK_DATA.profilePicture }}
+              style={{ width: 44, height: 44, borderRadius: 22 }}
+            />
+          ) : (
+            <UserIconPlaceholder width={44} height={44} />
+          )}
 
-      <Box marginBottom="l">
-        <CustomButton label={t('logout')} variant="primary" onPress={handleLogout} />
+          <Text marginTop="m" style={styles.header}>
+            {MOCK_DATA.firstName} {MOCK_DATA.lastName}
+          </Text>
+          <Text style={styles.subHeader}>{MOCK_DATA.job}</Text>
+        </Box>
       </Box>
     </DrawerContentScrollView>
   )
@@ -37,5 +42,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-between',
+    backgroundColor: 'white',
   },
+  header: { fontFamily: 'Nunito-Bold', color: theme.colors.black, fontSize: 18 },
+  subHeader: { fontFamily: 'Nunito-Regular', color: theme.colors.grey, fontSize: 16 },
 })
