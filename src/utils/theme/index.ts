955c935a16588895442360dbf6de31772e51434b
iceu-bb <bart.bajda@thewidlarzgroup.com>
2021-06-10 09:40:45 +0200

    Add extended ThemeBase, Add BaseOpacity restyle component Improve mkUseStyles function

--- src/utils/theme/index.ts
+++ src/utils/theme/index.ts
@@ -1,36 +1,11 @@
-import { createText, createBox, useTheme as useReTheme } from '@shopify/restyle'
-import { ViewStyle, TextStyle, ImageStyle } from 'react-native'
+import { useMemo } from 'react'
+import { createText, createBox, useTheme as useReTheme, RNStyle } from '@shopify/restyle'
+import { TouchableOpacityProps, TouchableOpacity } from 'react-native'
 import { textVariants } from './textVariants'
-import { colors, palette } from './colors'
+import { ThemeBase, themeBase } from './themeBase'
 
 export const theme = {
-  colors,
-  spacing: {
-    xs: 4,
-    s: 8,
-    xm: 12,
-    m: 16,
-    l: 24,
-    lplus: 30,
-    xl: 40,
-    xxl: 50,
-    xxxl: 75,
-  },
-  // must be sth here
-  breakpoints: {
-    phone: 0,
-    tablet: 768,
-  },
-  borderRadii: {
-    s: 4,
-    xm: 8,
-    m: 10,
-    mplus: 12,
-    l: 25,
-    lplus: 31,
-    xl: 75,
-    xxl: 100,
-  },
+  ...themeBase,
   textVariants,
 }
 
@@ -39,16 +14,21 @@ export const darkTheme: Theme = {
   ...theme,
   colors: {
     ...theme.colors,
-    mainBackground: palette.black,
   },
 }
 
 export const useTheme = () => useReTheme<Theme>()
 export const Box = createBox<Theme>()
 export const Text = createText<Theme>()
+export const BaseOpacity = createBox<
+  ThemeBase,
+  TouchableOpacityProps & {
+    children?: React.ReactNode
+  }
+>(TouchableOpacity)
 
-type NamedStyles<T> = { [P in keyof T]: ViewStyle | TextStyle | ImageStyle }
-export const makeStyles = <T extends NamedStyles<T>>(styles: (globalTheme: Theme) => T) => () => {
+type NamedStyles<T> = { [P in keyof T]: RNStyle }
+export const mkUseStyles = <T extends NamedStyles<T>>(styles: (globalTheme: Theme) => T) => () => {
   const currentTheme = useTheme()
-  return styles(currentTheme)
+  return useMemo(() => ({ ...styles(currentTheme) }), [styles, currentTheme])
 }
