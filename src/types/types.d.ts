559f2ec7e6d6e83d608421b4ee7e85223788655d
iceu-bb <bart.bajda@thewidlarzgroup.com>
2021-06-13 22:03:55 +0200

    Disable "no-undef" lint rule (ts do that by default, don't needed in eslint)

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -2,4 +2,3 @@ type F0<RT = void> = () => RT
 type F1<T, RT = void> = (arg: T) => RT
 type F2<T1, T2, RT = void> = (a1: T1, a2: T2) => RT
 type F3<T1, T2, T3, RT = void> = (a1: T1, a2: T2, a3: T3) => RT
-type Fn = (...args: any) => any
