18680ec8c3c76834e1795d6db8ad8161abc0723c
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-06-14 19:46:48 +0200

    MateHolidaysDetail component created

--- src/screens/dashboard/DashboardTeamMember.tsx
+++ src/screens/dashboard/DashboardTeamMember.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react'
 import { Box } from 'utils/theme'
-import { DashboardNavigationProps } from 'navigation/types'
+import { DashboardNavigationProps, DashboardNavigationType } from 'navigation/types'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import IconBack from 'assets/icons/icon-back.svg'
 import { MateHeader } from 'screens/dashboard/components/MateHeader'
 import { MateHoliday } from 'screens/dashboard/components/MateHoliday'
 import { MateHolidayDetail } from 'screens/dashboard/components/MateHolidayDetail'
 import { TouchableOpacity } from 'react-native'
-import { DashboardNavigationType } from 'navigation/types'
+
 import { useNavigation } from '@react-navigation/native'
 
 type DashboardTeamMemberProps = DashboardNavigationProps<'DashboardTeamMember'>
@@ -30,8 +30,8 @@ export const DashboardTeamMember: FC<DashboardTeamMemberProps> = ({ route }) => 
         <MateHeader {...params} />
         <MateHoliday {...params.holidays} />
         <Box flexDirection="row">
-          <MateHolidayDetail {...params.holidays} />
-          <MateHolidayDetail {...params.holidays} />
+          <MateHolidayDetail type="start" date={params.holidays.dayStart} />
+          <MateHolidayDetail type="end" date={params.holidays.dayEnd} />
         </Box>
       </Box>
     </SafeAreaWrapper>
--- src/screens/dashboard/components/MateHolidayDetail.tsx
+++ src/screens/dashboard/components/MateHolidayDetail.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Box, Text } from 'utils/theme'
-import { HolidayDetails } from 'types/holidaysDataTypes'
+import { useTranslation } from 'react-i18next'
+import { displayDayShort, setDateToBeDisplayed, displayWeekday } from 'utils/functions'
 
-export const MateHolidayDetail = (props: Required<HolidayDetails>) => {
-  const { isOnHoliday, dayStart, dayEnd } = props
+type MateHolidayDetailProps = {
+  type: 'start' | 'end'
+  date: string
+}
+
+export const MateHolidayDetail = (props: MateHolidayDetailProps) => {
+  const { date, type } = props
+  const { t, i18n } = useTranslation('dashboard')
+  const header = type === 'start' ? 'lastDayAtWork' : 'backAtWork'
+  const dateTobedisplay =
+    type === 'start' ? setDateToBeDisplayed(date, false) : setDateToBeDisplayed(date, true)
+  const color = type === 'start' ? 'greyDark' : 'black'
 
   return (
-    <Box padding="m">
-      <Text>details - TBD</Text>
+    <Box flexBasis="50%">
+      <Text variant="inputErrorMessage" marginVertical="m" color="headerGrey">
+        {t(header).toUpperCase()}
+      </Text>
+      <Text variant="bold20" color={color}>
+        {displayDayShort(dateTobedisplay, i18n.language)}
+      </Text>
+      <Text variant="regularGrey16" color={color}>
+        {displayWeekday(dateTobedisplay, i18n.language)}
+      </Text>
     </Box>
   )
 }
