964abe13389b3afc9a611e1b53ae903cab5e0235
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-06-14 12:07:43 +0200

    Add openDrawer button

--- src/screens/dashboard/components/DashboardHeader.tsx
+++ src/screens/dashboard/components/DashboardHeader.tsx
@@ -3,21 +3,25 @@ import { Box, Text, BaseOpacity } from 'utils/theme'
 import { useTranslation } from 'react-i18next'
 import IconProfile from 'assets/icons/icon-profile.svg'
 import IconBell from 'assets/icons/icon-bell.svg'
+import { useNavigation, DrawerActions } from '@react-navigation/native'
 
 export const DashboardHeader: FC = () => {
   const { t } = useTranslation('dashboard')
 
+  const navigation = useNavigation()
+
   return (
     <Box marginBottom="m" flexDirection="row" justifyContent="space-between" alignItems="center">
       <Box flexDirection="row" alignItems="center" justifyContent="flex-start">
-        <Box
+        <BaseOpacity
+          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
           bg="white"
           padding="xs"
           paddingLeft="m"
           borderTopRightRadius="lplus"
           borderBottomRightRadius="lplus">
           <IconProfile width={50} height={50} />
-        </Box>
+        </BaseOpacity>
         <Text variant="boldBlack18" paddingLeft="xm">
           {t('welcome')}
         </Text>
