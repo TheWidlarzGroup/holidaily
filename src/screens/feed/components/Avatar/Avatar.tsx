10acef3bd8447e87b5de8fa79861feedf35e037a
KacperM717 <kacpermikolajczak10@gmail.com>
2021-06-16 15:07:02 +0200

    Fix Avatar component

--- src/screens/feed/components/Avatar/Avatar.tsx
+++ src/screens/feed/components/Avatar/Avatar.tsx
@@ -3,19 +3,19 @@ import { Image } from 'react-native'
 import IconProfile from 'assets/icons/icon-profile.svg'
 import { Box } from 'utils/theme'
 
-type AvatarProps = {
-  src?: string
-  size?: 's' | 'm' | 'l' // | number
-}
-
 const sizes = {
   s: 44,
   m: 88,
   l: 176,
 }
 
+type AvatarProps = {
+  src?: string
+  size?: keyof typeof sizes | number
+}
+
 export const Avatar = ({ size = 'm', src }: AvatarProps) => {
-  const chosenSize = sizes[size] ?? sizes.m // size in sizes ? sizes[size] : size
+  const chosenSize = typeof size === 'number' ? size : sizes[size] ?? sizes.m
 
   return (
     <Box>
