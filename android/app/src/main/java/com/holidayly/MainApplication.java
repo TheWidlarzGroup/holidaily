7f1f55d22f8aa2359c8eaabdfb55ec442be2dae2
Piter Bedlechowicz <piter@thewidlarzgroup.com>
2021-06-16 14:19:33 +0200

    Unlink react-native-image-picker

--- android/app/src/main/java/com/holidayly/MainApplication.java
+++ android/app/src/main/java/com/holidayly/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
 import li.yunqi.rnsecurestorage.RNSecureStoragePackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -42,7 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
 
       @Override
       protected JSIModulePackage getJSIModulePackage() {
-        return new ReanimatedJSIModulePackage(); 
+        return new ReanimatedJSIModulePackage();
     }
   };
 
