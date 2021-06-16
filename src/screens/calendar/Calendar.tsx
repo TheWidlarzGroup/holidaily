6da3a038e3b8bc37f90ce17fd9021a721992d2ed
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-06-16 19:50:05 +0200

    Create DayInfo

--- src/screens/calendar/Calendar.tsx
+++ src/screens/calendar/Calendar.tsx
@@ -1,10 +1,11 @@
 import React, { FC, useState } from 'react'
 
 import { Box, theme } from 'utils/theme'
-import { FlatList, SafeAreaView, ScrollView } from 'react-native'
+import { FlatList, ScrollView } from 'react-native'
 import { SliderItem } from 'screens/calendar/SliderItem'
 import { Calendar as CalendarComponent } from 'components/Calendar'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
+import { DayInfo } from 'screens/calendar/DayInfo'
 
 const MOCKED_DATA = [
   { id: 1, title: 'Akademia', isSelected: true },
@@ -28,40 +29,76 @@ export const Calendar: FC = () => {
 
   return (
     <SafeAreaWrapper isDefaultBgColor>
-      <ScrollView>
-        <Box>
-          <FlatList
-            horizontal
-            data={data}
-            renderItem={({ item }) => (
-              <SliderItem {...item} toggleItemSelection={() => toggleItemSelection(item.id)} />
-            )}
-            ListHeaderComponent={() => <Box width={8} />}
-            ListFooterComponent={() => <Box width={8} />}
-            keyExtractor={({ id }) => id.toString()}
-            showsHorizontalScrollIndicator={false}
+      <Box paddingBottom="xl">
+        <FlatList
+          horizontal
+          data={data}
+          renderItem={({ item }) => (
+            <SliderItem {...item} toggleItemSelection={() => toggleItemSelection(item.id)} />
+          )}
+          ListHeaderComponent={() => <Box width={8} />}
+          ListFooterComponent={() => <Box width={8} />}
+          keyExtractor={({ id }) => id.toString()}
+          showsHorizontalScrollIndicator={false}
+        />
+        <Box
+          borderRadius="m"
+          backgroundColor="white"
+          marginTop="m"
+          paddingBottom="m"
+          style={{
+            borderRadius: 16,
+            shadowOffset: { width: 0, height: 0 },
+            shadowColor: theme.colors.black,
+            shadowOpacity: 0.1,
+            shadowRadius: 10,
+            elevation: 10,
+            borderWidth: 0,
+          }}>
+          <CalendarComponent
+            theme={{
+              calendarBackground: 'transparent',
+            }}
           />
-          <Box
-            borderRadius="m"
-            backgroundColor="white"
-            marginTop="m"
-            style={{
-              borderRadius: 16,
-              shadowOffset: { width: 0, height: 0 },
-              shadowColor: theme.colors.black,
-              shadowOpacity: 0.1,
-              shadowRadius: 10,
-              elevation: 10,
-              borderWidth: 0,
-            }}>
-            <CalendarComponent
-              theme={{
-                calendarBackground: 'transparent',
-              }}
-            />
-          </Box>
         </Box>
-      </ScrollView>
+        <Box marginTop="m">
+          <ScrollView>
+            <DayInfo
+              date="2020-06-16"
+              events={[
+                {
+                  person: 'Jakub Grzywacz',
+                  reason: 'Vaccation',
+                  position: 'Software Developer',
+                  color: 'red',
+                },
+              ]}
+            />
+            <DayInfo
+              date="2020-06-17"
+              events={[
+                {
+                  person: 'Jakub Grzywacz',
+                  reason: 'Vaccation',
+                  position: 'Software Developer',
+                  color: 'red',
+                },
+              ]}
+            />
+            <DayInfo
+              date="2020-06-18"
+              events={[
+                {
+                  person: 'Jakub Grzywacz',
+                  reason: 'Vaccation',
+                  position: 'Software Developer',
+                  color: 'red',
+                },
+              ]}
+            />
+          </ScrollView>
+        </Box>
+      </Box>
     </SafeAreaWrapper>
   )
 }
