b73e1906ca081482091ef28d42f100954d712bde
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-06-17 09:25:46 +0200

    Fixed - animation background, safe ara wrapper bottom padd

--- src/components/SafeAreaWrapper.tsx
+++ src/components/SafeAreaWrapper.tsx
@@ -3,12 +3,23 @@ import React, { FC } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StyleSheet } from 'react-native'
 import { colors } from 'utils/theme/colors'
+import { theme } from 'utils/theme'
 
 type WrapperProps = {
   isDefaultBgColor?: boolean
+  isTabNavigation?: boolean
 }
-export const SafeAreaWrapper: FC<WrapperProps> = ({ children, isDefaultBgColor }) => (
-  <SafeAreaView style={[styles.container, !isDefaultBgColor && styles.containerBackground]}>
+export const SafeAreaWrapper: FC<WrapperProps> = ({
+  isDefaultBgColor,
+  isTabNavigation,
+  children,
+}) => (
+  <SafeAreaView
+    style={[
+      styles.container,
+      !isDefaultBgColor && styles.containerBackground,
+      isTabNavigation && { paddingBottom: theme.spacing.xxxl },
+    ]}>
     {children}
   </SafeAreaView>
 )
--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -6,6 +6,7 @@ import { TabsUi } from 'navigation/BottomNavComponents/TabsUi'
 import { Calendar } from 'screens/calendar/Calendar'
 import { Panel } from 'screens/panel/Panel'
 import { Chat } from 'screens/chat/Chat'
+import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { BottomTabRoutes } from './types'
 import { DashboardNavigation } from './DashboardNavigation'
 
@@ -22,17 +23,19 @@ const tabs = [
   { name: 'Chat' },
 ]
 export const BottomTabNavigator = ({ style }: ViewProps) => (
-  <Animated.View style={[style, { flex: 1 }]}>
-    <Tab.Navigator tabBar={(props) => <TabsUi {...{ tabs, ...props }} />}>
-      <Tab.Screen
-        name="DashboardNavigation"
-        options={{ unmountOnBlur: true }}
-        component={DashboardNavigation}
-      />
-      <Tab.Screen name="Calendar" component={Calendar} />
-      <Tab.Screen name="RequestModal" component={EmptyComponent} />
-      <Tab.Screen name="Panel" component={Panel} />
-      <Tab.Screen name="Chat" component={Chat} />
-    </Tab.Navigator>
-  </Animated.View>
+  <SafeAreaWrapper>
+    <Animated.View style={[style, { flex: 1 }]}>
+      <Tab.Navigator tabBar={(props) => <TabsUi {...{ tabs, ...props }} />}>
+        <Tab.Screen
+          name="DashboardNavigation"
+          options={{ unmountOnBlur: true }}
+          component={DashboardNavigation}
+        />
+        <Tab.Screen name="Calendar" component={Calendar} />
+        <Tab.Screen name="RequestModal" component={EmptyComponent} />
+        <Tab.Screen name="Panel" component={Panel} />
+        <Tab.Screen name="Chat" component={Chat} />
+      </Tab.Navigator>
+    </Animated.View>
+  </SafeAreaWrapper>
 )
