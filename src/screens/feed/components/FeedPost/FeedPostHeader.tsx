b6bd9cb6fc8d1eec276b9f5e8e0ff20d8061db2a
KacperM717 <kacpermikolajczak10@gmail.com>
2021-06-18 17:09:07 +0200

    Refractor FeedPostHeader

--- src/screens/feed/components/FeedPost/FeedPostHeader.tsx
+++ src/screens/feed/components/FeedPost/FeedPostHeader.tsx
@@ -1,34 +1,25 @@
 import { DateTime } from 'luxon'
 import React from 'react'
-import { PostMetaData } from 'screens/feed/types'
+import { FeedPost } from 'screens/feed/types'
 import { BaseOpacity, Box, Text } from 'utils/theme'
 import { Avatar } from 'components/Avatar'
 
-type FeedPostHeaderProps = {
-  meta: PostMetaData
-}
+// TODO: Localization
 
-export const FeedPostHeader = ({ meta }: FeedPostHeaderProps) => {
-  const src =
-    'https://images.unsplash.com/photo-1617731653770-a62c51cf8696?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+type FeedPostHeaderProps = Pick<FeedPost, 'meta'>
 
-  const handlePress = () => {
-    // Switch to Post Screen
-  }
-
-  return (
-    <BaseOpacity onPress={handlePress}>
-      <Box justifyContent="flex-start" flexDirection="row">
-        <Box padding="s">
-          <Avatar src={src} />
-        </Box>
-        <Box padding="s" flex={1}>
-          <HeaderInfo meta={meta} />
-        </Box>
+export const FeedPostHeader = ({ meta }: FeedPostHeaderProps) => (
+  <BaseOpacity>
+    <Box justifyContent="flex-start" flexDirection="row">
+      <Box padding="s">
+        <Avatar src={meta.author.pictureUrl} />
       </Box>
-    </BaseOpacity>
-  )
-}
+      <Box padding="s" flex={1} paddingRight="m">
+        <HeaderInfo meta={meta} />
+      </Box>
+    </Box>
+  </BaseOpacity>
+)
 
 type HeaderInfoProps = Pick<FeedPostHeaderProps, 'meta'>
 
@@ -38,7 +29,7 @@ const HeaderInfo = ({ meta }: HeaderInfoProps) => {
 
   return (
     <>
-      <Box>
+      <Box flex={0.3}>
         <Text variant="labelGrey" textAlign="right">
           {formattedDate}
         </Text>
