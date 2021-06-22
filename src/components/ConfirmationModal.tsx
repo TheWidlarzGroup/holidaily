7abe6ad0648424f9586aa81f23b493a7f777da21
Magda <73060297+mjaskowska@users.noreply.github.com>
2021-06-22 08:38:51 +0100

    Feature | edit profile picture (#93) WIP  (#130)
    * fix line between buttons & add ripple effect
    
    * add edit picture modal
    
    * add edit picture modal to edit profile screen
    
    * add confirmation modal
    
    * update confirmation modal with custom buttons
    
    * move save changes button to separate component
    
    * fix customButton in ConfirmationModal
    
    * fix styling
    
    * add initial modal implementation to EditProfile
    
    * add back to dashboard button
    
    * add modalManager context
    
    * implement modalManager to UserProfile
    
    * fix type bug
    
    * add CR changes

---
+++ src/components/ConfirmationModal.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { ModalProps } from 'react-native-modal'
+import { useTranslation } from 'react-i18next'
+import { CustomModal } from 'components/CustomModal'
+import { theme, mkUseStyles, Theme, Text, Box } from 'utils/theme'
+import { CustomButton } from './CustomButton'
+
+type ConfirmationModalProps = Pick<ModalProps, 'isVisible'> & {
+  hideModal: F0
+  onAccept: F0
+  onDecline: F0
+  content: string
+}
+export const ConfirmationModal = ({
+  isVisible,
+  hideModal,
+  onAccept,
+  onDecline,
+  content,
+}: ConfirmationModalProps) => {
+  const styles = useStyles()
+  const { t } = useTranslation('confirmationModal')
+
+  return (
+    <CustomModal
+      isVisible={isVisible}
+      onBackdropPress={hideModal}
+      backdropColor={theme.colors.white}
+      backdropOpacity={0}
+      animationIn="slideInUp"
+      animationOut="slideOutDown"
+      animationInTiming={300}
+      animationOutTiming={300}
+      swipeDirection="down"
+      style={styles.modal}
+      hideModalContentWhileAnimating>
+      <Box justifyContent="center" alignItems="center">
+        <Text variant="boldBlackCenter20" marginBottom="l">
+          {t('areYouSure')}
+        </Text>
+        <Text variant="body1" marginBottom="xxl">
+          {content}
+        </Text>
+        <Box marginBottom="xm">
+          <TouchableOpacity onPress={onAccept} activeOpacity={1}>
+            <CustomButton label={t('yes')} variant="blackBgButton" width={221} height={53} />
+          </TouchableOpacity>
+        </Box>
+        <Box>
+          <TouchableOpacity onPress={onDecline} activeOpacity={1}>
+            <CustomButton label={t('no')} variant="secondary" width={221} height={53} />
+          </TouchableOpacity>
+        </Box>
+      </Box>
+    </CustomModal>
+  )
+}
+
+const useStyles = mkUseStyles((theme: Theme) => ({
+  modal: {
+    flex: 1,
+    height: 350,
+    backgroundColor: theme.colors.primary,
+    position: 'absolute',
+    bottom: -20,
+    left: -20,
+    right: -20,
+    borderTopLeftRadius: theme.borderRadii.lmin,
+    borderTopRightRadius: theme.borderRadii.lmin,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOffset: { width: -2, height: 0 },
+    shadowColor: theme.colors.black,
+    shadowOpacity: 0.04,
+    shadowRadius: 2,
+    elevation: 20,
+  },
+}))
