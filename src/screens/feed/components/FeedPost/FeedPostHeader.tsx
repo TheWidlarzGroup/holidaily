c836dbc87cc297eaf1f542a176a38132f6b42f46
KacperM717 <kacpermikolajczak10@gmail.com>
2021-06-22 13:27:59 +0200

    Fix FeedPostHeader

--- src/screens/feed/components/FeedPost/FeedPostHeader.tsx
+++ src/screens/feed/components/FeedPost/FeedPostHeader.tsx
@@ -1,47 +1,14 @@
-import { DateTime } from 'luxon'
 import React from 'react'
 import { FeedPost } from 'screens/feed/types'
-import { BaseOpacity, Box, Text } from 'utils/theme'
+import { Box } from 'utils/theme'
 import { Avatar } from 'components/Avatar'
-import { displayDDMonYYYY } from 'utils/functions'
-import { useTranslation } from 'react-i18next'
+import { FeedPostHeaderInfo } from 'screens/feed/FeedPostHeaderInfo/FeedPostHeaderInfo'
 
 type FeedPostHeaderProps = Pick<FeedPost, 'meta'>
 
 export const FeedPostHeader = ({ meta }: FeedPostHeaderProps) => (
-  <BaseOpacity>
-    <Box justifyContent="flex-start" flexDirection="row">
-      <Box padding="s">
-        <Avatar src={meta.author.pictureUrl} />
-      </Box>
-      <Box padding="s" flex={1} paddingRight="m">
-        <HeaderInfo meta={meta} />
-      </Box>
-    </Box>
-  </BaseOpacity>
+  <Box flexDirection="row" padding="s">
+    <Avatar src={meta.author.pictureUrl} marginRight="s" />
+    <FeedPostHeaderInfo meta={meta} />
+  </Box>
 )
-
-type HeaderInfoProps = Pick<FeedPostHeaderProps, 'meta'>
-
-const HeaderInfo = ({ meta }: HeaderInfoProps) => {
-  const { timestamp, author } = meta
-
-  const { i18n } = useTranslation('feed')
-  const formattedDate = displayDDMonYYYY(DateTime.fromJSDate(timestamp.createdAt), i18n.language)
-
-  return (
-    <>
-      <Box flex={0.3}>
-        <Text variant="labelGrey" textAlign="right">
-          {formattedDate}
-        </Text>
-      </Box>
-      <Box>
-        <Text variant="boldBlack18">{author.lastName}</Text>
-      </Box>
-      <Box>
-        <Text variant="lightGreyBold">{author.firstName}</Text>
-      </Box>
-    </>
-  )
-}
