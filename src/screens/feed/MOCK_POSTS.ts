59d558789a0a1387e77a12877a5c7632f53d550f
KacperM717 <kacpermikolajczak10@gmail.com>
2021-06-23 13:16:58 +0200

    Fix MOCK_POST eslint errors

--- src/screens/feed/MOCK_POSTS.ts
+++ src/screens/feed/MOCK_POSTS.ts
@@ -16,18 +16,15 @@ export const MOCK_POSTS: FeedPost[] = [
         editedAt: new Date(),
       },
     },
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit.',
     data: [
       {
         type: 'image',
-        src:
-          'https://images.unsplash.com/photo-1617731653770-a62c51cf8696?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        src: 'https://images.unsplash.com/photo-1617731653770-a62c51cf8696?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       },
       {
         type: 'image',
-        src:
-          'https://images.unsplash.com/photo-1623764802787-eea7b530c90a?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        src: 'https://images.unsplash.com/photo-1623764802787-eea7b530c90a?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       },
     ],
     reactions: [
@@ -96,8 +93,7 @@ export const MOCK_POSTS: FeedPost[] = [
         editedAt: new Date(),
       },
     },
-    text:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illo architecto maxime inventore ex non exercitationem soluta. Aperiam accusamus iure sed velit, asperiores sapiente soluta consequatur, rerum debitis eligendi odit adipisci quaerat! Illo laudantium repellendus ducimus molestiae similique modi voluptatibus voluptates cupiditate, esse quis mollitia dolore doloremque officiis excepturi quia? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit.',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum illo architecto maxime inventore ex non exercitationem soluta. Aperiam accusamus iure sed velit, asperiores sapiente soluta consequatur, rerum debitis eligendi odit adipisci quaerat! Illo laudantium repellendus ducimus molestiae similique modi voluptatibus voluptates cupiditate, esse quis mollitia dolore doloremque officiis excepturi quia? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit.',
     data: [],
     reactions: [],
     comments: [],
@@ -117,13 +113,11 @@ export const MOCK_POSTS: FeedPost[] = [
         editedAt: new Date(),
       },
     },
-    text:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit. Dicta optio non praesentium consequuntur saepe magnam eum cumque excepturi nobis reprehenderit.',
     data: [
       {
         type: 'image',
-        src:
-          'https://images.unsplash.com/photo-1617731653770-a62c51cf8696?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        src: 'https://images.unsplash.com/photo-1617731653770-a62c51cf8696?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
       },
     ],
     reactions: [
@@ -153,8 +147,7 @@ export const MOCK_POSTS: FeedPost[] = [
           },
         },
         comments: [],
-        text:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi earum illo, incidunt aperiam adipisci vel.',
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi earum illo, incidunt aperiam adipisci vel.',
         reactions: [
           {
             type: '😀',
