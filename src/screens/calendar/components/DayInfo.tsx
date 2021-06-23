d1a4f486d481e5d26952361680c4f16282c31c3d
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-06-23 11:44:50 +0200

    Split DayInfo into smaller components

--- src/screens/calendar/components/DayInfo.tsx
+++ src/screens/calendar/components/DayInfo.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import { Box, Text } from 'utils/theme'
-import UserIconPlaceholder from 'assets/icons/icon-profile.svg'
-import { getDateWithMonthString, getDayName } from 'utils/dates'
+import { getDateWithMonthString } from 'utils/dates'
 import { useTranslation } from 'react-i18next'
-import { weekendBasedStyles } from 'screens/calendar/utils'
-
-export type DayOffEvent = {
-  person: string
-  reason: string
-  position: string
-  color: string
-}
+import { DayWeekend } from './DayWeekend'
+import { DayEvent, DayOffEvent } from './DayEvent'
 
 export type DayInfoProps = {
   date: string
@@ -21,20 +14,7 @@ export type DayInfoProps = {
 export const DayInfo = ({ date, events, weekend }: DayInfoProps) => {
   const { i18n } = useTranslation()
 
-  if (weekend)
-    return (
-      <Box
-        paddingVertical="m"
-        paddingHorizontal="lplus"
-        borderColor="white"
-        backgroundColor="disabled"
-        justifyContent="space-between"
-        flexDirection="row"
-        style={weekendBasedStyles(weekend)}>
-        <Text variant="regularWhite12">{getDateWithMonthString(date, i18n.language)}</Text>
-        <Text variant="boldWhite12">{getDayName(date, i18n.language)}</Text>
-      </Box>
-    )
+  if (weekend) return <DayWeekend date={date} weekend={weekend} />
   return (
     <Box
       borderRadius="lmin"
@@ -46,29 +26,7 @@ export const DayInfo = ({ date, events, weekend }: DayInfoProps) => {
       {typeof events !== 'undefined' && events?.length > 0 && (
         <Box marginTop="s">
           {events.map((event) => (
-            <Box paddingVertical="s" flexDirection="row" alignItems="center" key={event.person}>
-              <UserIconPlaceholder width={24} height={24} />
-              <Box
-                marginHorizontal="s"
-                width={3}
-                height={24}
-                borderRadius="s"
-                style={{ backgroundColor: event.color }}
-              />
-              <Box>
-                <Box flexDirection="row" alignItems="center">
-                  <Text fontSize={12} fontFamily="Nunito-Bold" lineHeight={14}>
-                    {`${event.person}: `}
-                  </Text>
-                  <Text fontSize={12} fontFamily="Nunito-Regular" lineHeight={14}>
-                    {event.reason}
-                  </Text>
-                </Box>
-                <Text fontSize={12} fontFamily="Nunito-Regular" lineHeight={14} color="grey">
-                  {event.position}
-                </Text>
-              </Box>
-            </Box>
+            <DayEvent event={event} key={event.id} />
           ))}
         </Box>
       )}
