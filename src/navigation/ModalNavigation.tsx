219b1c6abdf07a7385e5f90652327ee8f911deee
KacperM717 <kacpermikolajczak10@gmail.com>
2021-06-24 17:20:22 +0200

    Hotfix GalleryScreen bgColor

--- src/navigation/ModalNavigation.tsx
+++ src/navigation/ModalNavigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack'
 
-import { Box } from 'utils/theme'
+import { Box, mkUseStyles, Theme } from 'utils/theme'
 import { RequestVacation } from 'screens/requestVacation/RequestVacation'
 import { CalendarRequestVacation } from 'screens/requestVacation/components/CalendarRequestVacation'
 import { GalleryScreen } from 'screens/gallery/GalleryScreen'
@@ -10,27 +10,36 @@ import { ModalRoutes } from './types'
 
 const AppStack = createStackNavigator<ModalRoutes>()
 
-export const ModalNavigation = () => (
-  <Box flex={1} backgroundColor="black">
-    <AppStack.Navigator
-      mode="modal"
-      headerMode="none"
-      initialRouteName="DrawerNavigator"
-      screenOptions={{
-        ...TransitionPresets.ModalPresentationIOS,
-        animationEnabled: true,
-      }}>
-      <AppStack.Screen name="RequestVacation" component={RequestVacation} />
-      <AppStack.Screen name="RequestVacationCalendar" component={CalendarRequestVacation} />
-      <AppStack.Screen name="DrawerNavigator" component={DrawerNavigator} />
-      <AppStack.Screen
-        name="Gallery"
-        component={GalleryScreen}
-        options={{
-          cardStyle: { backgroundColor: 'rgba(30,30,30,0.95)' },
-          cardStyleInterpolator: undefined,
-        }}
-      />
-    </AppStack.Navigator>
-  </Box>
-)
+export const ModalNavigation = () => {
+  const styles = useStyle()
+  return (
+    <Box flex={1} backgroundColor="black">
+      <AppStack.Navigator
+        mode="modal"
+        headerMode="none"
+        initialRouteName="DrawerNavigator"
+        screenOptions={{
+          ...TransitionPresets.ModalPresentationIOS,
+          animationEnabled: true,
+        }}>
+        <AppStack.Screen name="RequestVacation" component={RequestVacation} />
+        <AppStack.Screen name="RequestVacationCalendar" component={CalendarRequestVacation} />
+        <AppStack.Screen name="DrawerNavigator" component={DrawerNavigator} />
+        <AppStack.Screen
+          name="Gallery"
+          component={GalleryScreen}
+          options={{
+            cardStyle: styles.galleryScreenCard,
+            cardStyleInterpolator: undefined,
+          }}
+        />
+      </AppStack.Navigator>
+    </Box>
+  )
+}
+
+const useStyle = mkUseStyles((theme: Theme) => ({
+  galleryScreenCard: {
+    backgroundColor: theme.colors.modalBackground,
+  },
+}))
