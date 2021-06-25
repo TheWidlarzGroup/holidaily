df4570ccbf200c22b9af40bbbb4bdb460a56a36d
Magda <jaskowska.magda@gmail.com>
2021-06-25 17:49:33 +0100

    add unsubscribe team functionality

--- src/screens/editProfile/components/TeamSubscriptions.tsx
+++ src/screens/editProfile/components/TeamSubscriptions.tsx
@@ -2,26 +2,57 @@ import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigation } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
+import { useModalContext } from 'contexts/ModalProvider'
 import { BaseOpacity, Box, Text, mkUseStyles, Theme } from 'utils/theme'
 import { TeamsType } from 'utils/mocks/teamsMocks'
 import IconAdd from 'assets/icons/icon-add.svg'
+import { ChangesSavedModal } from 'components/ChangesSavedModal'
+import { ConfirmationModal } from 'components/ConfirmationModal'
 import { useUserDetailsContext } from '../helpers/UserDetailsContext'
 
 export const TeamSubscriptions = () => {
+  const { handleModal } = useModalContext()
   const { t } = useTranslation('userProfile')
   const styles = useStyles()
   const { navigate } = useNavigation()
-  const { userTeams } = useUserDetailsContext()
+  const { userTeams, setUserTeams } = useUserDetailsContext()
   const [teams, setTeams] = useState<TeamsType[]>([])
+
   useEffect(() => {
     setTeams(userTeams)
   }, [userTeams])
 
   const onAddSubscribedTeam = () => navigate('SubscribeTeam')
 
-  const onUnsubscribeTeam = () => {
-    // TODO display modal to confirm changes
-    console.log('unsubscribe')
+  const filterUnsubscribedTeams = (teamName: string) => {
+    const subscriptions = userTeams.filter((team) => team.teamName !== teamName)
+    setUserTeams(subscriptions)
+  }
+
+  const showChangesSavedModal = (teamName: string) =>
+    handleModal(
+      <ChangesSavedModal
+        isVisible
+        hideModal={() => handleModal()}
+        content={`${teamName} unsubscribed!`}
+      />
+    )
+  const onUnsubscribeTeam = (teamName: string) => {
+    handleModal(
+      <ConfirmationModal
+        isVisible
+        hideModal={() => handleModal()}
+        onAccept={() => {
+          handleModal()
+          filterUnsubscribedTeams(teamName)
+          showChangesSavedModal(teamName)
+        }}
+        onDecline={() => {
+          handleModal()
+        }}
+        content={`If you unsubscribe ${teamName} Team you will no longer see its members.`}
+      />
+    )
   }
 
   return (
@@ -44,7 +75,7 @@ export const TeamSubscriptions = () => {
       </BaseOpacity>
       <Box flexDirection="row" marginRight="xl" flexWrap="wrap">
         {teams.map(({ teamName, id }) => (
-          <RectButton onPress={onUnsubscribeTeam} key={id} style={styles.team}>
+          <RectButton onPress={() => onUnsubscribeTeam(teamName)} key={id} style={styles.team}>
             <Text variant="resendWhite" paddingHorizontal="l" paddingVertical="xm">
               {teamName}
             </Text>
