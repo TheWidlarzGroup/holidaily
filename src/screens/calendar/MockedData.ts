e623b00bf0055fd2df4d8cae7ee0972a905b6955
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-06-28 18:41:22 +0200

    Add more mocked data

--- src/screens/calendar/MockedData.ts
+++ src/screens/calendar/MockedData.ts
@@ -28,19 +28,15 @@ export const MOCKED_DATA = {
       days: [
         {
           date: '2021-06-01',
-          events: [],
         },
         {
           date: '2021-06-02',
-          events: [],
         },
         {
           date: '2021-06-03',
-          events: [],
         },
         {
           date: '2021-06-04',
-          events: [],
         },
         {
           date: '2021-06-05',
@@ -52,23 +48,18 @@ export const MOCKED_DATA = {
         },
         {
           date: '2021-06-07',
-          events: [],
         },
         {
           date: '2021-06-08',
-          events: [],
         },
         {
           date: '2021-06-09',
-          events: [],
         },
         {
           date: '2021-06-10',
-          events: [],
         },
         {
           date: '2021-06-11',
-          events: [],
         },
         {
           date: '2021-06-12',
@@ -80,15 +71,12 @@ export const MOCKED_DATA = {
         },
         {
           date: '2021-06-14',
-          events: [],
         },
         {
           date: '2021-06-15',
-          events: [],
         },
         {
           date: '2021-06-16',
-          events: [],
         },
         {
           date: '2021-06-17',
@@ -114,18 +102,10 @@ export const MOCKED_DATA = {
               color: 'red',
               id: 3,
             },
-            {
-              person: 'Tom Waits',
-              reason: 'Vacation in Portugal',
-              position: 'Software Developer',
-              color: 'blue',
-              id: 4,
-            },
           ],
         },
         {
           date: '2021-06-18',
-          events: [],
         },
         {
           date: '2021-06-19',
@@ -137,15 +117,12 @@ export const MOCKED_DATA = {
         },
         {
           date: '2021-06-21',
-          events: [],
         },
         {
           date: '2021-06-22',
-          events: [],
         },
         {
           date: '2021-06-23',
-          events: [],
         },
         {
           date: '2021-06-24',
@@ -182,7 +159,6 @@ export const MOCKED_DATA = {
         },
         {
           date: '2021-06-25',
-          events: [],
         },
         {
           date: '2021-06-26',
@@ -194,15 +170,42 @@ export const MOCKED_DATA = {
         },
         {
           date: '2021-06-28',
-          events: [],
         },
         {
           date: '2021-06-29',
-          events: [],
+          events: [
+            {
+              person: 'Peter Kansas',
+              reason: 'A couple days off',
+              position: 'Software Developer',
+              color: '#FF88DC',
+              id: 1,
+            },
+            {
+              person: 'Tom Waits',
+              reason: 'Vacation in Portugal',
+              position: 'Software Developer',
+              color: '#91A6FF',
+              id: 2,
+            },
+            {
+              person: 'Tom Waits',
+              reason: 'Vacation in Portugal',
+              position: 'Software Developer',
+              color: 'dodgerblue',
+              id: 3,
+            },
+            {
+              person: 'Tom Waits',
+              reason: 'Vacation in Portugal',
+              position: 'Software Developer',
+              color: 'blue',
+              id: 4,
+            },
+          ],
         },
         {
           date: '2021-06-30',
-          events: [],
         },
       ],
     },
