99397fe2fec49a7817ac5b29e0327405131663d7
KacperM717 <kacpermikolajczak10@gmail.com>
2021-06-29 12:33:35 +0200

    Fix CreatePostBody

--- src/screens/createPost/CreatePostForm/CreatePostForm.tsx
+++ src/screens/createPost/CreatePostForm/CreatePostForm.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { Box } from 'utils/theme'
-import { Gallery } from 'components/Gallery/Gallery'
 import { GalleryItemData } from 'types/holidaysDataTypes'
 import { Asset } from 'react-native-image-picker'
 import { ScrollView } from 'react-native-gesture-handler'
@@ -23,15 +21,11 @@ export const CreatePostForm = (props: CreatePostFormProps) => {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <PostHeader />
-      <Box flexGrow={1} padding="s">
-        <Box paddingBottom="m">
-          <PostBody
-            text={state.text}
-            onTextChange={(text) => dispatch({ type: 'updateText', payload: { text } })}
-          />
-        </Box>
-        <Gallery data={galleryImages} />
-      </Box>
+      <PostBody
+        text={state.text}
+        onTextChange={(text) => dispatch({ type: 'updateText', payload: { text } })}
+        data={galleryImages}
+      />
       {!keyboardOpened && (
         <PostFooter
           onCTAPress={() => props.onSend(state)}
--- src/screens/createPost/CreatePostForm/PostFormBody.tsx
+++ src/screens/createPost/CreatePostForm/PostFormBody.tsx
@@ -4,11 +4,14 @@ import { useTranslation } from 'react-i18next'
 import { Box, mkUseStyles } from 'utils/theme'
 import { Avatar } from 'components/Avatar'
 import { TextInput } from 'react-native-gesture-handler'
+import { Gallery } from 'components/Gallery/Gallery'
 import { themeBase } from 'utils/theme/themeBase'
+import { GalleryItemData } from 'types/holidaysDataTypes'
 
 type PostBodyProps = {
   text: string
   onTextChange: F1<string>
+  data: GalleryItemData[]
 }
 
 export const PostBody = (props: PostBodyProps) => {
@@ -18,16 +21,19 @@ export const PostBody = (props: PostBodyProps) => {
   const styles = useStyles()
 
   return (
-    <Box flexDirection="row">
-      <Avatar src={user.photo} size="s" padding="l" />
-      <TextInput
-        multiline
-        underlineColorAndroid={themeBase.colors.transparent}
-        style={styles.textInput}
-        placeholder={t('inputPlaceholder')}
-        onChangeText={props.onTextChange}
-        value={props.text}
-      />
+    <Box flexGrow={1} padding="s">
+      <Box flexDirection="row">
+        <Avatar src={user.photo} size="s" padding="l" />
+        <TextInput
+          multiline
+          underlineColorAndroid={themeBase.colors.transparent}
+          style={styles.textInput}
+          placeholder={t('inputPlaceholder')}
+          onChangeText={props.onTextChange}
+          value={props.text}
+        />
+      </Box>
+      <Gallery data={props.data} />
     </Box>
   )
 }
@@ -35,5 +41,8 @@ export const PostBody = (props: PostBodyProps) => {
 const useStyles = mkUseStyles(() => ({
   textInput: {
     flexGrow: 1,
+    padding: themeBase.spacing.m,
+    paddingBottom: themeBase.spacing.l,
+    borderColor: themeBase.colors.transparent,
   },
 }))
