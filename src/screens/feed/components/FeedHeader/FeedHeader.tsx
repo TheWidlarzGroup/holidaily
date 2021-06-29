bede16a1a27ce601c3ccf7a8311e963e9b79d38f
KacperM717 <kacpermikolajczak10@gmail.com>
2021-06-29 16:25:25 +0200

    Hotfix FeedHeader paddings

--- src/screens/feed/components/FeedHeader/FeedHeader.tsx
+++ src/screens/feed/components/FeedHeader/FeedHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, BaseOpacity, Text } from 'utils/theme'
 import { Avatar } from 'components/Avatar'
-import IconGallery from 'assets/icons/icon-gallery.svg'
+import IconGallery from 'assets/icons/icon-gallery-2.svg'
 import { useTranslation } from 'react-i18next'
 import { useNavigation } from '@react-navigation/native'
 
@@ -10,14 +10,14 @@ export const FeedHeader = () => {
   const { t } = useTranslation('feed')
 
   return (
-    <Box flexDirection="row" alignItems="center" padding="m" bg="white" borderRadius="l">
+    <Box flexDirection="row" alignItems="stretch" padding="m" bg="white" borderRadius="l">
       <Box marginRight="m">
         <Avatar size="s" />
       </Box>
-      <BaseOpacity flexGrow={1} onPress={() => navigate('CreatePost')}>
+      <BaseOpacity flexGrow={1} onPress={() => navigate('CreatePost')} justifyContent="center">
         <Text>{t('createPostLabel')}</Text>
       </BaseOpacity>
-      <BaseOpacity padding="s" onPress={() => navigate('CreatePost')}>
+      <BaseOpacity onPress={() => navigate('CreatePost')} justifyContent="center">
         <IconGallery />
       </BaseOpacity>
     </Box>
