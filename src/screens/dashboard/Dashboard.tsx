df66c1023d8076787e280d2218a4593edaf57dbf
Monika Dziub-Korczynska <66638216+monika1127@users.noreply.github.com>
2021-06-30 11:59:38 +0200

    Feature/teams rearranging (#131)
    close #99 
    
    FEATURE | Teams - Rearranging 
    
    * Add - step1 - Dragable element
    
    * Add - step2 - Element drop
    
    * add - step3 - drag and drop with scroll
    
    * Cearousel Component created
    
    * Dragged after long press and finger lift

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { Box, Text } from 'utils/theme'
-import { ScrollView } from 'react-native'
+
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { DashboardHeader } from 'screens/dashboard/components/DashboardHeader'
 import { USER_GROUPS_DAYS_OFF } from 'screens/dashboard/helpers/temporaryData'
 import { ValidationOfGroupDayOff } from 'types/holidaysDataTypes'
 import { TeamElement } from 'screens/dashboard/components/TeamElement'
-import { CarouselElement } from 'screens/dashboard/components/CarouselElement'
-import { dataToBeDisplayed, ValidationOfDataToBeDisplayed } from 'screens/dashboard/helpers/helper'
 import { DashboardNavigationType } from 'navigation/types'
 import { useNavigation } from '@react-navigation/native'
+import { SortableList } from 'screens/dashboard/dragAndDrop/SortableList'
 
 export const Dashboard = () => {
-  const { t, i18n } = useTranslation('dashboard')
   const teamsList: ValidationOfGroupDayOff[] = USER_GROUPS_DAYS_OFF
-  const companyHolidaysData: ValidationOfDataToBeDisplayed[] = dataToBeDisplayed(i18n.language)
 
   const navigation = useNavigation<DashboardNavigationType<'Dashboard'>>()
   const navigateToTeamDetails = (team: ValidationOfGroupDayOff) =>
@@ -24,34 +19,15 @@ export const Dashboard = () => {
   return (
     <SafeAreaWrapper isDefaultBgColor isTabNavigation edges={['left', 'right', 'bottom']}>
       <DashboardHeader />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {companyHolidaysData.map((item) => (
-            <CarouselElement
-              key={item.id}
-              isOnHoliday={item.isOnHoliday}
-              firstName={item.user.firstName}
-              lastName={item.user.lastName}
-              photo={item.user.photo}
-              dayToBeDisplayed={item.dayToBeDisplayed}
-            />
-          ))}
-        </ScrollView>
-        <Box marginTop="l">
-          <Text variant="lightGreyRegular" color="headerGrey" marginHorizontal="m">
-            {t('teamsList').toUpperCase()}
-          </Text>
-          <Box m="s" flexDirection="row" flexWrap="wrap" justifyContent="space-between">
-            {teamsList.map((team) => (
-              <TeamElement
-                {...team}
-                key={team.groupId}
-                navigateToTeamScreen={() => navigateToTeamDetails(team)}
-              />
-            ))}
-          </Box>
-        </Box>
-      </ScrollView>
+      <SortableList>
+        {teamsList.map((team) => (
+          <TeamElement
+            {...team}
+            key={team.groupId}
+            navigateToTeamScreen={() => navigateToTeamDetails(team)}
+          />
+        ))}
+      </SortableList>
     </SafeAreaWrapper>
   )
 }
