4b7ac4126a1369b7413113b2d082c39d227fb756
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-06-30 20:26:33 +0200

    use getItemLayout

--- src/screens/calendar/components/EventsList.tsx
+++ src/screens/calendar/components/EventsList.tsx
@@ -6,7 +6,8 @@ import { Box } from 'utils/theme'
 export type EventsListProps = {
   days: DayInfoProps[]
 }
-
+const ITEM_HEIGHT = 63
+const EVENT_HEIGHT = 45
 export const EventsList = React.forwardRef<FlatList, EventsListProps>(({ days }, flatListRef) => (
   <Box marginTop="m" flex={1}>
     <FlatList
@@ -17,8 +18,26 @@ export const EventsList = React.forwardRef<FlatList, EventsListProps>(({ days },
       extraData={days}
       keyExtractor={(item) => item.date}
       initialScrollIndex={0}
-      initialNumToRender={days.length}
-      onScrollToIndexFailed={() => {}}
+      getItemLayout={(data, index) => {
+        if (!data)
+          return {
+            length: ITEM_HEIGHT,
+            offset: ITEM_HEIGHT * index,
+            index,
+          }
+        let prevEventsCount = 0
+        for (let i = 0; i < index; i++) {
+          prevEventsCount += data[i].events?.length ? data[i].events?.length : 0
+        }
+        return {
+          length: ITEM_HEIGHT,
+          offset: ITEM_HEIGHT * index + prevEventsCount * EVENT_HEIGHT,
+          index,
+        }
+      }}
+      onScrollToIndexFailed={(e) => {
+        console.log(e)
+      }}
       ref={flatListRef}
     />
   </Box>
