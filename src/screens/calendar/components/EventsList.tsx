aa3b78c1c991f7a2120a98dcbf9226e23dc1cb16
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-07-01 11:32:38 +0200

    Add weekend height

--- src/screens/calendar/components/EventsList.tsx
+++ src/screens/calendar/components/EventsList.tsx
@@ -6,8 +6,9 @@ import { Box } from 'utils/theme'
 export type EventsListProps = {
   days: DayInfoProps[]
 }
-const ITEM_HEIGHT = 63
-const EVENT_HEIGHT = 45
+const ITEM_HEIGHT = 49.9 + 16
+const WEEKEND_HEIGHT = 50.5 + 5
+const EVENT_HEIGHT = 46
 export const EventsList = React.forwardRef<FlatList, EventsListProps>(({ days }, flatListRef) => (
   <Box marginTop="m" flex={1}>
     <FlatList
@@ -26,12 +27,18 @@ export const EventsList = React.forwardRef<FlatList, EventsListProps>(({ days },
             index,
           }
         let prevEventsCount = 0
+        let prevWeekendsCount = 0
         for (let i = 0; i < index; i++) {
           prevEventsCount += data[i].events?.length ? data[i].events?.length : 0
+          prevWeekendsCount += data[i].weekend ? 1 : 0
         }
         return {
           length: ITEM_HEIGHT,
-          offset: ITEM_HEIGHT * index + prevEventsCount * EVENT_HEIGHT,
+          offset:
+            index * ITEM_HEIGHT +
+            prevEventsCount * EVENT_HEIGHT -
+            prevWeekendsCount * ITEM_HEIGHT +
+            prevWeekendsCount * WEEKEND_HEIGHT,
           index,
         }
       }}
