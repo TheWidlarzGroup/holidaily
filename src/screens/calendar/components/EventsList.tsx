11d89928d98a0977b90d478ba58ffa22d7c4dd5a
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-07-01 14:25:08 +0200

    Fix/events list render (#165)
    * Add more mocked data
    
    * Add animation on day selection
    
    * Improve selected size
    
    * Create own WeekCalendar component
    
    * fix margin
    
    * Delete old code
    
    * After month change switch selected day to first
    
    * use getItemLayout
    
    * Fix margin
    
    * Add weekend height
    
    * Add flatList optimization

--- src/screens/calendar/components/EventsList.tsx
+++ src/screens/calendar/components/EventsList.tsx
@@ -6,7 +6,9 @@ import { Box } from 'utils/theme'
 export type EventsListProps = {
   days: DayInfoProps[]
 }
-
+const ITEM_HEIGHT = 49.9 + 16
+const WEEKEND_HEIGHT = 50.5 + 5
+const EVENT_HEIGHT = 46
 export const EventsList = React.forwardRef<FlatList, EventsListProps>(({ days }, flatListRef) => (
   <Box marginTop="m" flex={1}>
     <FlatList
@@ -17,8 +19,31 @@ export const EventsList = React.forwardRef<FlatList, EventsListProps>(({ days },
       extraData={days}
       keyExtractor={(item) => item.date}
       initialScrollIndex={0}
-      initialNumToRender={days.length}
+      getItemLayout={(data, index) => {
+        if (!data)
+          return {
+            length: ITEM_HEIGHT,
+            offset: ITEM_HEIGHT * index,
+            index,
+          }
+        let prevEventsCount = 0
+        let prevWeekendsCount = 0
+        for (let i = 0; i < index; i++) {
+          prevEventsCount += data[i].events?.length ? data[i].events?.length : 0
+          prevWeekendsCount += data[i].weekend ? 1 : 0
+        }
+        return {
+          length: ITEM_HEIGHT,
+          offset:
+            index * ITEM_HEIGHT +
+            prevEventsCount * EVENT_HEIGHT -
+            prevWeekendsCount * ITEM_HEIGHT +
+            prevWeekendsCount * WEEKEND_HEIGHT,
+          index,
+        }
+      }}
       onScrollToIndexFailed={() => {}}
+      windowSize={5}
       ref={flatListRef}
     />
   </Box>
