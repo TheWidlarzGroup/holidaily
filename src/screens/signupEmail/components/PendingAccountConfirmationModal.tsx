04078efaef84d83a2408e0db27297502ca1cde0e
Magda <jaskowska.magda@gmail.com>
2021-07-01 12:01:55 +0100

    add confetti to successfull signup

--- src/screens/signupEmail/components/PendingAccountConfirmationModal.tsx
+++ src/screens/signupEmail/components/PendingAccountConfirmationModal.tsx
@@ -5,6 +5,7 @@ import { ModalProps } from 'react-native-modal'
 import { CustomModal } from 'components/CustomModal'
 import { theme } from 'utils/theme/index'
 import { colors } from 'utils/theme/colors'
+import { Confetti } from 'components/Confetti'
 import { FirstRegisterDialogBox } from './FirstRegisterDialogBox'
 import { SecondRegisterDialogBox } from './SecondRegisterDialogBox'
 
@@ -26,28 +27,35 @@ export const PendingAccountConfirmationModal: FC<PendingAccountConfModalProps> =
   }, [isConfirmed, showModal])
 
   return (
-    <CustomModal
-      isVisible={isVisible}
-      onBackButtonPress={hideModal}
-      onBackdropPress={hideModal}
-      backdropColor={colors.white}
-      animationInTiming={500}
-      animationOutTiming={500}
-      backdropTransitionInTiming={500}
-      backdropTransitionOutTiming={500}
-      backdropOpacity={0.8}
-      style={styles.modal}
-      hideModalContentWhileAnimating>
-      {!isConfirmed ? (
-        <FirstRegisterDialogBox />
-      ) : (
-        <SecondRegisterDialogBox hideModal={hideModal} />
-      )}
-    </CustomModal>
+    <>
+      <CustomModal
+        isVisible={isVisible}
+        onBackButtonPress={hideModal}
+        onBackdropPress={hideModal}
+        backdropColor={colors.white}
+        animationInTiming={500}
+        animationOutTiming={500}
+        backdropTransitionInTiming={500}
+        backdropTransitionOutTiming={500}
+        backdropOpacity={0.8}
+        style={styles.modal}
+        hideModalContentWhileAnimating>
+        {!isConfirmed ? (
+          <FirstRegisterDialogBox />
+        ) : (
+          <>
+            <Confetti origin={{ x: -40, y: -10 }} />
+            <SecondRegisterDialogBox hideModal={hideModal} />
+          </>
+        )}
+      </CustomModal>
+    </>
   )
 }
 const styles = StyleSheet.create({
   modal: {
-    marginHorizontal: theme.spacing.l,
+    flex: 1,
+    paddingHorizontal: theme.spacing.l,
+    justifyContent: 'center',
   },
 })
