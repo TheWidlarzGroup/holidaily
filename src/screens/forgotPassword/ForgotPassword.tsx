94fc0600746c797276b48b0bb04e6b4fcb1ed816
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-07-02 11:52:17 +0200

    Fix conflict

--- src/screens/forgotPassword/ForgotPassword.tsx
+++ src/screens/forgotPassword/ForgotPassword.tsx
@@ -51,6 +51,7 @@ export const ForgotPassword: FC = () => {
               isError={!!errors.email}
               name="email"
               inputLabel={t('email')}
+              autoCapitalize="none"
               validationPattern={emailRegex}
               errorMessage={t('incorrectEmail')}
             />
