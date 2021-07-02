a300e7df6f50346b0f970c0257399a2d7d440dcb
KacperM717 <kacpermikolajczak10@gmail.com>
2021-07-02 14:21:08 +0200

    Add MessageInput to FooterBar

--- src/screens/feed/components/FooterBar/FooterBar.tsx
+++ src/screens/feed/components/FooterBar/FooterBar.tsx
@@ -6,6 +6,8 @@ import IconReaction from 'assets/icons/icon-reaction.svg'
 import { Reaction } from 'screens/feed/types'
 import { Box, Text } from 'utils/theme'
 import { useTranslation } from 'react-i18next'
+import { useBooleanState } from 'hooks/useBooleanState'
+import { MessageInput } from 'components/MessageInput'
 import { Bubble, BubbleProps } from '../Bubble/Bubble'
 import { ReactionBubble } from '../Bubble/ReactionBubble'
 
@@ -14,26 +16,42 @@ type FooterBarProps = {
 }
 
 export const FooterBar = ({ reactions }: FooterBarProps) => {
+  const [displayCommentInput, { setTrue: showCommentInput, setFalse: hideCommentInput }] =
+    useBooleanState(false)
   const { t } = useTranslation('feed')
   return (
-    <Box flexDirection="row" padding="s" justifyContent="space-between" alignItems="flex-start">
-      <Box
-        flexDirection="row"
-        flexWrap="wrap"
-        justifyContent="flex-start"
-        flexGrow={1}
-        flexShrink={1}>
-        <ReactionPickerBtn onPress={() => {}} />
-        {reactions &&
-          reactions.map(({ type, users }) => (
-            <ReactionBubble key={type} emoji={type} quantity={users.length} selected={false} />
-          ))}
+    <>
+      <Box flexDirection="row" padding="s" justifyContent="space-between" alignItems="flex-start">
+        <Box
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="flex-start"
+          flexGrow={1}
+          flexShrink={1}>
+          <ReactionPickerBtn onPress={() => {}} />
+          {reactions &&
+            reactions.map(({ type, users }) => (
+              <ReactionBubble key={type} emoji={type} quantity={users.length} selected={false} />
+            ))}
+        </Box>
+        <Bubble padding="s" onPress={showCommentInput}>
+          <Box padding="xs">
+            <IconComment />
+          </Box>
+          <Text padding="xs">{t('postCommentBtn')}</Text>
+        </Bubble>
       </Box>
-      <Bubble padding="s">
-        <IconComment />
-        <Text padding="xs">{t('postCommentBtn')}</Text>
-      </Bubble>
-    </Box>
+      {displayCommentInput && (
+        <MessageInput
+          autofocus
+          defaultValue=""
+          onSubmitEditing={(x) => {
+            console.log(x)
+          }}
+          onBlur={hideCommentInput}
+        />
+      )}
+    </>
   )
 }
 
