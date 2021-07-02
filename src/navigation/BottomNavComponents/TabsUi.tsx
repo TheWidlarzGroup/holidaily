b153b944b38044180407c94fa34d9cfd4b6084ab
KacperM717 <kacpermikolajczak10@gmail.com>
2021-07-02 10:59:51 +0200

    Remove isIos import

--- src/navigation/BottomNavComponents/TabsUi.tsx
+++ src/navigation/BottomNavComponents/TabsUi.tsx
@@ -2,7 +2,6 @@ import React, { FC, useMemo } from 'react'
 import { Dimensions } from 'react-native'
 import { NavigationState } from '@react-navigation/native'
 import { Box } from 'utils/theme'
-import { isIos } from 'utils/layout'
 import { NavigationDot } from './NavigationDot'
 import { TabsHandler } from './TabsHandler'
 
