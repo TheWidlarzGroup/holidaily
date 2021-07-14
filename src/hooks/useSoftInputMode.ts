d538c164e7b7b7ed0bf14d8d13925c9dca491734
skhyr <piotrmal2002@gmail.com>
2021-07-14 21:43:03 +0200

    code review

--- src/hooks/useSoftInputMode.ts
+++ src/hooks/useSoftInputMode.ts
@@ -1,26 +1,18 @@
 import { useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { isIos } from 'utils/layout'
+import { isAndroid } from 'utils/layout'
 import SoftInputMode, { SoftInputModes, defaultInputMode } from '../modules/SoftInputMode'
 
 export const useSoftInputMode = (mode: SoftInputModes) => {
   const { addListener } = useNavigation()
   useEffect(() => {
-    const handleFocus = () => {
-      if (!isIos) {
-        SoftInputMode.set(mode)
-      }
-    }
-    const handleBlur = () => {
-      if (!isIos) {
-        SoftInputMode.set(defaultInputMode)
-      }
-    }
-    const onFocus = addListener('focus', handleFocus)
-    const onBlur = addListener('blur', handleBlur)
+    const handleFocus = () => isAndroid && SoftInputMode.set(mode)
+    const handleBlur = () => isAndroid && SoftInputMode.set(defaultInputMode)
+    const removeFocusListener = addListener('focus', handleFocus)
+    const removeBlurListener = addListener('blur', handleBlur)
     return () => {
-      onFocus()
-      onBlur()
+      removeBlurListener()
+      removeFocusListener()
     }
   }, [addListener, mode])
 }
