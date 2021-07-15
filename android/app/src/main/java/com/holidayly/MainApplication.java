cd92df45fea89597096d58dee2cc8d22c718192b
mateki0 <mateki0@interia.pl>
2021-07-15 10:03:17 +0200

    main application java changed

--- android/app/src/main/java/com/holidayly/MainApplication.java
+++ android/app/src/main/java/com/holidayly/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import li.yunqi.rnsecurestorage.RNSecureStoragePackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
