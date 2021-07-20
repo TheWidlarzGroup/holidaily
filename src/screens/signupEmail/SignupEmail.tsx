46fe4e33e5f4a490fae473172adfd12b797d434f
KacperM <kacpermikolajczak10@gmail.com>
2021-07-20 16:48:16 +0200

    Fix signupEmail screen scrolling.

--- src/screens/signupEmail/SignupEmail.tsx
+++ src/screens/signupEmail/SignupEmail.tsx
@@ -1,12 +1,11 @@
 import React, { useCallback, useEffect, useRef } from 'react'
-import { StyleSheet, KeyboardAvoidingView, TextInput, ScrollView } from 'react-native'
+import { TextInput, ScrollView } from 'react-native'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { useFocusEffect } from '@react-navigation/native'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
-import { Box, Text, theme } from 'utils/theme/index'
+import { Box, mkUseStyles, Text, Theme } from 'utils/theme/index'
 import { shadow } from 'utils/theme/shadows'
-import { isIos } from 'utils/layout'
 import { minTwoWordsRegex, minOneSignRegex, emailRegex, passwordRegex } from 'utils/regex'
 import { FormInput } from 'components/FormInput'
 import { CustomButton } from 'components/CustomButton'
@@ -21,6 +20,8 @@ export const SignupEmail = () => {
   const { control, handleSubmit, errors } = useForm()
   const { t } = useTranslation('signupEmail')
   const inputsRefs = [useRef<TextInput>(null), useRef<TextInput>(null), useRef<TextInput>(null)]
+
+  const styles = useStyles()
 
   const onSubmitEditing = (index: number) => {
     inputsRefs[index]?.current?.focus()
@@ -41,68 +42,66 @@ export const SignupEmail = () => {
       <Box flex={0.2} justifyContent="center">
         <Text variant="title1">{t('signupEmailTitle')}</Text>
       </Box>
-      <KeyboardAvoidingView behavior={isIos ? 'padding' : 'height'} style={styles.keyboardAvoiding}>
-        <ScrollView style={{ marginHorizontal: theme.spacing.l }}>
-          <Box>
-            <FormInput
-              control={control}
-              isError={!!errors.nameSurname}
-              errors={errors}
-              name="nameSurname"
-              inputLabel={t('nameSurname')}
-              validationPattern={minTwoWordsRegex}
-              errorMessage={t('nameSurnameErrMsg')}
-              onSubmitEditing={() => onSubmitEditing(0)}
-              blurOnSubmit={false}
-            />
-          </Box>
-          <Box>
-            <FormInput
-              control={control}
-              isError={!!errors.companyName}
-              errors={errors}
-              name="companyName"
-              inputLabel={t('companyName')}
-              validationPattern={minOneSignRegex}
-              errorMessage={t('nameSurnameErrMsg')}
-              onSubmitEditing={() => onSubmitEditing(1)}
-              blurOnSubmit={false}
-              ref={inputsRefs[0]}
-            />
-          </Box>
-          <Box>
-            <FormInput
-              control={control}
-              isError={!!errors.email}
-              errors={errors}
-              name="email"
-              inputLabel={t('email')}
-              validationPattern={emailRegex}
-              errorMessage={t('invalidEmailErr')}
-              onSubmitEditing={() => onSubmitEditing(2)}
-              keyboardType="email-address"
-              autoCompleteType="email"
-              autoCapitalize="none"
-              blurOnSubmit={false}
-              ref={inputsRefs[1]}
-            />
-          </Box>
-          <Box>
-            <FormInput
-              control={control}
-              isError={!!errors.password}
-              errors={errors}
-              name="password"
-              inputLabel={t('password')}
-              validationPattern={passwordRegex}
-              errorMessage={t('nameSurnameErrMsg')}
-              ref={inputsRefs[2]}
-              signupPasswordHint={t('passwordHint')}
-              isPasswordIconVisible
-            />
-          </Box>
-        </ScrollView>
-      </KeyboardAvoidingView>
+      <ScrollView style={styles.scrollView}>
+        <Box>
+          <FormInput
+            control={control}
+            isError={!!errors.nameSurname}
+            errors={errors}
+            name="nameSurname"
+            inputLabel={t('nameSurname')}
+            validationPattern={minTwoWordsRegex}
+            errorMessage={t('nameSurnameErrMsg')}
+            onSubmitEditing={() => onSubmitEditing(0)}
+            blurOnSubmit={false}
+          />
+        </Box>
+        <Box>
+          <FormInput
+            control={control}
+            isError={!!errors.companyName}
+            errors={errors}
+            name="companyName"
+            inputLabel={t('companyName')}
+            validationPattern={minOneSignRegex}
+            errorMessage={t('nameSurnameErrMsg')}
+            onSubmitEditing={() => onSubmitEditing(1)}
+            blurOnSubmit={false}
+            ref={inputsRefs[0]}
+          />
+        </Box>
+        <Box>
+          <FormInput
+            control={control}
+            isError={!!errors.email}
+            errors={errors}
+            name="email"
+            inputLabel={t('email')}
+            validationPattern={emailRegex}
+            errorMessage={t('invalidEmailErr')}
+            onSubmitEditing={() => onSubmitEditing(2)}
+            keyboardType="email-address"
+            autoCompleteType="email"
+            autoCapitalize="none"
+            blurOnSubmit={false}
+            ref={inputsRefs[1]}
+          />
+        </Box>
+        <Box>
+          <FormInput
+            control={control}
+            isError={!!errors.password}
+            errors={errors}
+            name="password"
+            inputLabel={t('password')}
+            validationPattern={passwordRegex}
+            errorMessage={t('nameSurnameErrMsg')}
+            ref={inputsRefs[2]}
+            signupPasswordHint={t('passwordHint')}
+            isPasswordIconVisible
+          />
+        </Box>
+      </ScrollView>
       <Box
         position="absolute"
         right={0}
@@ -131,8 +130,9 @@ export const SignupEmail = () => {
   )
 }
 
-const styles = StyleSheet.create({
-  keyboardAvoiding: {
+const useStyles = mkUseStyles((theme: Theme) => ({
+  scrollView: {
+    marginHorizontal: theme.spacing.l,
     flex: 1,
   },
-})
+}))
