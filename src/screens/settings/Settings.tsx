8592388667fd06ea574d609720869c2590732836
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-07-21 09:08:24 +0200

    create temp add button

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -7,10 +7,23 @@ import { DrawerBackArrow } from 'components/DrawerBackArrow'
 import { useTranslation } from 'react-i18next'
 import { useBooleanState } from 'hooks/useBooleanState'
 import { LoadingModal } from 'components/LoadingModal'
+import AddToSiriButton, { SiriButtonStyles } from 'react-native-siri-shortcut/AddToSiriButton'
+import { presentShortcut, ShortcutOptions } from 'react-native-siri-shortcut'
 import { Language } from './components/Language'
 import { DarkModeSwitch } from './components/DarkModeSwitch'
 import { BiometricPasscode } from './components/BiometricPasscode'
 
+const opts: ShortcutOptions = {
+  activityType: 'com.holidaily.AddRequest', // This activity type needs to be set in `NSUserActivityTypes` on the Info.plist
+  title: 'Take sickday',
+  isEligibleForSearch: true,
+  isEligibleForPrediction: true,
+  suggestedInvocationPhrase: 'Take sickday',
+  userInfo: {
+    action: 'sickday',
+  },
+  needsSave: true,
+}
 export const Settings = () => {
   const navigation = useNavigation<DrawerNavigationType<'Settings'>>()
   const [loading, { setTrue: setLoadingTrue, setFalse: setLoadingFalse }] = useBooleanState(false)
@@ -33,6 +46,16 @@ export const Settings = () => {
         <DarkModeSwitch />
         <BiometricPasscode />
         <Language setLoadingFalse={setLoadingFalse} setLoadingTrue={setLoadingTrue} />
+        <AddToSiriButton
+          shortcut={opts}
+          style={{ flex: 1 }}
+          buttonStyle={SiriButtonStyles.whiteOutline}
+          onPress={() => {
+            presentShortcut(opts, ({ status }) => {
+              console.log(`I was ${status}`)
+            })
+          }}
+        />
       </Box>
       <LoadingModal show={loading} />
     </SafeAreaWrapper>
