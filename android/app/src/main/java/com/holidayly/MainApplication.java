94adf77c9734cdb1f62c53d51c3e505b404859fa
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-07-22 16:38:11 +0200

    Feature/install expo libs (#209)
    * Install unimodules ios
    
    * configure android
    
    * install secure storage
    
    * Install expo location

--- android/app/src/main/java/com/holidayly/MainApplication.java
+++ android/app/src/main/java/com/holidayly/MainApplication.java
@@ -1,5 +1,7 @@
 package com.holidayly;
 
+import com.holidayly.generated.BasePackageList;
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -11,11 +13,17 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Arrays;
+ 
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.SingletonModule;
 
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
   private final ReactNativeHost mReactNativeHost =
     new ReactNativeHost(this) {
@@ -31,6 +39,12 @@ public class MainApplication extends Application implements ReactApplication {
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
         packages.add(new SoftInputModePackage());
+         
+        // Add unimodules
+        List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+          new ModuleRegistryAdapter(mModuleRegistryProvider)
+        );
+        packages.addAll(unimodules);
         return packages;
       }
 
