dc836cb54c57bfe80073bcc8ef40ca195b8ccac5
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-07-22 14:18:55 +0200

    Install expo location

--- android/app/src/main/java/com/holidayly/generated/BasePackageList.java
+++ android/app/src/main/java/com/holidayly/generated/BasePackageList.java
@@ -9,7 +9,9 @@ public class BasePackageList {
     return Arrays.<Package>asList(
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
-        new expo.modules.imageloader.ImageLoaderPackage()
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.location.LocationPackage(),
+        new expo.modules.securestore.SecureStorePackage()
     );
   }
 }
