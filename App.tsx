38808b3bc848839d3eeaf98ff43b8032ae6db882
KacperM <kacpermikolajczak10@gmail.com>
2021-07-28 00:08:25 +0200

    move OneSignal init to hook

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import { useOneSignal } from 'hooks/useOneSignal'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Main } from './src/Main'
 
 const queryClient = new QueryClient()
 
-export const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <Main />
-  </QueryClientProvider>
-)
+export const App = () => {
+  useOneSignal()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Main />
+    </QueryClientProvider>
+  )
+}
--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import { AppRegistry } from 'react-native'
 import { App } from './App'
 import { name as appName } from './app.json'
 import './i18n'
-import './onesignal'
 
 AppRegistry.registerComponent(appName, () => App)
