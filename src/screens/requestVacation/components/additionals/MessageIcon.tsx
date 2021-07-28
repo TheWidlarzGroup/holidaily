16cb7b6eb7a92ee37f01312666c3dca6278248cb
Piotr Malatyński <piotrmal2002@gmail.com>
2021-07-28 12:59:05 +0200

    Feature/add request attachment (#181)
    * attachment
    
    * fixed: additionals display order
    
    * responsive attachments
    
    * fixed AddMore positioning
    
    * photos in summary
    
    * lint
    
    * remove photo
    
    * separate components
    
    * fixed additionals display order
    
    * fixed photos padding
    
    * icon
    
    * fixed overflow issue
    
    * rounded prgress bar
    
    * remove photo button hidden on summary
    
    * remove photo confirmation
    
    * lint
    
    * cr
    
    * cr

---
+++ src/screens/requestVacation/components/additionals/MessageIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Box } from 'utils/theme'
+import AddCommentIcon from 'assets/icons/addComment.svg'
+
+type MessageIconProps = {
+  onPress: F0
+}
+
+export const MessageIcon = ({ onPress }: MessageIconProps) => (
+  <Box backgroundColor="lightGrey" padding="xm" margin="s" marginLeft={0} borderRadius="l">
+    <TouchableOpacity onPress={onPress}>
+      <AddCommentIcon width={22} height={22} />
+    </TouchableOpacity>
+  </Box>
+)
