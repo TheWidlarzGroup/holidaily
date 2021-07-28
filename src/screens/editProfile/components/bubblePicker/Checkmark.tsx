ae9b43f2281ac27aac3a491e324367c2ff3d6472
Magda <jaskowska.magda@gmail.com>
2021-07-28 17:37:47 +0100

    Move Checkmark to separate file

---
+++ src/screens/editProfile/components/bubblePicker/Checkmark.tsx
@@ -0,0 +1,44 @@
+import { useWindowDimensions } from 'react-native'
+import Checkmark from 'components/Checkmark'
+import { Box, mkUseStyles, theme } from 'utils/theme'
+import { CONSTANTS as C } from './BubbleHelper'
+
+type CheckMarkProps = {
+  animateCheckmark: boolean
+}
+
+export const CheckMark = ({ animateCheckmark }: CheckMarkProps) => {
+  const styles = useStyles()
+  const { width, height } = useWindowDimensions()
+  const checkmarkCenter = {
+    x: width / 2 - C.CHECKMARK_BOX_SIZE / 2,
+    y: height / 2 - C.CHECKMARK_BOX_SIZE / 2,
+  }
+
+  return (
+    <Box
+      style={styles.scaleCheckmark}
+      position="absolute"
+      top={checkmarkCenter.y}
+      left={checkmarkCenter.x}
+      width={C.CHECKMARK_BOX_SIZE}
+      height={C.CHECKMARK_BOX_SIZE}
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="transparent"
+      zIndex="10">
+      <Checkmark
+        width={10}
+        start={animateCheckmark}
+        color={theme.colors.white}
+        onFinish={() => {}}
+      />
+    </Box>
+  )
+}
+
+const useStyles = mkUseStyles(() => ({
+  scaleCheckmark: {
+    transform: [{ scale: 2 }],
+  },
+}))
