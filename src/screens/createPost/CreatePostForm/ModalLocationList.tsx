4d5009656518df1aa94189c915f81f12e958473f
KacperM <kacpermikolajczak10@gmail.com>
2021-07-29 23:04:50 +0200

    Add ModalLocationList component.

---
+++ src/screens/createPost/CreatePostForm/ModalLocationList.tsx
@@ -0,0 +1,49 @@
+import { CompoundLocation } from 'hooks/useLocation'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { BaseOpacity, Box, Text } from 'utils/theme'
+
+type ModalLocationListProps = {
+  loading: boolean
+  locations: Maybe<CompoundLocation[]>
+  onLocationPress: F1<CompoundLocation>
+}
+
+export const ModalLocationList = (props: ModalLocationListProps) => {
+  const { loading, locations, onLocationPress } = props
+
+  const { t } = useTranslation('feed')
+
+  return (
+    <Box paddingHorizontal="xs">
+      {loading && (
+        <Box alignItems="center" padding="m">
+          <Text>{t('loading')}</Text>
+        </Box>
+      )}
+      {!loading &&
+        locations &&
+        (locations.length > 0 ? (
+          locations.map((location) =>
+            location.addresses.map((address) => (
+              <BaseOpacity
+                onPress={() => onLocationPress(location)}
+                paddingVertical="m"
+                key={address.name}>
+                <Text variant="regular15">
+                  {address.name}, {address.city}
+                </Text>
+                <Text variant="labelGrey">
+                  {address.region}, {address.country}
+                </Text>
+              </BaseOpacity>
+            ))
+          )
+        ) : (
+          <Box alignItems="center" padding="m">
+            <Text>{t('locationNotFound')}</Text>
+          </Box>
+        ))}
+    </Box>
+  )
+}
