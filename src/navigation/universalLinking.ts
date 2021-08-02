832641aebc0d8477be5697851bb375423070a1a7
KacperM <kacpermikolajczak10@gmail.com>
2021-08-02 15:19:06 +0200

    Add linking to screens for OneSignal

--- src/navigation/universalLinking.ts
+++ src/navigation/universalLinking.ts
@@ -2,10 +2,26 @@ const config = {
   screens: {
     ConfirmedAccount: '/confirm/:token',
     RecoveryCode: '/password_reset/:code',
+    RequestVacation: '/request_vacation',
+    DrawerNavigator: {
+      screens: {
+        Home: {
+          screens: {
+            Stats: '/stats',
+            DashboardNavigation: {
+              screens: {
+                Dashboard: '/dashboard',
+                // DashboardTeam: '/team:id',
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
 
 export const linking = {
-  prefixes: ['https://holidaily.danielgrychtol.com', 'holidaily://open'],
+  prefixes: ['https://holidaily.danielgrychtol.com', 'holidaily://'],
   config,
 }
