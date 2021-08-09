03fca64e65b4bae7bc2e979c6fd22f311b3e5499
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-08-09 17:26:07 +0200

    Add role to mutation and screen

--- src/graphql/mutations/createInvitationMutation.ts
+++ src/graphql/mutations/createInvitationMutation.ts
@@ -2,11 +2,11 @@ import { gql } from 'graphql-request'
 import { client } from 'graphqlActions/client'
 import { CreateInvitationTypes } from 'types/useCreateInvitationTypes'
 
-export const createInviationMutation = ({ email }: CreateInvitationTypes) =>
+export const createInviationMutation = ({ email, role }: CreateInvitationTypes) =>
   client.request(
     gql`
     mutation{
-      createInvitation(email: "${email}") {
+      createInvitation(email: "${email}", role: ${role}) {
         code
         email
         expired
