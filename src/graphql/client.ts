058320f0bcbacc4974d226f386b6c97c2cc57e60
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-08-10 17:18:11 +0200

    Fix / fetch user data error (#244)
    * Prevent sending fetch when token is not available
    
    * Add Bearer only when token is available

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -6,5 +6,7 @@ const getToken = async () => getItemAsync('token')
 
 export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
 getToken().then((token) => {
-  client.setHeader('Authorization', `Bearer ${token}`)
+  if (token) {
+    client.setHeader('Authorization', `Bearer ${token}`)
+  }
 })
