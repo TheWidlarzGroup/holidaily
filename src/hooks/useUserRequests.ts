905efd6ad234cdfbd443011723f67e744826d9e2
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-08-11 19:57:53 +0200

    Create useUserRequests

---
+++ src/hooks/useUserRequests.ts
@@ -0,0 +1,15 @@
+import { useQuery } from 'react-query'
+import { useState } from 'react'
+import { userRequestsQuery } from 'graphqlActions/queries/userRequestsQuery'
+import { RequestsQueryTypes, RequestTypes } from 'types/useUserRequestsTypes'
+
+export const useUserRequests = () => {
+  const [requests, setRequests] = useState<RequestTypes[]>([])
+  const { isLoading, error } = useQuery('fetch-user-requests', userRequestsQuery, {
+    onSuccess: (data: RequestsQueryTypes) => {
+      setRequests(data.requests)
+    },
+  })
+
+  return { requests, isLoading, error }
+}
