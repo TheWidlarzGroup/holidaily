6afaacc6e40ce71f1f3d65ab17a117bb3b023d66
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-08-12 11:01:55 +0200

    create useResetPassword

---
+++ src/hooks/useResetPassword.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { useMutation } from 'react-query'
+
+import { useTranslation, TFunction } from 'react-i18next'
+import { ErrorTypes, ResetPasswordDataTypes, ResetPasswordTypes } from 'types/useResetPasswordTypes'
+import { resetPasswordMutation } from 'graphqlActions/mutations/resetPasswordMutation'
+
+const customErrorMessage = (translate: TFunction<'mutationsErrors'>, errorMessage: string) => {
+  if (errorMessage?.startsWith('invalid_code')) {
+    return translate('invalidCredentials')
+  }
+  return translate('default')
+}
+
+export const useResetPassword = () => {
+  const [errorMessage, setErrorMessage] = useState('')
+  const { t } = useTranslation('mutationsErrors')
+  const {
+    mutate: handleResetPassword,
+    isLoading,
+    isSuccess,
+  } = useMutation<ResetPasswordDataTypes, ErrorTypes, ResetPasswordTypes>(resetPasswordMutation, {
+    onSuccess: (data: ResetPasswordDataTypes) => {
+      console.log(data)
+    },
+    onError: (error: ErrorTypes) => {
+      setErrorMessage(customErrorMessage(t, error.message))
+    },
+  })
+
+  return { handleResetPassword, isLoading, errorMessage, isSuccess }
+}
