1000d9115dd9804e786499326fc367a440d222a9
KacperM <kacpermikolajczak10@gmail.com>
2021-08-16 08:47:55 +0200

    add SummaryDays translation

--- src/screens/requestVacation/components/SummaryDays.tsx
+++ src/screens/requestVacation/components/SummaryDays.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Box, Text } from 'utils/theme/index'
 
-export const SummaryDays = () => (
-  <Box flexDirection="row" justifyContent="space-around" alignItems="center">
-    <Box alignItems="center">
-      <Text variant="captionText">YOU'RE TAKING</Text>
-      <Text variant="heading1">3</Text>
-      <Text variant="captionText">DAYS OF PTO</Text>
+export const SummaryDays = () => {
+  const { t } = useTranslation('requestVacation')
+
+  return (
+    <Box flexDirection="row" justifyContent="space-around" alignItems="center">
+      <Box alignItems="center">
+        <Text variant="captionText">{t('ptoTaken1').toUpperCase()}</Text>
+        <Text variant="heading1">3</Text>
+        <Text variant="captionText">{t('ptoTaken2').toUpperCase()}</Text>
+      </Box>
+      <Box alignItems="center">
+        <Text variant="captionText">{t('ptoLeft1').toUpperCase()}</Text>
+        <Text variant="heading1">19</Text>
+        <Text variant="captionText">{t('ptoLeft2').toUpperCase()}</Text>
+      </Box>
     </Box>
-    <Box alignItems="center">
-      <Text variant="captionText">YOU'LL HAVE</Text>
-      <Text variant="heading1">19</Text>
-      <Text variant="captionText">DAYS OF PTO LEFT</Text>
-    </Box>
-  </Box>
-)
+  )
+}
