4b390e10d9f98d52b1f4f51f3971658ecb00ec26
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2021-08-16 14:05:45 +0200

    Fix / authorized client (#263)
    * Change client to authorizedClient
    
    * Authorize client on login and reopen app + delete auth token when logout

--- src/graphql/mutations/createInvitationMutation.ts
+++ src/graphql/mutations/createInvitationMutation.ts
@@ -1,9 +1,9 @@
 import { gql } from 'graphql-request'
-import { client } from 'graphqlActions/client'
+import { authorizedClient } from 'graphqlActions/client'
 import { CreateInvitationTypes } from 'types/useCreateInvitationTypes'
 
 export const createInviationMutation = ({ email, role }: CreateInvitationTypes) =>
-  client.request(
+  authorizedClient.request(
     gql`
     mutation{
       createInvitation(email: "${email}", role: ${role}) {
