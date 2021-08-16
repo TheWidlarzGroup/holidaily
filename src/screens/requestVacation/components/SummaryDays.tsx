f68d667986e29ff224eaeba6b67e0f506fa7a818
KacperM <kacpermikolajczak10@gmail.com>
2021-08-16 10:09:58 +0200

    fix SummaryDays uppercase

--- src/screens/requestVacation/components/SummaryDays.tsx
+++ src/screens/requestVacation/components/SummaryDays.tsx
@@ -8,15 +8,21 @@ export const SummaryDays = () => {
   return (
     <Box flexDirection="row" justifyContent="space-around" alignItems="center">
       <Box alignItems="center">
-        <Text variant="captionText">{t('ptoTaken1').toUpperCase()}</Text>
+        <SummaryDaysText>{t('ptoTaken1')}</SummaryDaysText>
         <Text variant="heading1">3</Text>
-        <Text variant="captionText">{t('ptoTaken2').toUpperCase()}</Text>
+        <SummaryDaysText>{t('ptoTaken2')}</SummaryDaysText>
       </Box>
       <Box alignItems="center">
-        <Text variant="captionText">{t('ptoLeft1').toUpperCase()}</Text>
+        <SummaryDaysText>{t('ptoLeft1')}</SummaryDaysText>
         <Text variant="heading1">19</Text>
-        <Text variant="captionText">{t('ptoLeft2').toUpperCase()}</Text>
+        <SummaryDaysText>{t('ptoLeft2')}</SummaryDaysText>
       </Box>
     </Box>
   )
 }
+
+const SummaryDaysText: React.FC = (props) => (
+  <Text variant="captionText" textTransform="uppercase">
+    {props.children}
+  </Text>
+)
