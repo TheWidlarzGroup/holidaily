9e1838ca73161b2128724fb3fda4e5a68f45be79
Lukasz Baranowski <lukaszb48@gmail.com>
2022-03-11 17:57:21 +0100

    Added double quote character

--- src/screens/dashboard/components/TeamHeader.tsx
+++ src/screens/dashboard/components/TeamHeader.tsx
@@ -23,7 +23,7 @@ export const TeamHeader = (props: TeamHeaderProps) => {
         <IconBack />
       </TouchableOpacity>
       <Text variant="header">{title}</Text>
-      <Box paddingRight='xl' />
+      <Box paddingRight="xl" />
     </Box>
   )
 }
