54dd2a053044f3754cb574e6fbbdffe76effbcc0
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-11 19:05:31 +0100

    fix header text, add placeholder for date

--- src/screens/dashboard/components/DashboardHeader.tsx
+++ src/screens/dashboard/components/DashboardHeader.tsx
@@ -4,12 +4,13 @@ import { useTranslation } from 'react-i18next'
 import IconProfile from 'assets/icons/icon-profile.svg'
 import IconBell from 'assets/icons/icon-bell.svg'
 import { useNavigation, DrawerActions } from '@react-navigation/native'
+import { useUserContext } from 'hooks/useUserContext'
+import { getDayName } from 'utils/dates'
 
 export const DashboardHeader: FC = () => {
   const { t } = useTranslation('dashboard')
-
   const navigation = useNavigation()
-
+  const { user } = useUserContext()
   return (
     <Box marginVertical="m" flexDirection="row" justifyContent="space-between" alignItems="center">
       <Box flexDirection="row" alignItems="center" justifyContent="flex-start">
@@ -22,9 +23,12 @@ export const DashboardHeader: FC = () => {
           borderBottomRightRadius="lplus">
           <IconProfile width={50} height={50} />
         </BaseOpacity>
-        <Text variant="boldBlack18" paddingLeft="xm">
-          {t('welcome')}
-        </Text>
+        <Box alignItems="center" flex={1}>
+          <Text variant="boldBlack18">{t('welcome', { name: user?.firstName })}</Text>
+          <Text variant="lightGreyRegular" lineHeight={14}>
+            {getDayName(new Date())}
+          </Text>
+        </Box>
       </Box>
       <BaseOpacity padding="m">
         <IconBell />
