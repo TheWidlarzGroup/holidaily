862ee4dfdb648a8d8680dda2842296601cac4104
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-14 12:37:45 +0100

    change BudgetNavigation mode to transparent modal

--- src/navigation/BudgetNavigation.tsx
+++ src/navigation/BudgetNavigation.tsx
@@ -8,13 +8,27 @@ const BudgetStack = createStackNavigator<BudgetRoutes>()
 
 export const BudgetNavigation = () => (
   <BudgetStack.Navigator
-    headerMode="none"
-    initialRouteName="Budget"
-    mode="modal"
     screenOptions={{
-      ...TransitionPresets.ModalPresentationIOS,
-      animationEnabled: true,
-    }}>
+      headerShown: false,
+      cardStyle: { backgroundColor: 'transparent' },
+      cardOverlayEnabled: true,
+      cardStyleInterpolator: ({ current: { progress } }) => ({
+        cardStyle: {
+          opacity: progress.interpolate({
+            inputRange: [0, 0.5, 0.9, 1],
+            outputRange: [0, 0.25, 0.7, 1],
+          }),
+        },
+        overlayStyle: {
+          opacity: progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 0.5],
+            extrapolate: 'clamp',
+          }),
+        },
+      }),
+    }}
+    mode="modal">
     <BudgetStack.Screen name="Budget" component={Budget} />
 
     <BudgetStack.Screen name="PtoPolicy" component={PtoPolicy} />
