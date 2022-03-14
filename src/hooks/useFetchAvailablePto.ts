6be30344a1dd23957914d8c0ff1a52c8ee8d22d7
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-14 18:13:51 +0100

    fix type

--- src/hooks/useFetchAvailablePto.ts
+++ src/hooks/useFetchAvailablePto.ts
@@ -4,7 +4,7 @@ import { availablePtoQuery } from 'graphqlActions/queries/availablePtoQuery'
 import { FetchAvailablePtoQueryTypes } from 'types/useFetchAvailablePtoTypes'
 
 export const useFetchAvailablePto = () => {
-  const [availablePto, setAvailablePto] = useState<number | null>(null)
+  const [availablePto, setAvailablePto] = useState<number>(0)
   const { isLoading, error } = useQuery('fetch-available-pto', availablePtoQuery, {
     onSuccess: (data: FetchAvailablePtoQueryTypes) => {
       setAvailablePto(data.availableDaysOff)
