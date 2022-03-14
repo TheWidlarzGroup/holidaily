5e6556caa043bd44abc3b5eabb3e2ebc62ab1c74
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-14 17:15:36 +0100

    split requests to sections

--- src/screens/stats/Requests.tsx
+++ src/screens/stats/Requests.tsx
@@ -1,13 +1,15 @@
 import { useUserRequests } from 'hooks/useUserRequests'
 import React from 'react'
-import { FlatList, TouchableOpacity } from 'react-native'
+import { useTranslation } from 'react-i18next'
+import { SectionList, TouchableOpacity } from 'react-native'
 
-import { Box } from 'utils/theme'
+import { Box, Text } from 'utils/theme'
 import { Request } from './components/Request'
 import { SectionHeader } from './components/SectionHeader'
 
 export const Requests = () => {
   const { requests } = useUserRequests()
+  const { t } = useTranslation('score')
   const handleSearch = () => {
     console.log('handleSearch')
   }
@@ -15,12 +17,41 @@ export const Requests = () => {
     console.log('handleFilter')
   }
 
+  const pendingRequests = requests.filter((req) => req.status === 'PENDING')
+  const approvedRequests = requests.filter((req) => req.status === 'APPROVED')
+  const pastRequests = requests.filter((req) => req.status === 'PAST')
+  const currentRequests = requests.filter((req) => req.status === 'NOW')
+
   return (
     <Box marginTop="xxl" flex={1}>
       <SectionHeader text="Requests" onSearch={handleSearch} onFilter={handleFilter} />
-      <FlatList
-        data={requests}
+      <SectionList
+        sections={[
+          {
+            title: 'NOW',
+            data: currentRequests,
+          },
+          {
+            title: t('pendingRequestsHeader'),
+            data: pendingRequests,
+          },
+          {
+            title: t('acceptedRequestsHeader'),
+            data: approvedRequests,
+          },
+          {
+            title: t('pastRequestsHeader'),
+            data: pastRequests,
+          },
+        ]}
         keyExtractor={({ id }) => id}
+        renderSectionHeader={({ section: { title, data } }) =>
+          data.length ? (
+            <Text variant="lightGreyRegular" margin="xm">
+              {title}
+            </Text>
+          ) : null
+        }
         renderItem={({ item }) => (
           <TouchableOpacity activeOpacity={1}>
             <Request item={item} />
