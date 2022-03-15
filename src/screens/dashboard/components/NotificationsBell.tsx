41c01ac0f433fe14a323156f9a079075905375ae
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-15 17:02:19 +0100

    add notification icon

---
+++ src/screens/dashboard/components/NotificationsBell.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import IconBell from 'assets/icons/icon-bell.svg'
+import { BaseOpacity, Text, Box } from 'utils/theme'
+import { useNavigation } from '@react-navigation/native'
+
+export const NotificationsBell = ({ unseenCount }: { unseenCount: number }) => {
+  const navigation = useNavigation()
+  return (
+    <BaseOpacity
+      padding="m"
+      height={45}
+      width={45}
+      onPress={() => {
+        console.log('nav')
+        navigation.navigate('DashboardNotifications')
+      }}>
+      {!!unseenCount && (
+        <Box
+          borderRadius="full"
+          position="absolute"
+          width={19}
+          height={19}
+          left={24}
+          top={8}
+          zIndex="2"
+          backgroundColor="errorRed"
+          justifyContent="center"
+          alignItems="center">
+          <Text color="white" variant="boldWhite12">
+            {unseenCount}
+          </Text>
+        </Box>
+      )}
+      <IconBell />
+    </BaseOpacity>
+  )
+}
