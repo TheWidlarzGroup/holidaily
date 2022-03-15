79b38d4fbeb00dfd8cc10743ffdfe2499107b7c6
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-15 09:54:16 +0100

    fix type

--- src/types/useUserRequestsTypes.ts
+++ src/types/useUserRequestsTypes.ts
@@ -1,10 +1,12 @@
+export type RequestStatus = 'CANCELLED' | 'APPROVED' | 'PENDING' | 'PAST'
+
 export type RequestTypes = {
   description: string
   id: string
   message: string
   range: string[]
   sickTime: boolean
-  status: 'NOW' | 'APPROVED' | 'PENDING' | 'PAST'
+  status: RequestStatus
 }
 export type RequestsQueryTypes = {
   requests: RequestTypes[]
