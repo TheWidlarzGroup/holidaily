6cdb968ebd943adb2a069dcb9015d36f2ab50ec3
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-21 13:38:46 +0100

    fix request sent translations

--- src/screens/requestVacation/components/SummaryRequestVacation.tsx
+++ src/screens/requestVacation/components/SummaryRequestVacation.tsx
@@ -9,6 +9,7 @@ import { SummaryDays } from './SummaryRequestVacation/SummaryDays'
 import { SummaryRequestVacationHeader } from './SummaryRequestVacation/SummaryRequestVacationHeader'
 import { SicktimeAndMessage } from './SummaryRequestVacation/SicktimeAndMessage'
 import { SummaryRequestVacationPhotos } from './SummaryRequestVacation/SummaryRequestVacationPhotos'
+import { useBooleanState } from 'hooks/useBooleanState'
 
 type SummaryRequestVacationProps = {
   description: string
@@ -30,26 +31,29 @@ export const SummaryRequestVacation = ({
   photos = [],
 }: SummaryRequestVacationProps) => {
   const styles = useStyles()
-  const { handleRequestHolidays, isLoading, isSuccess } = useRequestHolidays()
+  const [isLoading, { setTrue: startLoading, setFalse: stopLoading }] = useBooleanState(false)
+  const [isSuccess, { setTrue: markSuccess }] = useBooleanState(false)
 
   const { t } = useTranslation('requestVacation')
 
   const handleSend = () => {
     if (startDate && endDate) {
-      handleRequestHolidays({
-        startDate: getISODateString(startDate),
-        endDate: getISODateString(endDate),
-        description,
-        sickTime: isSick,
-        message,
-      })
+      startLoading()
     }
   }
   useEffect(() => {
+    let timeout: number | undefined
+    if (isLoading) {
+      timeout = setTimeout(() => {
+        stopLoading()
+        markSuccess()
+      }, 800)
+    }
     if (isSuccess) {
       onNextPressed()
     }
-  }, [isSuccess, onNextPressed])
+    return () => clearTimeout(timeout)
+  }, [isSuccess, onNextPressed, isLoading, markSuccess, stopLoading])
   const ptoTaken = startDate && endDate ? calculatePTO(startDate, endDate) : 0
 
   return (
