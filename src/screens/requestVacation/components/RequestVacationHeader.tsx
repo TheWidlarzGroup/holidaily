92917551bce52946f129aaf7ada665ae4b4499e7
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-29 12:25:01 +0200

    fix types

--- src/screens/requestVacation/components/RequestVacationHeader.tsx
+++ src/screens/requestVacation/components/RequestVacationHeader.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Box } from 'utils/theme'
 import { RequestVacationBar } from 'components/RequestVacationBar'
-import { ModalHeader } from 'components/ModalHeader'
 import { RequestVacationHeaderText } from './RequestVacationHeaderText'
 import { useRequestVacationContext } from '../contexts/RequestVacationContext'
 
-export const RequestVacationHeader = ({ isSent }: { isSent?: true }) => {
+export const RequestVacationHeader = () => {
   const { step, setStep } = useRequestVacationContext()
-  return isSent ? (
-    <ModalHeader>
-      <RequestVacationHeaderText isSent />
-    </ModalHeader>
-  ) : (
+
+  return (
     <Box paddingBottom="m">
       <RequestVacationHeaderText step={step} setStep={setStep} />
       <RequestVacationBar currentScreen={step ? 'Summary' : 'Form'} />
