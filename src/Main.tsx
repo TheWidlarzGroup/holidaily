3ea121beb019963322a9890eb32d67294ab05057
Jan Koziński <jan.kozinski.02@gmail.com>
2022-03-30 16:32:23 +0200

    remove logs

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { StatusBar } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ThemeProvider } from '@shopify/restyle'
-import axios from 'axios'
 import { UserContextProvider } from 'contexts/UserProvider'
 import { ModalProvider } from 'contexts/ModalProvider'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
@@ -16,20 +15,7 @@ initBackendMocks()
 export const Main = () => {
   // FIXME: read from user preferences
   const darkMode = false
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const { data: tempUserRes } = await axios.post('/api/users', { firstName: 'jan' })
-        axios.defaults.headers.common.userId = tempUserRes.user.id
-        const { data } = await axios.get('/api/notifications')
-        console.log(data)
-      } catch (error) {
-        if (error.response) console.log(error.response.body, error.response.headers)
-        else console.log(error)
-      }
-    }
-    init()
-  }, [])
+
   return (
     <ThemeProvider theme={darkMode ? darkTheme : theme}>
       <SafeAreaProvider>
