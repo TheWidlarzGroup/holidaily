8c47a2371b93cf195df42440122c5c6fb3630cd8
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-05 13:03:26 +0200

    load and mark seen notifications before fetch

--- src/data-access/queries/useFetchNotifications.ts
+++ src/data-access/queries/useFetchNotifications.ts
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query'
 import axios from 'axios'
 import { Notification } from 'mock-api/models'
+import { setItem, getItem } from 'utils/localStorage'
 import { QueryKeys } from '../QueryKeys'
 import { API } from '../API'
 
@@ -9,8 +10,23 @@ type FetchNotificationsResponse = {
 }
 
 const fetchNotifications = async () => {
+  // wee keep seen ids in the local storage to fake the real app experience. Remove it once we switch miragejs with real backend
+  const seenNotificationsIds = JSON.parse((await getItem('seenNotificationsIds')) ?? '[]')
+  if (seenNotificationsIds instanceof Array) {
+    seenNotificationsIds.forEach(async (id) => {
+      console.log(id)
+      await axios.patch(API.PATCH.markNotificationAsSeen(id))
+    })
+  }
   const res = await axios.get<FetchNotificationsResponse>(API.GET.notifications)
   return res.data
 }
 
-export const useFetchNotifications = () => useQuery([QueryKeys.NOTIFICATIONS], fetchNotifications)
+export const useFetchNotifications = () =>
+  useQuery([QueryKeys.NOTIFICATIONS], fetchNotifications, {
+    onSuccess: (data) => {
+      const { notifications } = data
+      const seenNotificationsIds = notifications.filter((n) => n.wasSeenByHolder).map((n) => n.id)
+      setItem('seenNotificationsIds', JSON.stringify(seenNotificationsIds))
+    },
+  })
