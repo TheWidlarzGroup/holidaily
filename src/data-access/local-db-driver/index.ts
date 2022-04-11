bf7c8a1cd154ae4f4423a68eea514638e54ec89a
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-11 13:05:53 +0200

    refactor

--- src/data-access/local-db-driver/index.ts
+++ src/data-access/local-db-driver/index.ts
@@ -3,43 +3,44 @@ import { keys } from 'utils/manipulation'
 import { Schemas } from './Schemas'
 import type { Models } from './Schemas'
 
+const initDbClient = async () => Realm.open({ path: 'mirage-state', schema: Schemas })
+
 const initDbService = async () => {
-  const client = await Realm.open({
-    path: 'mirage-state',
-    schema: Schemas,
-  })
+  const client = await initDbClient()
+  const createOne = (model: keyof Models, props: Models[typeof model]) => {
+    let record
+    client.write(() => {
+      record = client.create<Models[typeof model]>(model, props)
+    })
+    return record
+  }
+  const findMany = (model: keyof Models, props: Partial<Models[typeof model]>) => {
+    let query = ''
+    const propertyKeys = keys(props)
+    propertyKeys.forEach((key, idx) => {
+      if (idx === 0) return (query = `${key} == ${props[key]}`)
+      query += ` AND ${key} == ${props[key]}`
+    })
+    const allRecords = client.objects(model)
+    return query.length ? allRecords.filtered(query) : allRecords
+  }
+  const findOneById = (model: keyof Models, id: string) =>
+    client.objectForPrimaryKey<Models[typeof model]>(model, id)
+
+  const updateOneById = (model: keyof Models, id: string, props: Partial<Models[typeof model]>) => {
+    const record = findOneById(model, id)
+    if (!record) throw new Error('Tried to update non existant record')
+    client.write(() => {
+      // @ts-ignore
+      keys(props).forEach((p) => (record[p] = props[p]))
+    })
+    return record
+  }
   return {
-    _client: client,
-    createOne(model: keyof Models, props: Models[typeof model]) {
-      let record
-      this._client.write(() => {
-        record = this._client.create<Models[typeof model]>(model, props)
-      })
-      return record
-    },
-    findMany(model: keyof Models, props: Partial<Models[typeof model]>) {
-      let query = ''
-      const propertyKeys = keys(props)
-      propertyKeys.forEach((key, idx) => {
-        if (idx === 0) return (query = `${key} == ${props[key]}`)
-        query += ` AND ${key} == ${props[key]}`
-      })
-      const allRecords = this._client.objects(model)
-      return query.length ? allRecords.filtered(query) : allRecords
-    },
-    findOneById(model: keyof Models, id: string) {
-      return this._client.objectForPrimaryKey<Models[typeof model]>(model, id)
-    },
-    updateOneById(model: keyof Models, id: string, props: Partial<Models[typeof model]>) {
-      const record = this.findOneById(model, id)
-      if (!record) throw new Error('Tried to update non existant record')
-      this._client.write(() => {
-        // @ts-ignore
-        keys(props).forEach((p) => (record[p] = props[p]))
-      })
-      return record
-    },
+    createOne,
+    findMany,
+    findOneById,
+    updateOneById,
   }
 }
-const dbService = initDbService()
-export const getDbService = () => dbService
+export const dbService = initDbService()
