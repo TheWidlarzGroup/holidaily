1f7b08f0315ad7aec364034364a07831c43513e6
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-12 13:46:27 +0200

    make initDbService synchronous

--- src/data-access/local-db-driver/index.ts
+++ src/data-access/local-db-driver/index.ts
@@ -3,11 +3,20 @@ import { keys } from 'utils/manipulation'
 import { Schemas } from './Schemas'
 import type { Models } from './Schemas'
 
-const initDbClient = async () => Realm.open({ path: 'mirage-state', schema: Schemas })
+// const initDbClient = async () => Realm.open({ path: 'mirage-state', schema: Schemas })
 
-const initDbService = async () => {
-  const client = await initDbClient()
+const initDbService = () => {
+  let client: Realm
+  const initialize = async () => {
+    client = await Realm.open({ path: 'mirage-state', schema: Schemas })
+  }
+
+  const checkIfInitialized = () => {
+    if (!client) throw new Error('Db client is uninitialized')
+  }
+
   const createOne = (model: keyof Models, props: Models[typeof model]) => {
+    checkIfInitialized()
     let record
     client.write(() => {
       record = client.create<Models[typeof model]>(model, props)
@@ -15,6 +24,7 @@ const initDbService = async () => {
     return record
   }
   const findMany = (model: keyof Models, props: Partial<Models[typeof model]>) => {
+    checkIfInitialized()
     let query = ''
     const propertyKeys = keys(props)
     propertyKeys.forEach((key, idx) => {
@@ -24,19 +34,22 @@ const initDbService = async () => {
     const allRecords = client.objects(model)
     return query.length ? allRecords.filtered(query) : allRecords
   }
-  const findOneById = (model: keyof Models, id: string) =>
-    client.objectForPrimaryKey<Models[typeof model]>(model, id)
+  const findOneById = (model: keyof Models, id: string) => {
+    checkIfInitialized()
+    return client.objectForPrimaryKey<Models[typeof model]>(model, id)
+  }
 
   const updateOneById = (model: keyof Models, id: string, props: Partial<Models[typeof model]>) => {
-    const record = findOneById(model, id)
+    checkIfInitialized()
+    const record: any = findOneById(model, id)
     if (!record) throw new Error('Tried to update non existant record')
     client.write(() => {
-      // @ts-ignore
       keys(props).forEach((p) => (record[p] = props[p]))
     })
     return record
   }
   return {
+    initialize,
     createOne,
     findMany,
     findOneById,
@@ -44,3 +57,4 @@ const initDbService = async () => {
   }
 }
 export const dbService = initDbService()
+export type DbService = ReturnType<typeof initDbService>
