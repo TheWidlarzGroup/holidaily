680d8fba80df59851139a7c8a02ff70fccb77721
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-14 09:59:06 +0200

    Fix/pto hack (#390)
    * add translations
    
    * reafactor
    
    * add alert to request vacation form
    
    * refacotr and fix modal
    
    * fix modal

---
+++ src/screens/requestVacation/components/NotEnoughPTO.tsx
@@ -0,0 +1,46 @@
+import { CustomButton } from 'components/CustomButton'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { getDurationInDays } from 'utils/dates'
+import { Box, Text } from 'utils/theme'
+import ExlamationMark from 'assets/icons/icon-exclamation-mark.svg'
+
+type NotEnoughPtoProps = {
+  availablePto: number
+  onPress: F0
+  origin?: 'calendar' | 'form'
+}
+
+export const NotEnoughPTO = (p: NotEnoughPtoProps) => {
+  const { t } = useTranslation('requestVacation')
+  return (
+    <>
+      <Box flexDirection="row" alignItems="flex-start">
+        <Box
+          margin="m"
+          bg="errorRed"
+          height={28}
+          width={28}
+          marginTop={0}
+          borderRadius="full"
+          alignItems="center"
+          justifyContent="center">
+          <ExlamationMark color="white" />
+        </Box>
+        <Box>
+          <Text fontSize={15} lineHeight={20}>
+            {t('error')}
+          </Text>
+          <Text fontSize={14} fontFamily="Nunito-Bold" marginTop="m" marginBottom="l">
+            {t('availablePto', { availablePto: getDurationInDays(p.availablePto) })}
+          </Text>
+        </Box>
+      </Box>
+      <CustomButton
+        label={p.origin === 'form' ? t('changeDays') : t('clear')}
+        variant="danger"
+        onPress={p.onPress}
+      />
+    </>
+  )
+}
--- src/screens/requestVacation/components/RequestSent.tsx
+++ src/screens/requestVacation/components/RequestSent.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { ModalProps } from 'react-native-modal'
-import { mkUseStyles, Theme, Box } from 'utils/theme'
-import Animated, { useAnimatedStyle, useDerivedValue, withSpring } from 'react-native-reanimated'
-import useDimensions from '@shopify/restyle/dist/hooks/useDimensions'
+import { Box } from 'utils/theme'
+import { BottomModal } from 'components/BottomModal'
 import { RequestSentButtons } from './RequestSent/RequestSentButtons'
 import { RequestSentInfo } from './RequestSent/RequestSentInfo'
 import { RequestSentImage } from './RequestSent/RequestSentImage'
@@ -13,53 +12,17 @@ type RequestSentProps = Pick<ModalProps, 'isVisible'> & {
   onPressOk: F0
 }
 
-export const RequestSent = ({ isVisible, ...buttonPressHandlers }: RequestSentProps) => {
-  const styles = useStyles()
-  const { height } = useDimensions()
-  const progress = useDerivedValue(() => (isVisible ? 1 : 0), [isVisible])
-
-  const animatedModalStyles = useAnimatedStyle(() => {
-    const v = progress.value
-    const h = height
-    return {
-      transform: [{ translateY: withSpring((1 - v) * h, { overshootClamping: true }) }],
-      opacity: withSpring(v, { overshootClamping: true }),
-    }
-  }, [])
-
-  if (!isVisible) return null
-
-  return (
-    <Animated.View style={[styles.modal, animatedModalStyles]}>
-      <Box
-        alignItems="center"
-        paddingHorizontal="xxl"
-        flex={1}
-        paddingBottom="xl"
-        justifyContent="flex-end">
-        <RequestSentImage />
-        <RequestSentInfo />
-        <RequestSentButtons {...buttonPressHandlers} />
-      </Box>
-    </Animated.View>
-  )
-}
-
-const useStyles = mkUseStyles((theme: Theme) => ({
-  modal: {
-    flex: 1,
-    backgroundColor: theme.colors.primary,
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    top: -5,
-    borderTopLeftRadius: theme.borderRadii.lmin,
-    borderTopRightRadius: theme.borderRadii.lmin,
-    shadowOffset: { width: -2, height: 0 },
-    shadowColor: theme.colors.black,
-    shadowOpacity: 0.04,
-    shadowRadius: 2,
-    elevation: 20,
-  },
-}))
+export const RequestSent = ({ isVisible, ...buttonPressHandlers }: RequestSentProps) => (
+  <BottomModal isVisible={isVisible} coverScreen>
+    <Box
+      alignItems="center"
+      paddingHorizontal="xxl"
+      flex={1}
+      paddingBottom="xl"
+      justifyContent="flex-end">
+      <RequestSentImage />
+      <RequestSentInfo />
+      <RequestSentButtons {...buttonPressHandlers} />
+    </Box>
+  </BottomModal>
+)
