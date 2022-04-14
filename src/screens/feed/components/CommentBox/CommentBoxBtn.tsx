c6e3791b17c74034a16a7b28bb971a25c5b401db
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-14 17:30:53 +0200

    fix lint

--- src/screens/feed/components/CommentBox/CommentBoxBtn.tsx
+++ src/screens/feed/components/CommentBox/CommentBoxBtn.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
 import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated'
 import { BaseOpacity, Box, Text } from 'utils/theme'
 
