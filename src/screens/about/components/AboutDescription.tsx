aee1b3c624f7f7b262759a1cf6ea971f719c1158
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-15 15:47:43 +0200

    Fix/translations (#394)
    * fix translations
    
    * fix lint
    
    * remove line break
    
    * Update src/screens/about/components/AboutDescription.tsx
    
    * Update src/screens/about/components/AboutDescription.tsx
    
    Trans component type is to hard for typescript to understand
    
    * fix
    
    * add informative comment
    
    * Update src/screens/feed/components/CommentBox/CommentBoxBtn.tsx

--- src/screens/about/components/AboutDescription.tsx
+++ src/screens/about/components/AboutDescription.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+import { Trans } from 'react-i18next'
 import { Box, Text } from 'utils/theme'
 
-export const AboutDescription = () => {
-  const { t } = useTranslation('welcome')
-  // TODO: replace with Trans component
-  return (
-    <Box paddingHorizontal="m" paddingBottom="xxxl">
-      <Text textAlign="left" variant="body1">
-        <Text variant="body1Bold" color="primary">
-          {t('aboutDesc1Part1')}
-        </Text>
-        {t('aboutDesc1Part2')}
-        <Text variant="body1Bold">{t('aboutDesc1Part3')}</Text>
-        {t('aboutDesc1Part4')}
-        <Text variant="body1Bold">{t('aboutDesc1Part5')}</Text>
-      </Text>
-      <Text textAlign="left" marginTop="l" variant="body1">
-        <Text variant="body1Bold">{t('aboutDesc2Part1')}</Text>
-        {t('aboutDesc2Part2')}
-        <Text variant="body1Bold">{t('aboutDesc2Part3')}</Text>
-      </Text>
-    </Box>
-  )
-}
+export const AboutDescription = () => (
+  <Box paddingHorizontal="m" paddingBottom="xxxl">
+    <Text textAlign="left" variant="body1">
+      {/* @ts-ignore  Trans component causes error in pipeline but not in IDE: "Type instantiation is excessively deep and possibly infinite." */}
+      <Trans
+        ns="welcome"
+        i18nKey="aboutDesc1"
+        components={{
+          b: <Text variant="body1Bold" />,
+          bp: <Text variant="body1Bold" color="primary" />,
+        }}
+      />
+    </Text>
+    <Text textAlign="left" marginTop="l" variant="body1">
+      {/* @ts-ignore */}
+      <Trans
+        ns="welcome"
+        i18nKey="aboutDesc2"
+        components={{
+          b: <Text variant="body1Bold" />,
+        }}
+      />
+    </Text>
+  </Box>
+)
