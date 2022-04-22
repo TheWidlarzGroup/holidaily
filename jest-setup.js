5ce4d1bc268d074170d8ba04def88652a4c106b5
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-22 11:02:25 +0200

    Fix/vacation during holiday (#420)
    * fix drawing pto during holiday
    
    * add not working test
    
    * add tests for mocked dates
    
    * fix
    
    * reduce tests running time

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,7 @@
 require('react-native-gesture-handler/jestSetup')
-require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests()
+// require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests()
+jest.mock('poland-public-holidays', () => ({
+  __esModule: true,
+  isHoliday: jest.fn(() => false),
+  isWorkingDay: jest.fn((date) => ![0, 6].includes(date.getDay())),
+}))
