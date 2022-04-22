ed30981b6bfd34111aa97831adede0700e08a92f
Lukasz Baranowski <lukaszb48@gmail.com>
2022-04-22 18:03:19 +0200

    refactored listeners

--- App.tsx
+++ App.tsx
@@ -11,10 +11,9 @@ export const App = () => {
 
   LogBox.ignoreLogs([
     'Non-serializable values were found in the navigation state',
-    'Setting a timer for a long period of time',
-    // to fix EventEmitter.removeListener we will have to bump lots of packages, this might be a breaking change
-    'EventEmitter.removeListener',
+    '`new NativeEventEmitter()`',
     'Require cycle: node_modules',
+    'EventEmitter.removeListener',
   ])
 
   return (
