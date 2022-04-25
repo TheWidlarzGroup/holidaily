737efc9851570f8b6c9083c563d7bd01f7e5903c
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-25 16:54:10 +0200

    add comment

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -21,6 +21,7 @@ export const AppNavigation = () => {
   const isFirstRender = useRef(true)
   const idRef = useRef(user?.id)
   useEffect(() => {
+    // update teams if we get to dashboard through onboarding
     if (user && isTempUserCreated && !isOrgLoading && organization?.teams) {
       // if idRef contains user.id than we already have updated the user teams
       if (user.id === idRef.current) return
