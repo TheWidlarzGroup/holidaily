8d79b3495a5c7311f4c29bcd6cf62105d997151c
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-26 13:44:14 +0200

    Add swipeable modal

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useRef } from 'react'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { DashboardHeader } from 'screens/dashboard/components/DashboardHeader'
 import { TeamElement } from 'screens/dashboard/components/TeamElement'
@@ -7,15 +7,25 @@ import { useNavigation } from '@react-navigation/native'
 import { SortableList } from 'screens/dashboard/dragAndDrop/SortableList'
 import { Team, User } from 'mock-api/models/mirageTypes'
 import { LoadingModal } from 'components/LoadingModal'
-import { useBooleanState } from 'hooks/useBooleanState'
 import { useUserContext } from 'hooks/useUserContext'
+import { SwipeableModal, SwipeableModalRef } from 'components/SwipeableModal'
 import { useModalContext } from 'contexts/ModalProvider'
-import Modal from 'react-native-modal'
 import { DashboardTeamMember } from './DashboardTeamMember'
 
 export const Dashboard = () => {
   const { showModal, hideModal } = useModalContext()
-  const openModal = (user: User) => showModal(<UserModal hideModal={hideModal} modalUser={user} />)
+  const modalRef = useRef<SwipeableModalRef>(null)
+  const openModal = (user: User) =>
+    showModal(
+      <SwipeableModal unmount={hideModal} ref={modalRef}>
+        <DashboardTeamMember
+          closeModal={() => {
+            if (modalRef && modalRef.current) modalRef.current.hide()
+          }}
+          user={user}
+        />
+      </SwipeableModal>
+    )
   const { user } = useUserContext()
 
   const navigation = useNavigation<DashboardNavigationType<'Dashboard'>>()
@@ -37,34 +47,5 @@ export const Dashboard = () => {
         </SortableList>
       </SafeAreaWrapper>
     </>
-  )
-}
-const USER_MODAL_ANIM_TIME = 300
-const UserModal = ({ hideModal, modalUser }: { hideModal: F0; modalUser: User }) => {
-  const [isVisible, { setFalse: fadeOut }] = useBooleanState(true)
-  useEffect(() => {
-    let timeout: number
-    if (!isVisible) timeout = setTimeout(hideModal, USER_MODAL_ANIM_TIME + 20)
-    return () => clearTimeout(timeout)
-  }, [isVisible, hideModal])
-  return (
-    <Modal
-      statusBarTranslucent
-      swipeThreshold={20}
-      isVisible={isVisible}
-      hasBackdrop
-      backdropColor="black"
-      backdropOpacity={0.6}
-      swipeDirection="down"
-      animationIn="slideInUp"
-      animationOut="slideOutDown"
-      style={{ margin: 0, marginTop: 120 }}
-      animationInTiming={USER_MODAL_ANIM_TIME}
-      animationOutTiming={USER_MODAL_ANIM_TIME}
-      onSwipeComplete={fadeOut}
-      onBackButtonPress={fadeOut}
-      onBackdropPress={fadeOut}>
-      <DashboardTeamMember closeModal={fadeOut} user={modalUser} />
-    </Modal>
   )
 }
