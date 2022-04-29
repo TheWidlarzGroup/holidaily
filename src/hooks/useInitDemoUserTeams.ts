c517e13628d3f1af0af5ff5cb055806b858fd4cd
Jan Koziński <jan.kozinski.02@gmail.com>
2022-04-29 16:40:55 +0200

    refactor simplify and DRY up demo user's team initialization

--- src/hooks/useInitDemoUserTeams.ts
+++ src/hooks/useInitDemoUserTeams.ts
@@ -1,5 +1,5 @@
 import { useGetOrganization } from 'dataAccess/queries/useOrganizationData'
-import { useCallback } from 'react'
+import { useCallback, useRef } from 'react'
 import { useTeamsContext } from './useTeamsContext'
 import { useUserContext } from './useUserContext'
 
@@ -7,10 +7,12 @@ export const useInitDemoUserTeams = () => {
   const { user, updateUser } = useUserContext()
   const { data: organization } = useGetOrganization()
   const { addUserToTeams } = useTeamsContext()
+  const idRef = useRef<string | null>(null)
   return useCallback(() => {
     if (user && organization?.teams) {
+      if (user.id === idRef.current) return
+      idRef.current = user.id
       const userTeams = organization.teams.slice(0, -2)
-
       updateUser({
         teams: userTeams,
       })
