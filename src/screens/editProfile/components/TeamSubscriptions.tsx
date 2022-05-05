57ffdba2219c72343bd4b4448ae2ec4ada5c2052
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-05 16:48:55 +0200

    refactor

--- src/screens/editProfile/components/TeamSubscriptions.tsx
+++ src/screens/editProfile/components/TeamSubscriptions.tsx
@@ -1,26 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigation } from '@react-navigation/native'
-import { RectButton } from 'react-native-gesture-handler'
-import { BaseOpacity, Box, Text, mkUseStyles, Theme, useTheme } from 'utils/theme'
+import { Box, Text } from 'utils/theme'
 import { TeamsType, useTeamMocks } from 'utils/mocks/teamsMocks'
-import IconAdd from 'assets/icons/icon-add.svg'
 import { ChangesSavedModal } from 'components/ChangesSavedModal'
 import { useUserContext } from 'hooks/useUserContext'
 import { LoadingModal } from 'components/LoadingModal'
 import { Team } from 'mockApi/models'
 import { useBooleanState } from 'hooks/useBooleanState'
-import { ConfirmationModal } from 'components/ConfirmationModal'
 import { UserProfileType } from 'navigation/types'
-
-type TeamProps = {
-  teamName: string
-  filterUnsubscribedTeams: F1<string>
-}
+import { AddSubscriptionsButton } from './TeamSubscriptions/AddSubsriptionsButton'
+import { ActiveSubscriptions } from './TeamSubscriptions/ActiveSubscriptions'
 
 export const TeamSubscriptions = () => {
   const { t } = useTranslation('userProfile')
-  const theme = useTheme()
   const { navigate } = useNavigation<UserProfileType<'EditProfile'>>()
   const { user, updateUser } = useUserContext()
   const { isLoading } = useTeamMocks()
@@ -70,66 +63,10 @@ export const TeamSubscriptions = () => {
           marginBottom={user.teams.length > 0 ? 'xm' : 'xxxl'}>
           {t('userSubscriptions')}
         </Text>
-        <BaseOpacity
-          style={user.teams.length > 0 ? { right: 24 } : { left: 30 }}
-          onPress={onSubscribeTeam}
-          justifyContent="center"
-          alignItems="center"
-          position="absolute"
-          top={32}
-          height={44}
-          width={44}
-          borderRadius="full"
-          backgroundColor="lightGrey">
-          <IconAdd color={theme.colors.headerGrey} />
-        </BaseOpacity>
-        <Box flexDirection="row" marginRight="xl" flexWrap="wrap">
-          {teams.map(({ teamName, id }) => (
-            <TeamElement
-              teamName={teamName}
-              key={id}
-              filterUnsubscribedTeams={filterUnsubscribedTeams}
-            />
-          ))}
-        </Box>
+        <AddSubscriptionsButton onSubscribeTeam={onSubscribeTeam} userTeams={user.teams} />
+        <ActiveSubscriptions teams={teams} filterUnsubscribedTeams={filterUnsubscribedTeams} />
       </Box>
       <ChangesSavedModal isVisible={changesSaved} content={modalContent} />
     </>
   )
 }
-
-const TeamElement = (p: TeamProps) => {
-  const [isConfirmationNeeded, { setTrue: askForConfirmation, setFalse: dismissConfirmation }] =
-    useBooleanState(false)
-
-  const styles = useStyles()
-  return (
-    <>
-      <RectButton onPress={askForConfirmation} style={styles.team}>
-        <Text variant="resendWhite" paddingHorizontal="l" paddingVertical="xm">
-          {p.teamName}
-        </Text>
-      </RectButton>
-      <ConfirmationModal
-        isVisible={isConfirmationNeeded}
-        onAccept={() => {
-          dismissConfirmation()
-          p.filterUnsubscribedTeams(p.teamName)
-        }}
-        hideModal={dismissConfirmation}
-        onDecline={dismissConfirmation}
-      />
-    </>
-  )
-}
-
-const useStyles = mkUseStyles((theme: Theme) => ({
-  team: {
-    marginRight: theme.spacing.s,
-    marginBottom: theme.spacing.m,
-    backgroundColor: theme.colors.black,
-    borderRadius: theme.spacing.l,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}))
