4458e98977ecff8b223212d096802eaddb79e1b6
Lukasz Baranowski <lukaszb48@gmail.com>
2022-05-05 22:44:55 +0200

    Feat/reskin inputs (#478)
    * inputs reskin
    
    * adjusted styles
    
    * adjustments

---
+++ src/components/InputSearchIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SearchIcon } from 'assets/icons/SearchIcon'
+import { Box } from 'utils/theme'
+
+export const InputSearchIcon = () => (
+  <Box position="absolute" left={6} top={25}>
+    <SearchIcon fill="grey" />
+  </Box>
+)
