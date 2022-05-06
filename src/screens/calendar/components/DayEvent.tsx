5c856f3e3dccde77ea0a22e58fe59fbf942729a0
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-06 14:43:12 +0200

    fix calendar scroll issues (#487)
    * fix calendar scroll issues
    
    * remove unused function
    
    * add comment
    
    * update comments

--- src/screens/calendar/components/DayEvent.tsx
+++ src/screens/calendar/components/DayEvent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Box, Text } from 'utils/theme'
-import { Avatar } from 'components/Avatar'
+import { Box, Text, Theme, theme } from 'utils/theme'
+import { Avatar, avatarSizes } from 'components/Avatar'
 
 export type DayOffEvent = {
   id: string
@@ -17,8 +17,17 @@ export type DayOffEvent = {
 
 type DayEventProps = { event: DayOffEvent }
 
+const EVENT_VERTICAL_PADDING: keyof Theme['spacing'] = 's'
+const AVATAR_SIZE: keyof typeof avatarSizes = 's'
+// Comment: used to determine container flatlist scroll offset
+export const EVENT_HEIGHT = theme.spacing[EVENT_VERTICAL_PADDING] * 2 + avatarSizes[AVATAR_SIZE]
+
 export const DayEvent = ({ event }: DayEventProps) => (
-  <Box paddingVertical="s" flexDirection="row" alignItems="center" key={event.person}>
+  <Box
+    paddingVertical={EVENT_VERTICAL_PADDING}
+    flexDirection="row"
+    alignItems="center"
+    key={event.person}>
     <Avatar
       src={event?.photo}
       userDetails={{
