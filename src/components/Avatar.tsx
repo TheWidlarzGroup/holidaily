a0785ccfb405550b1ad110dd0a6fed2767179241
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-06 11:33:31 +0200

    fix calendar scroll issues

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,16 +4,16 @@ import { Box, Text } from 'utils/theme'
 import FastImage from 'react-native-fast-image'
 import { User } from 'mockApi/models'
 
-const sizes = { xs: 24, s: 44, m: 62, l: 112 }
+export const avatarSizes = { xs: 24, s: 44, m: 62, l: 112 }
 
 type AvatarProps = React.ComponentProps<typeof Box> & {
   src?: string | null
-  size?: keyof typeof sizes | number
+  size?: keyof typeof avatarSizes | number
   userDetails?: Pick<User, 'userColor' | 'firstName'> & Partial<Pick<User, 'lastName'>>
 }
 
 export const Avatar = ({ size = 'm', src, userDetails, ...containerProps }: AvatarProps) => {
-  const chosenSize = typeof size === 'number' ? size : sizes[size]
+  const chosenSize = typeof size === 'number' ? size : avatarSizes[size]
   const width = chosenSize
   const height = chosenSize
   return (
--- src/screens/calendar/components/DayInfo.tsx
+++ src/screens/calendar/components/DayInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Text } from 'utils/theme'
+import { Box, Text, theme } from 'utils/theme'
 import { getDateWithMonthString } from 'utils/dates'
 import { DayWeekend } from './DayWeekend'
 import { DayEvent, DayOffEvent } from './DayEvent'
@@ -10,18 +10,30 @@ export type DayInfoProps = {
   weekend?: number
 }
 
+const DAY_PADDING_VERTICAL = 'm'
+const OUTER_BOX_MARGIN = 's'
+const INNER_BOX_MARGIN = 's'
+const HEADING_FONT_SIZE = 12
+const FONT_OFFSET = 4
+export const DAY_ITEM_HEIGHT =
+  2 * theme.spacing[DAY_PADDING_VERTICAL] +
+  2 * theme.spacing[OUTER_BOX_MARGIN] +
+  theme.spacing[INNER_BOX_MARGIN] +
+  HEADING_FONT_SIZE +
+  FONT_OFFSET
+
 export const DayInfo = ({ date, events, weekend }: DayInfoProps) => {
   if (weekend) return <DayWeekend date={date} weekend={weekend} />
   return (
     <Box
       borderRadius="lmin"
       backgroundColor="white"
-      paddingVertical="m"
+      paddingVertical={DAY_PADDING_VERTICAL}
       paddingHorizontal="lplus"
-      marginVertical="s">
+      marginVertical={OUTER_BOX_MARGIN}>
       <Text variant="captionText">{getDateWithMonthString(date)}</Text>
       {typeof events !== 'undefined' && events?.length > 0 && (
-        <Box marginTop="s">
+        <Box marginTop={INNER_BOX_MARGIN}>
           {events.map((event) => (
             <DayEvent event={event} key={event.id} />
           ))}
