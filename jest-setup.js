6cf4ec0c0fb6052dbdff06ad7fb9442a296da63b
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-06 12:53:37 +0200

    fix tests modules mock

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,13 @@
+import { setUpTests } from 'react-native-reanimated/lib/src/reanimated2/jestUtils'
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
 require('react-native-gesture-handler/jestSetup')
 // TODO: fix reanimated mock, because the following line is not working since we bumped reanimated version
-// require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests()
+// require('react-native-reanimated/lib/src/reanimated2/jestUtils').setUpTests()
+setUpTests()
+setUpTests()
+
 jest.mock('poland-public-holidays', () => ({
   __esModule: true,
   isHoliday: jest.fn(() => false),
