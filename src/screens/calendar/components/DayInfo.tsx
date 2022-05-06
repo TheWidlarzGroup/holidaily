8b5912bb5c75861f0b9656835894d3eeb89372f4
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-06 12:10:13 +0200

    add comment

--- src/screens/calendar/components/DayInfo.tsx
+++ src/screens/calendar/components/DayInfo.tsx
@@ -13,13 +13,14 @@ export type DayInfoProps = {
 const DAY_PADDING_VERTICAL = 'm'
 const OUTER_BOX_MARGIN = 's'
 const INNER_BOX_MARGIN = 's'
-const HEADING_FONT_SIZE = 12
+const HEADING_TEXT_VARIANT = 'captionText'
 const FONT_OFFSET = 4
+// used to determine container flatlist scroll offset
 export const DAY_ITEM_HEIGHT =
   2 * theme.spacing[DAY_PADDING_VERTICAL] +
   2 * theme.spacing[OUTER_BOX_MARGIN] +
   theme.spacing[INNER_BOX_MARGIN] +
-  HEADING_FONT_SIZE +
+  theme.textVariants[HEADING_TEXT_VARIANT].fontSize +
   FONT_OFFSET
 
 export const DayInfo = ({ date, events, weekend }: DayInfoProps) => {
@@ -31,7 +32,7 @@ export const DayInfo = ({ date, events, weekend }: DayInfoProps) => {
       paddingVertical={DAY_PADDING_VERTICAL}
       paddingHorizontal="lplus"
       marginVertical={OUTER_BOX_MARGIN}>
-      <Text variant="captionText">{getDateWithMonthString(date)}</Text>
+      <Text variant={HEADING_TEXT_VARIANT}>{getDateWithMonthString(date)}</Text>
       {typeof events !== 'undefined' && events?.length > 0 && (
         <Box marginTop={INNER_BOX_MARGIN}>
           {events.map((event) => (
