434065f968b5cac97e4ac4aafd83253e3c3742c0
Lukasz Baranowski <lukaszb48@gmail.com>
2022-05-09 09:25:22 +0200

    fixed dashboard success modal (#492)

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,12 +1,11 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { DashboardHeader } from 'screens/dashboard/components/DashboardHeader'
 import { User } from 'mock-api/models/mirageTypes'
 import { SwipeableModal } from 'components/SwipeableModal'
 import { TeamsModal } from 'screens/welcome/components/TeamsModal'
-import { BottomSheetModalComponent } from 'components/BottomSheetModalComponent'
-import { BottomSheetModal } from '@gorhom/bottom-sheet'
 import { getItem, setItem } from 'utils/localStorage'
+import { useBooleanState } from 'hooks/useBooleanState'
 import { DashboardTeamMember } from './DashboardTeamMember'
 import { SortableTeams } from './components/SortableTeams'
 
@@ -17,20 +16,20 @@ export const Dashboard = () => {
     setModalUser(user)
     setIsModalVisible(true)
   }
-  const organizationModalRef = useRef<BottomSheetModal>(null)
-  const openOrganizationModal = useCallback(() => organizationModalRef.current?.present(), [])
-  const closeOrganizationModal = useCallback(() => organizationModalRef.current?.dismiss(), [])
+
+  const [isSuccessModalVisible, { setFalse: closeSuccessModal, setTrue: openSuccessModal }] =
+    useBooleanState(true)
   useEffect(() => {
     const openModalOnFirstAppLaunch = async () => {
       const seenTeamsModal = await getItem('seenTeamsModal')
       if (seenTeamsModal === 'false') return
       setTimeout(async () => {
-        openOrganizationModal()
+        openSuccessModal()
         await setItem('seenTeamsModal', 'false')
       }, 2000)
     }
     openModalOnFirstAppLaunch()
-  }, [openOrganizationModal])
+  }, [openSuccessModal])
   return (
     <>
       <SafeAreaWrapper isDefaultBgColor edges={['left', 'right', 'bottom']}>
@@ -42,9 +41,9 @@ export const Dashboard = () => {
           <DashboardTeamMember closeModal={() => setIsModalVisible(false)} user={modalUser} />
         </SwipeableModal>
       )}
-      <BottomSheetModalComponent snapPoints={['90%']} modalRef={organizationModalRef}>
-        <TeamsModal closeModal={closeOrganizationModal} />
-      </BottomSheetModalComponent>
+      <SwipeableModal isOpen={isSuccessModalVisible} onHide={closeSuccessModal}>
+        <TeamsModal closeModal={closeSuccessModal} />
+      </SwipeableModal>
     </>
   )
 }
