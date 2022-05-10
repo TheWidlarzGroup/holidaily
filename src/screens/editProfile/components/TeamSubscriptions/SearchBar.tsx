aa0bc67cfed75dbd5f94d87471976872bf4695f4
Lukasz Baranowski <lukaszb48@gmail.com>
2022-05-10 09:16:15 +0200

    adjusted input

--- src/screens/editProfile/components/TeamSubscriptions/SearchBar.tsx
+++ src/screens/editProfile/components/TeamSubscriptions/SearchBar.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import IconSearch from 'assets/icons/icon-search.svg'
-import IconClose from 'assets/icons/icon-circle-cross.svg'
 import { RectButton, TextInput } from 'react-native-gesture-handler'
 import { Box, mkUseStyles, Theme, useTheme } from 'utils/theme'
+import { InputSearchIcon } from 'components/InputSearchIcon'
+import DeleteIcon from 'assets/icons/icon-delete.svg'
 
 type SearchBarProps = {
   searchFilter: F1<string>
@@ -15,13 +15,15 @@ export const SearchBar = ({ searchFilter, searchPhrase }: SearchBarProps) => {
   return (
     <Box position="relative" marginTop="lplus" marginBottom="l">
       <TextInput style={styles.searchInput} onChangeText={searchFilter} value={searchPhrase} />
-      <IconSearch style={styles.searchIcon} />
+      <Box position="absolute" top={-20}>
+        <InputSearchIcon />
+      </Box>
       <RectButton
         rippleColor={theme.colors.rippleColor}
         activeOpacity={0.5}
         style={styles.dropSearchBtn}
         onPress={() => searchFilter('')}>
-        <IconClose />
+        <DeleteIcon width={20} height={20} />
       </RectButton>
     </Box>
   )
@@ -29,16 +31,16 @@ export const SearchBar = ({ searchFilter, searchPhrase }: SearchBarProps) => {
 const useStyles = mkUseStyles((theme: Theme) => ({
   searchIcon: {
     position: 'absolute',
-    top: 0,
+    top: 7,
     left: 10,
   },
   searchInput: {
     backgroundColor: theme.colors.white,
     justifyContent: 'center',
-    height: 36,
+    height: 40,
     borderRadius: theme.borderRadii.xxl,
-    paddingHorizontal: theme.spacing.m,
-    paddingLeft: theme.spacing.xxl,
+    paddingRight: theme.spacing.xl,
+    paddingLeft: theme.spacing.xl,
   },
   dropSearchBtn: {
     position: 'absolute',
