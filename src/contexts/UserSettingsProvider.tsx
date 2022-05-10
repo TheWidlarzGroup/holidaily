9f8134216b500bfe39075124ee983efba104fb76
asgalecki <a.s.galecki@gmail.com>
2022-05-10 10:27:03 +0200

    Connect UserSettingsContext with localStorage

--- src/contexts/UserSettingsProvider.tsx
+++ src/contexts/UserSettingsProvider.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode, useState, useCallback } from 'react'
+import React, { ReactNode, useState, useEffect } from 'react'
+import { getItem, setItem } from 'utils/localStorage'
 import { UserSettingsContextProps, UserSettings, UserSettingsContext } from './UserSettingsContext'
 
 type ProviderProps = {
@@ -11,13 +12,28 @@ export const defaultUserSettings: UserSettings = {
 
 export const UserSettingsContextProvider = ({ children }: ProviderProps) => {
   const [userSettings, setUserSettings] = useState<UserSettings | null>(null)
-  const updateSettings = useCallback(
-    (newData: Partial<UserSettings> | null) =>
-      setUserSettings((settings) =>
-        settings ? { ...settings, ...newData } : { ...defaultUserSettings, ...newData }
-      ),
-    []
-  )
+
+  const updateSettings = (newData: Partial<UserSettings> | null) => {
+    if (userSettings && newData) {
+      setUserSettings({ ...userSettings, ...newData })
+    }
+  }
+
+  useEffect(() => {
+    getItem('userSettings')
+      .then((res) => {
+        if (res) {
+          setUserSettings(JSON.parse(res))
+        } else {
+          setUserSettings(defaultUserSettings)
+        }
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    setItem('userSettings', JSON.stringify(userSettings))
+  }, [userSettings])
 
   const value: UserSettingsContextProps = { userSettings, updateSettings }
   return <UserSettingsContext.Provider value={value}>{children}</UserSettingsContext.Provider>
