0bb6a91743c22b46946d405c16d1cf659834e10f
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-10 11:28:28 +0200

    fix tab habdler rounded corners

--- src/navigation/BottomNavComponents/TabsHandler.tsx
+++ src/navigation/BottomNavComponents/TabsHandler.tsx
@@ -51,6 +51,8 @@ export const TabsHandler: FC<TabsHandlerProps> = ({
             alignItems="center"
             flexDirection="column"
             backgroundColor="white"
+            borderTopRightRadius={key === 1 ? 's' : undefined}
+            borderTopLeftRadius={key === 3 ? 's' : undefined}
             zIndex="5">
             <BorderlessButton onPress={onPress} style={styles.button}>
               {getBottomTabIcon(
