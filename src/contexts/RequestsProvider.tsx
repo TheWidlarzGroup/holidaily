af5ec229bf40923c6a8a3a9b8c3918b80d06893e
Lukasz Baranowski <lukaszb48@gmail.com>
2022-05-12 09:49:42 +0200

    added weekends to calendar

--- src/contexts/RequestsProvider.tsx
+++ src/contexts/RequestsProvider.tsx
@@ -1,5 +1,9 @@
 import React, { ReactNode, useState, useCallback, useEffect } from 'react'
 import { MonthType, useGetHolidayRequests } from 'hooks/useGetHolidayRequests'
+import { eachDayOfInterval, lastDayOfMonth } from 'date-fns'
+import { getISODateString } from 'utils/dates'
+import { DayInfoProps } from 'screens/calendar/components/DayInfo'
+import { sortByRequestDate } from 'utils/sortByDate'
 import { RequestContextProps, RequestsContext } from './RequestsContext'
 
 type RequestProviderProps = {
@@ -16,7 +20,38 @@ export const RequestsContextProvider = ({ children }: RequestProviderProps) => {
 
   useEffect(() => {
     if (allMonths.length > 0) {
-      setRequests(allMonths)
+      const monthsWithWeekendDays = allMonths.map((month) => {
+        const firstMonthDay = new Date(`${month.date}-01`)
+        const eachDayOfMonth = eachDayOfInterval({
+          start: new Date(firstMonthDay),
+          end: new Date(lastDayOfMonth(firstMonthDay)),
+        })
+
+        const days: DayInfoProps[] = []
+        eachDayOfMonth.forEach((day) => {
+          if (day.getDay() === 0 || day.getDay() === 6) {
+            days.push({ date: getISODateString(day) })
+          }
+        })
+        return {
+          date: month.date,
+          days,
+        }
+      })
+
+      const mergedMonths: MonthType[] = []
+      allMonths.forEach((x) => {
+        monthsWithWeekendDays.forEach((y) => {
+          if (x.date === y.date) {
+            const days = [...x.days, ...y.days].sort(sortByRequestDate)
+            mergedMonths.push({
+              date: x.date,
+              days,
+            })
+          }
+        })
+      })
+      setRequests(mergedMonths)
     }
   }, [allMonths])
 
