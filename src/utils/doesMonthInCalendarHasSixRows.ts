2c1492fe44c5deafee50004b39fcb0b2496b5431
Lukasz Baranowski <lukaszb48@gmail.com>
2022-05-13 12:29:20 +0200

    adjustment

--- src/utils/doesMonthInCalendarHasSixRows.ts
+++ src/utils/doesMonthInCalendarHasSixRows.ts
@@ -4,11 +4,7 @@ export const doesMonthInCalendarHasSixRows = (month: Date) => {
   const daysInMonth = getDaysInMonth(new Date(month))
   const monthYear = format(new Date(month), 'yyyy-MM-01')
   const firstDayOfMonth = format(new Date(monthYear), 'EEE')
-  if (daysInMonth === 30 && firstDayOfMonth === 'Sun') {
-    return true
-  }
-  if ((daysInMonth === 31 && firstDayOfMonth === 'Sat') || firstDayOfMonth === 'Sun') {
-    return true
-  }
+  if (daysInMonth === 30 && firstDayOfMonth === 'Sun') return true
+  if ((daysInMonth === 31 && firstDayOfMonth === 'Sat') || firstDayOfMonth === 'Sun') return true
   return false
 }
