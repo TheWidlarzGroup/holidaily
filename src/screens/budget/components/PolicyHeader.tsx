57ca0ca2a02e80009cb683f1a6c718340cfb055c
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-14 17:16:04 +0200

    fix

--- src/screens/budget/components/PolicyHeader.tsx
+++ src/screens/budget/components/PolicyHeader.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Box, Text, BaseOpacity, useTheme } from 'utils/theme'
 import CloseIcon from 'assets/icons/icon-close.svg'
+import { useNavigation } from '@react-navigation/native'
 
-export const PolicyHeader = ({ closeModal }: { closeModal: F0 }) => {
+export const PolicyHeader = () => {
   const { t } = useTranslation('budget')
+  const { goBack } = useNavigation()
   const theme = useTheme()
   return (
     <Box flexDirection="row" alignItems="center" paddingHorizontal="l" paddingTop="m">
-      <BaseOpacity onPress={closeModal}>
+      <BaseOpacity onPress={goBack}>
         <CloseIcon width={14} height={14} color={theme.colors.black} />
       </BaseOpacity>
       <Box flex={1}>
--- src/screens/editProfile/components/TeamSubscriptions/SearchHeader.tsx
+++ src/screens/editProfile/components/TeamSubscriptions/SearchHeader.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { Box, mkUseStyles, Text } from 'utils/theme'
-import IconBack from 'assets/icons/icon-back2.svg'
+import { BaseOpacity, Box, mkUseStyles, Text } from 'utils/theme'
+import CloseIcon from 'assets/icons/icon-close.svg'
 
 export const SearchHeader = ({ handleGoBack }: { handleGoBack: F0 }) => {
   const { t } = useTranslation('userProfile')
   const styles = useStyles()
   return (
     <Box flexDirection="row" justifyContent="space-between" alignItems="center">
-      <TouchableOpacity activeOpacity={0.2} onPress={handleGoBack}>
-        <IconBack height={18} width={18} color={styles.arrow.color} />
-      </TouchableOpacity>
+      <BaseOpacity padding="m" activeOpacity={0.2} onPress={handleGoBack}>
+        <CloseIcon style={styles.closeIcon} />
+      </BaseOpacity>
       <Text variant="boldBlackCenter20">{t('subscribeMoreTeams')}</Text>
       <Box paddingRight="s" />
     </Box>
@@ -19,7 +18,11 @@ export const SearchHeader = ({ handleGoBack }: { handleGoBack: F0 }) => {
 }
 
 const useStyles = mkUseStyles((theme) => ({
-  arrow: {
-    color: theme.colors.black,
+  closeIcon: {
+    color: theme.colors.titleActive,
+    width: 12,
+    height: 12,
+    paddingTop: theme.spacing.m,
+    paddingLeft: theme.spacing.m,
   },
 }))
