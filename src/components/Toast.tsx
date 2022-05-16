2771b92da66f70186c4691b9cc35c32094224912
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-16 14:26:46 +0200

    add toast animation and fade out

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,5 +1,11 @@
 import { BoxProps } from '@shopify/restyle'
-import React from 'react'
+import React, { useEffect } from 'react'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated'
 import { exhaustiveTypeCheck } from 'utils/functions'
 import { Box, mkUseStyles, Text, Theme } from 'utils/theme'
 import { CircleStatusIcon, IconStatus } from './CircleStatusIcon'
@@ -7,7 +13,48 @@ import { CircleStatusIcon, IconStatus } from './CircleStatusIcon'
 type ToastProps = {
   variant: 'success'
   text: string
+  onHide: F0
+  visibilityTime?: number
 } & BoxProps<Theme>
+
+export const Toast = ({
+  variant,
+  text,
+  onHide,
+  visibilityTime = 1200,
+  ...styleProps
+}: ToastProps) => {
+  const styles = useStyles()
+  const iconStatus: IconStatus = variant
+  const translateY = useSharedValue<number>(-100)
+
+  useEffect(() => {
+    translateY.value = withDelay(100, withTiming(0))
+    let timeout = setTimeout(() => {
+      translateY.value = withTiming(-200)
+      timeout = setTimeout(onHide, 300)
+    }, visibilityTime)
+    return () => clearTimeout(timeout)
+  }, [translateY, onHide, visibilityTime])
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+  }))
+  return (
+    <AnimatedBox style={[styles.container, animatedStyle]}>
+      <Box
+        borderRadius="l1min"
+        paddingVertical="ml"
+        paddingHorizontal="xm"
+        alignItems="center"
+        flexDirection="row"
+        {...getVariantStyle(variant)}
+        {...styleProps}>
+        <CircleStatusIcon width={25} status={iconStatus} />
+        <Text variant="textSM">{text}</Text>
+      </Box>
+    </AnimatedBox>
+  )
+}
 
 const getVariantStyle = (variant: ToastProps['variant']): BoxProps<Theme> => {
   switch (variant) {
@@ -23,26 +70,6 @@ const getVariantStyle = (variant: ToastProps['variant']): BoxProps<Theme> => {
       return {}
   }
 }
-export const Toast = ({ variant, text, ...styleProps }: ToastProps) => {
-  const styles = useStyles()
-  const iconStatus: IconStatus = variant
-
-  return (
-    <Box style={styles.container}>
-      <Box
-        borderRadius="l1min"
-        paddingVertical="ml"
-        paddingHorizontal="xm"
-        alignItems="center"
-        flexDirection="row"
-        {...getVariantStyle(variant)}
-        {...styleProps}>
-        <CircleStatusIcon width={25} status={iconStatus} />
-        <Text variant="textSM">{text}</Text>
-      </Box>
-    </Box>
-  )
-}
 
 const useStyles = mkUseStyles((theme: Theme) => ({
   container: {
@@ -53,3 +80,5 @@ const useStyles = mkUseStyles((theme: Theme) => ({
     zIndex: theme.zIndices['20'],
   },
 }))
+
+const AnimatedBox = Animated.createAnimatedComponent(Box)
