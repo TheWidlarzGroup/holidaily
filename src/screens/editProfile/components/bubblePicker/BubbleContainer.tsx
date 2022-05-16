596a76b504dad6371504870363cb227934ccb576
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-16 18:23:47 +0200

    add info placeholder

--- src/screens/editProfile/components/bubblePicker/BubbleContainer.tsx
+++ src/screens/editProfile/components/bubblePicker/BubbleContainer.tsx
@@ -5,9 +5,11 @@ import { useNavigation } from '@react-navigation/native'
 import { Box, Text, mkUseStyles, Theme, useTheme } from 'utils/theme'
 import { shadow } from 'utils/theme/shadows'
 import IconBack from 'assets/icons/icon-back2.svg'
+import Info from 'assets/icons/icon-info.svg'
 import { UserProfileNavigationProps } from 'navigation/types'
 import { useTranslation } from 'react-i18next'
 import { windowWidth } from 'utils/deviceSizes'
+import { useBooleanState } from 'hooks/useBooleanState'
 import { Bubble } from './Bubble'
 import { useBubbles } from './useBubbles'
 import { CheckMark } from './Checkmark'
@@ -18,8 +20,6 @@ export const BubbleContainer = ({
 }: UserProfileNavigationProps<'ColorPicker'>) => {
   const styles = useStyles()
   const theme = useTheme()
-  const { t } = useTranslation('userProfile')
-  const { goBack } = useNavigation()
   const [dropColor, setDropColor] = useState(theme.colors.colorPickerDropArea)
   const { animatedDrop, bubbles, animateCheckmark, dropArea, animateDropArea } = useBubbles()
 
@@ -30,18 +30,9 @@ export const BubbleContainer = ({
   return (
     <View style={styles.mainContainer}>
       {animateCheckmark && <CheckMark animateCheckmark={animateCheckmark} />}
-      <TouchableOpacity onPress={goBack} style={styles.backBtn} activeOpacity={0.2}>
-        <IconBack color={theme.colors.alwaysWhite} />
-      </TouchableOpacity>
-      <Box marginTop="xxxl" alignItems="center" style={{ width: '100%' }}>
-        <Text variant="displayBoldSM" marginBottom="m" color="alwaysWhite">
-          {t('colorPicker')}
-        </Text>
-        <Text variant="textSM" color="alwaysWhite">
-          {t('colorPickerSubtitle')}
-        </Text>
-      </Box>
-      <Animated.View
+      <BubbleContainerButtons />
+      <BubbleContainerHeader />
+      <DropArea
         style={[
           styles.dropArea,
           animatedDrop,
@@ -65,12 +56,56 @@ export const BubbleContainer = ({
     </View>
   )
 }
+
+const BubbleContainerButtons = () => {
+  const styles = useStyles()
+  const theme = useTheme()
+  const { goBack } = useNavigation()
+  const [isInfoVisible, { setTrue: showInfo, setFalse: hideInfo }] = useBooleanState(false)
+  return (
+    <>
+      <Box position="absolute" height={200} width="100%">
+        <Box flexDirection="row">
+          <TouchableOpacity onPress={goBack} style={styles.backBtn} activeOpacity={0.2}>
+            <IconBack color={theme.colors.alwaysWhite} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={showInfo} style={styles.info} activeOpacity={0.2}>
+            <Info color={theme.colors.alwaysWhite} />
+          </TouchableOpacity>
+        </Box>
+        {isInfoVisible && <Box flex={1} marginHorizontal="m" marginTop="s" bg="alwaysWhite"></Box>}
+      </Box>
+    </>
+  )
+}
+
+const BubbleContainerHeader = () => {
+  const { t } = useTranslation('userProfile')
+  return (
+    <Box marginTop="xxxl" alignItems="center" style={{ width: '100%' }}>
+      <Text variant="displayBoldSM" marginTop="l" marginBottom="m" color="alwaysWhite">
+        {t('colorPicker')}
+      </Text>
+      <Text variant="textSM" color="alwaysWhite">
+        {t('colorPickerSubtitle')}
+      </Text>
+    </Box>
+  )
+}
+
+const DropArea = Animated.createAnimatedComponent(Box)
+
 const useStyles = mkUseStyles((theme: Theme) => ({
   backBtn: {
     padding: theme.spacing.m,
-    position: 'absolute',
     top: 44,
     left: 3,
+    zIndex: theme.zIndices['2'],
+  },
+  info: {
+    padding: theme.spacing.m,
+    top: 30,
+    marginLeft: 'auto',
     zIndex: theme.zIndices['2'],
   },
   mainContainer: {
