8fe9245ed12d1ad3e5f6e4cdeb18baa4b4b44ea2
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-17 11:18:12 +0200

    refactor

--- src/screens/editProfile/components/bubblePicker/BubbleContainer.tsx
+++ src/screens/editProfile/components/bubblePicker/BubbleContainer.tsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
 import Animated from 'react-native-reanimated'
-import { useNavigation } from '@react-navigation/native'
-import { Box, Text, mkUseStyles, Theme, useTheme, BaseOpacity } from 'utils/theme'
+import { Box, mkUseStyles, Theme, useTheme } from 'utils/theme'
 import { shadow } from 'utils/theme/shadows'
-import IconBack from 'assets/icons/icon-back2.svg'
-import Info from 'assets/icons/icon-info.svg'
-import CrossIcon from 'assets/icons/icon-close.svg'
 import { UserProfileNavigationProps } from 'navigation/types'
-import { useTranslation } from 'react-i18next'
 import { windowWidth } from 'utils/deviceSizes'
-import { useBooleanState } from 'hooks/useBooleanState'
 import { Bubble } from './Bubble'
 import { useBubbles } from './useBubbles'
 import { CheckMark } from './Checkmark'
 import { BUBBLE_CONSTANTS as C } from './BubbleHelper'
+import { BubbleContainerButtons } from './BubbleContainerButtons'
+import { BubbleContainerHeader } from './BubbleContainerHeader'
+
+const DropArea = Animated.createAnimatedComponent(Box)
 
 export const BubbleContainer = ({
   route: { params: p },
@@ -58,78 +56,7 @@ export const BubbleContainer = ({
   )
 }
 
-const BubbleContainerButtons = () => {
-  const styles = useStyles()
-  const theme = useTheme()
-  const { goBack } = useNavigation()
-  const { t } = useTranslation('userProfile')
-  const [isInfoVisible, { setTrue: showInfo, setFalse: hideInfo }] = useBooleanState(false)
-  return (
-    <>
-      <Box position="absolute" zIndex="10" width="100%">
-        <Box flexDirection="row">
-          <BaseOpacity onPress={goBack} style={styles.backBtn} activeOpacity={0.2}>
-            <IconBack color={theme.colors.alwaysWhite} />
-          </BaseOpacity>
-          <BaseOpacity onPress={showInfo} style={styles.info} activeOpacity={0.2}>
-            <Info color={theme.colors.alwaysWhite} />
-          </BaseOpacity>
-        </Box>
-        {isInfoVisible && (
-          <Box
-            flex={1}
-            marginHorizontal="m"
-            marginTop="m"
-            bg="white"
-            borderRadius="l1min"
-            padding="m"
-            flexDirection="row">
-            <Text variant="textSM" style={styles.infoText}>
-              {t('colorPickerInfo')}
-            </Text>
-            <BaseOpacity
-              onPress={hideInfo}
-              alignSelf="flex-start"
-              padding="s"
-              top={theme.spacing['-xs']}>
-              <CrossIcon color={theme.colors.titleActive} />
-            </BaseOpacity>
-          </Box>
-        )}
-      </Box>
-    </>
-  )
-}
-
-const BubbleContainerHeader = () => {
-  const { t } = useTranslation('userProfile')
-  return (
-    <Box marginTop="xxxl" alignItems="center" style={{ width: '100%' }}>
-      <Text variant="displayBoldSM" marginTop="l" marginBottom="m" color="alwaysWhite">
-        {t('colorPicker')}
-      </Text>
-      <Text variant="textSM" color="alwaysWhite">
-        {t('colorPickerSubtitle')}
-      </Text>
-    </Box>
-  )
-}
-
-const DropArea = Animated.createAnimatedComponent(Box)
-
 const useStyles = mkUseStyles((theme: Theme) => ({
-  backBtn: {
-    padding: theme.spacing.m,
-    top: 44,
-    left: 3,
-    zIndex: theme.zIndices['2'],
-  },
-  info: {
-    padding: theme.spacing.m,
-    top: 30,
-    marginLeft: 'auto',
-    zIndex: theme.zIndices['2'],
-  },
   mainContainer: {
     flex: 1,
     backgroundColor: theme.colors.colorPickerBackdrop,
@@ -148,8 +75,5 @@ const useStyles = mkUseStyles((theme: Theme) => ({
   },
   scaleCheckmark: {
     transform: [{ scale: 2 }],
-  },
-  infoText: {
-    flex: 1,
   },
 }))
---
+++ src/screens/editProfile/components/bubblePicker/BubbleContainerButtons.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Box, Text, mkUseStyles, Theme, useTheme, BaseOpacity } from 'utils/theme'
+import IconBack from 'assets/icons/icon-back2.svg'
+import Info from 'assets/icons/icon-info.svg'
+import CrossIcon from 'assets/icons/icon-close.svg'
+import { useTranslation } from 'react-i18next'
+import { useBooleanState } from 'hooks/useBooleanState'
+
+export const BubbleContainerButtons = () => {
+  const styles = useStyles()
+  const theme = useTheme()
+  const { goBack } = useNavigation()
+  const { t } = useTranslation('userProfile')
+  const [isInfoVisible, { setTrue: showInfo, setFalse: hideInfo }] = useBooleanState(false)
+  return (
+    <>
+      <Box position="absolute" zIndex="10" width="100%">
+        <Box flexDirection="row">
+          <BaseOpacity onPress={goBack} style={styles.backBtn} activeOpacity={0.2}>
+            <IconBack color={theme.colors.alwaysWhite} />
+          </BaseOpacity>
+          <BaseOpacity onPress={showInfo} style={styles.info} activeOpacity={0.2}>
+            <Info color={theme.colors.alwaysWhite} />
+          </BaseOpacity>
+        </Box>
+        {isInfoVisible && (
+          <Box
+            flex={1}
+            marginHorizontal="m"
+            marginTop="m"
+            bg="white"
+            borderRadius="l1min"
+            padding="m"
+            flexDirection="row">
+            <Text variant="textSM" style={styles.infoText}>
+              {t('colorPickerInfo')}
+            </Text>
+            <BaseOpacity
+              onPress={hideInfo}
+              alignSelf="flex-start"
+              padding="s"
+              top={theme.spacing['-xs']}>
+              <CrossIcon color={theme.colors.titleActive} />
+            </BaseOpacity>
+          </Box>
+        )}
+      </Box>
+    </>
+  )
+}
+
+const useStyles = mkUseStyles((theme: Theme) => ({
+  backBtn: {
+    padding: theme.spacing.m,
+    top: 44,
+    left: 3,
+    zIndex: theme.zIndices['2'],
+  },
+  info: {
+    padding: theme.spacing.m,
+    top: 30,
+    marginLeft: 'auto',
+    zIndex: theme.zIndices['2'],
+  },
+  infoText: {
+    flex: 1,
+  },
+}))
