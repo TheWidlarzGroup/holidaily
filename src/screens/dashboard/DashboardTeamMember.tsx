52c0982a8c58a63c4190357d143df0b8c28401e1
Lukasz Baranowski <lukaszb48@gmail.com>
2022-05-17 11:46:01 +0200

    added teams to dashboard user modal

--- src/screens/dashboard/DashboardTeamMember.tsx
+++ src/screens/dashboard/DashboardTeamMember.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box } from 'utils/theme'
+import { Box, Text } from 'utils/theme'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import IconBack from 'assets/icons/icon-back2.svg'
 import { MateHeader } from 'screens/dashboard/components/MateHeader'
@@ -7,52 +7,79 @@ import { MateHoliday } from 'screens/dashboard/components/MateHoliday'
 import { MateHolidayDetail } from 'screens/dashboard/components/MateHolidayDetail'
 import { TouchableOpacity } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import { User } from 'mock-api/models/mirageTypes'
+import { Team, User } from 'mock-api/models/mirageTypes'
 import { isIos } from 'utils/layout'
+import { useTranslation } from 'react-i18next'
 
 type MemberProps = { user: User; closeModal: F0 }
 
-export const DashboardTeamMember = ({ user, closeModal }: MemberProps) => (
-  <SafeAreaWrapper isDefaultBgColor>
-    <Box
-      padding="m"
-      paddingTop="xl"
-      borderTopLeftRadius="m"
-      borderTopRightRadius="m"
-      backgroundColor="white"
-      flexGrow={1}
-      marginTop={isIos ? '-l' : 'none'}>
-      <ScrollView showsHorizontalScrollIndicator={false}>
-        <TouchableOpacity
-          onPress={closeModal}
-          hitSlop={{ top: 30, bottom: 30, left: 30, right: 30 }}>
-          <IconBack height={18} width={18} color="black" />
-        </TouchableOpacity>
-        <MateHeader user={user} />
-        {!!user?.requests[0] && (
-          <>
-            <MateHoliday user={user} />
-            <Box flexDirection="row">
-              <MateHolidayDetail type="start" date={user?.requests[0].startDate || ''} />
-              <MateHolidayDetail type="end" date={user?.requests[0].endDate || ''} />
-            </Box>
-          </>
-        )}
-        {!!user?.requests[1] && (
-          <>
-            <MateHoliday user={user} isNextRequest />
-          </>
-        )}
-        {/* TODO: Display teams that user belongs to */}
-        {/* {user.teams.length > 0 &&
-          user.teams.map((team) => {
-            return (
-              <Box>
-                <Text variant='textSM'>{team}</Text>
+export const DashboardTeamMember = ({ user, closeModal }: MemberProps) => {
+  const { t } = useTranslation('dashboard')
+
+  return (
+    <SafeAreaWrapper isDefaultBgColor>
+      <Box
+        padding="m"
+        paddingTop="xl"
+        borderTopLeftRadius="m"
+        borderTopRightRadius="m"
+        backgroundColor="white"
+        flexGrow={1}
+        marginTop={isIos ? '-l' : 'none'}>
+        <ScrollView showsHorizontalScrollIndicator={false}>
+          <TouchableOpacity
+            onPress={closeModal}
+            hitSlop={{ top: 30, bottom: 30, left: 30, right: 30 }}>
+            <IconBack height={18} width={18} color="black" />
+          </TouchableOpacity>
+          <MateHeader user={user} />
+          {!!user?.requests[0] && (
+            <>
+              <MateHoliday user={user} />
+              <Box flexDirection="row">
+                <MateHolidayDetail type="start" date={user?.requests[0].startDate || ''} />
+                <MateHolidayDetail type="end" date={user?.requests[0].endDate || ''} />
               </Box>
-            )
-          })} */}
-      </ScrollView>
-    </Box>
-  </SafeAreaWrapper>
-)
+            </>
+          )}
+          {!!user?.requests[1] && (
+            <>
+              <MateHoliday user={user} isNextRequest />
+            </>
+          )}
+          {user.teams.length > 0 ? (
+            <>
+              <Text
+                variant="displayXS"
+                color="darkGrey"
+                letterSpacing={0.7}
+                paddingTop="m"
+                marginBottom="s">
+                {t('teams').toUpperCase()}
+              </Text>
+              <Box flexDirection="row" flexWrap="wrap">
+                {user.teams.map((team: Team | string) => {
+                  const teamName = typeof team === 'string' ? team : team.name
+                  return (
+                    <Box
+                      key={teamName}
+                      height={37}
+                      paddingHorizontal="m"
+                      marginRight="s"
+                      marginTop="s"
+                      backgroundColor="input"
+                      justifyContent="center"
+                      alignItems="center"
+                      borderRadius="l">
+                      <Text variant="textSM">{teamName}</Text>
+                    </Box>
+                  )
+                })}
+              </Box>
+            </>
+          ) : null}
+        </ScrollView>
+      </Box>
+    </SafeAreaWrapper>
+  )
+}
