20d80cf20618c0e494bcd98d1022cffcccab59b1
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-18 17:38:11 +0200

    fix teams modal content overlapping on small screens

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { DashboardHeader } from 'screens/dashboard/components/DashboardHeader'
 import { TeamsModal } from 'screens/welcome/components/TeamsModal'
 import { getItem, setItem } from 'utils/localStorage'
+import { isScreenHeightShort } from 'utils/deviceSizes'
 import { SwipeableModal } from 'components/SwipeableModal'
 import { useBooleanState } from 'hooks/useBooleanState'
 import { SortableTeams } from './components/SortableTeams'
@@ -11,25 +12,37 @@ export const Dashboard = () => {
   const [isSuccessModalVisible, { setFalse: closeSuccessModal, setTrue: openSuccessModal }] =
     useBooleanState(false)
   useEffect(() => {
+    let timeout: number
     const openModalOnFirstAppLaunch = async () => {
       const seenTeamsModal = await getItem('seenTeamsModal')
       if (seenTeamsModal === 'false') return
-      setTimeout(async () => {
+      // Comment: Modal just flickers in instead of sliding from the bottom without setTimeout. setImmediate doesn't fix the issue.
+      timeout = setTimeout(() => {
         openSuccessModal()
-        await setItem('seenTeamsModal', 'false')
-      }, 2000)
+        setItem('seenTeamsModal', 'false')
+      }, 1)
     }
     openModalOnFirstAppLaunch()
+    return () => clearTimeout(timeout)
   }, [openSuccessModal])
+
   return (
     <>
       <SafeAreaWrapper isDefaultBgColor edges={['left', 'right', 'bottom']}>
         <DashboardHeader />
         <SortableTeams />
       </SafeAreaWrapper>
-      <SwipeableModal isOpen={isSuccessModalVisible} onHide={closeSuccessModal}>
+      <SwipeableModal
+        style={teamsModalStyle}
+        isOpen={isSuccessModalVisible}
+        onHide={closeSuccessModal}>
         <TeamsModal closeModal={closeSuccessModal} />
       </SwipeableModal>
     </>
   )
+}
+
+const teamsModalStyle = {
+  margin: 0,
+  marginTop: isScreenHeightShort ? 20 : 110,
 }
