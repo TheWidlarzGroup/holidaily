4594a8e58fa2c3a4a527407289f23a7b574784f7
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-19 08:51:30 +0200

    remove safearea from modal (#559)

--- src/components/SwipeableModalRegular.tsx
+++ src/components/SwipeableModalRegular.tsx
@@ -1,14 +1,11 @@
-import { useBooleanState } from 'hooks/useBooleanState'
 import React, { PropsWithChildren } from 'react'
 import { ScrollView, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
-import Modal, { ModalProps } from 'react-native-modal'
+import { ModalProps } from 'react-native-modal'
 import { isIos } from 'utils/layout'
 import { Box } from 'utils/theme'
 import { CustomButton } from './CustomButton'
-import { SafeAreaWrapper } from './SafeAreaWrapper'
+import { SwipeableModal } from './SwipeableModal'
 import { SwipeableModalHeader } from './SwipeableModalHeader'
-
-const DEFAULT_MODAL_ANIM_TIME = 300
 
 type SwipeableModalTempProps = PropsWithChildren<
   {
@@ -28,79 +25,46 @@ type SwipeableModalTempProps = PropsWithChildren<
 >
 
 export const SwipeableModalRegular = (props: SwipeableModalTempProps) => {
-  // COMMENT: we keep internal state to schedule parent rerender after the modal hide animation is finished. Otherwise we would experience lag between swipe gesture and hide animation
-  const [isVisible, { setFalse: fadeOut, setTrue: resetState }] = useBooleanState(true)
-
-  const modalHideHandler = () => {
-    props.onHide()
-    resetState()
-  }
-
   const btnStyle = { marginTop: 'auto' }
-  const modalStyle = { margin: 0, marginTop: 110 }
   const btnLabel = props.buttonLabel ? props.buttonLabel : ''
-
   return (
-    <Modal
-      hasBackdrop
-      coverScreen
-      style={modalStyle}
-      swipeThreshold={20}
-      statusBarTranslucent
-      backdropColor="black"
-      backdropOpacity={0.6}
-      swipeDirection="down"
-      animationIn="slideInUp"
-      onSwipeComplete={fadeOut}
-      onBackdropPress={fadeOut}
-      onBackButtonPress={fadeOut}
-      animationOut="slideOutDown"
-      useNativeDriverForBackdrop
-      hideModalContentWhileAnimating
-      isVisible={props.isOpen && isVisible}
-      onModalHide={modalHideHandler}
-      animationInTiming={DEFAULT_MODAL_ANIM_TIME}
-      animationOutTiming={DEFAULT_MODAL_ANIM_TIME}
-      propagateSwipe
-      {...props}>
-      <Box flex={1} borderTopLeftRadius="m" borderTopRightRadius="l" overflow="hidden">
-        <SafeAreaWrapper isDefaultBgColor>
-          <Box
-            flexGrow={1}
-            paddingTop="xm"
-            paddingHorizontal="m"
-            backgroundColor="white"
-            borderTopLeftRadius="m"
-            borderTopRightRadius="m"
-            marginTop={isIos ? '-l' : 'none'}>
-            <ScrollView showsHorizontalScrollIndicator={false}>
-              <TouchableOpacity>
-                <TouchableWithoutFeedback>
-                  <>
-                    <SwipeableModalHeader
-                      title={props.title}
-                      subtitle={props.subtitle}
-                      aboutIcon={props.aboutIcon}
-                      closeModal={modalHideHandler}
-                      hasIndicator={props.hasIndicator}
-                      closeAction={props.closeAction || 'close'}
-                      aboutAction={props.aboutAction || modalHideHandler}
-                    />
-                    {props.children}
-                    {props.buttonLabel && (
-                      <Box paddingBottom="lplus" style={btnStyle}>
-                        <TouchableOpacity onPress={props.buttonAction || modalHideHandler}>
-                          <CustomButton label={btnLabel} variant="primary" />
-                        </TouchableOpacity>
-                      </Box>
-                    )}
-                  </>
-                </TouchableWithoutFeedback>
-              </TouchableOpacity>
-            </ScrollView>
-          </Box>
-        </SafeAreaWrapper>
+    <SwipeableModal propagateSwipe {...props}>
+      <Box flex={1} borderTopLeftRadius="l1min" borderTopRightRadius="l1min" overflow="hidden">
+        <Box
+          flexGrow={1}
+          paddingTop="xm"
+          paddingHorizontal="m"
+          backgroundColor="white"
+          borderTopLeftRadius="m"
+          borderTopRightRadius="m"
+          marginTop={isIos ? '-l' : 'none'}>
+          <ScrollView showsHorizontalScrollIndicator={false}>
+            <TouchableOpacity activeOpacity={1}>
+              <TouchableWithoutFeedback>
+                <>
+                  <SwipeableModalHeader
+                    title={props.title}
+                    subtitle={props.subtitle}
+                    aboutIcon={props.aboutIcon}
+                    closeModal={props.onHide}
+                    hasIndicator={props.hasIndicator}
+                    closeAction={props.closeAction || 'close'}
+                    aboutAction={props.aboutAction || props.onHide}
+                  />
+                  {props.children}
+                  {props.buttonLabel && (
+                    <Box paddingBottom="lplus" style={btnStyle}>
+                      <TouchableOpacity onPress={props.buttonAction || props.onHide}>
+                        <CustomButton label={btnLabel} variant="primary" />
+                      </TouchableOpacity>
+                    </Box>
+                  )}
+                </>
+              </TouchableWithoutFeedback>
+            </TouchableOpacity>
+          </ScrollView>
+        </Box>
       </Box>
-    </Modal>
+    </SwipeableModal>
   )
 }
