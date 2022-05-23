720ae755138b418a1af63d8ee914a377b55c6547
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-23 17:02:28 +0200

    fix distant in future calendar months issues

--- src/utils/getWeekendDays.ts
+++ src/utils/getWeekendDays.ts
@@ -4,20 +4,25 @@ import { getISODateString, isWeekend } from './dates'
 
 export const getWeekendDays = (months: string[]) =>
   months.map((month) => {
-    const firstMonthDay = new Date(`${month}-01`)
-    const eachDayOfMonth = eachDayOfInterval({
-      start: new Date(firstMonthDay),
-      end: new Date(lastDayOfMonth(firstMonthDay)),
-    })
-
-    const days: DayInfoProps[] = []
-    eachDayOfMonth.forEach((day) => {
-      if (isWeekend(day)) {
-        days.push({ date: getISODateString(day) })
-      }
-    })
+    const firstDayOfMonth = new Date(`${month}-01`)
+    const days = eachWeekendDaysOfMonth(firstDayOfMonth)
     return {
       date: month,
       days,
     }
   })
+
+export const eachWeekendDaysOfMonth = (firstDayOfMonth: Date) => {
+  const eachDayOfMonth = eachDayOfInterval({
+    start: new Date(firstDayOfMonth),
+    end: new Date(lastDayOfMonth(firstDayOfMonth)),
+  })
+
+  const days: DayInfoProps[] = []
+  eachDayOfMonth.forEach((day) => {
+    if (isWeekend(day)) {
+      days.push({ date: getISODateString(day) })
+    }
+  })
+  return days
+}
