10183b203d602cffe5bc124c8e3b799d1fc2a0c7
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-23 13:40:05 +0200

    remove isFromWelcomeScreen from about

--- src/screens/about/components/AboutHeader.tsx
+++ src/screens/about/components/AboutHeader.tsx
@@ -1,25 +1,14 @@
 import React from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { Box, mkUseStyles, Text } from 'utils/theme'
-import IconClose from 'assets/icons/icon-close2.svg'
 import IconBack from 'assets/icons/icon-back2.svg'
-import { DrawerActions, useNavigation } from '@react-navigation/native'
 import { useTranslation } from 'react-i18next'
 
 type HeaderProps = {
-  closeModal: F0
-  isFromWelcomeScreen?: true
+  onClose: F0
 }
-export const AboutHeader = ({ closeModal, isFromWelcomeScreen }: HeaderProps) => {
-  const handleGoBack = () => {
-    if (!isFromWelcomeScreen) {
-      navigation.goBack()
-      navigation.dispatch(DrawerActions.openDrawer())
-    } else closeModal()
-  }
-
+export const AboutHeader = ({ onClose }: HeaderProps) => {
   const styles = useStyles()
-  const navigation = useNavigation()
   const { t } = useTranslation('welcome')
 
   return (
@@ -28,14 +17,8 @@ export const AboutHeader = ({ closeModal, isFromWelcomeScreen }: HeaderProps) =>
       flexDirection="row"
       paddingBottom="lplus"
       paddingHorizontal="m">
-      <TouchableOpacity
-        onPress={handleGoBack}
-        hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}>
-        {isFromWelcomeScreen ? (
-          <IconClose height={14} width={14} color={styles.arrow.color} />
-        ) : (
-          <IconBack height={18} width={18} color={styles.arrow.color} />
-        )}
+      <TouchableOpacity onPress={onClose} hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}>
+        <IconBack height={18} width={18} color={styles.arrow.color} />
       </TouchableOpacity>
       <Box>
         <Text variant="displayBoldSM">{t('about')}</Text>
