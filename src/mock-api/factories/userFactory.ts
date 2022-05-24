c2c8734e6487a985d8e956b74e6ac1cb88935f23
Lukasz Baranowski <lukaszb48@gmail.com>
2022-05-24 16:52:41 +0200

    team adjustments

--- src/mock-api/factories/userFactory.ts
+++ src/mock-api/factories/userFactory.ts
@@ -378,7 +378,7 @@ export const usersList = [
   {
     firstName: 'Jeff',
     lastName: 'Perry',
-    photo: 'https://randomuser.me/api/portraits/men/43.jpg',
+    photo: 'https://randomuser.me/api/portraits/men/44.jpg',
     occupation: 'QA Tester',
     userColor: getRandomValue(colors, faker.datatype.number({ min: 0, max: colors.length - 1 })),
   },
