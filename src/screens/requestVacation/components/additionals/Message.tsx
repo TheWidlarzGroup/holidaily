ab7abbbcd0b4c3d4788c6467f8c9ba939d1514b6
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-26 20:18:58 +0200

    Revert "reskin message"
    This reverts commit 91e1b6977d7dd19c8fbdd38f97fdf624b3153408.

--- src/screens/requestVacation/components/additionals/Message.tsx
+++ src/screens/requestVacation/components/additionals/Message.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Box, Text } from 'utils/theme/index'
+import { Box, mkUseStyles, Text } from 'utils/theme/index'
+import CommentIcon from 'assets/icons/icon-message.svg'
 import { TouchableOpacity } from 'react-native'
 
 type MessageProps = {
@@ -7,14 +8,42 @@ type MessageProps = {
   messageContent: string
 }
 
-export const Message = ({ onPressMessage, messageContent }: MessageProps) => (
-  <TouchableOpacity onPress={onPressMessage}>
-    <Box>
-      <Box padding="xm" backgroundColor="lightGrey" borderRadius="l1min">
-        <Text variant="inputText" color="black">
-          {messageContent}
-        </Text>
+export const Message = ({ onPressMessage, messageContent }: MessageProps) => {
+  const styles = useStyles()
+
+  return (
+    <TouchableOpacity onPress={onPressMessage} style={{ marginTop: 35 }}>
+      <Box position="relative" style={styles.container}>
+        <Box backgroundColor="lightGrey" borderRadius="l" style={styles.cornerAddComment}>
+          <TouchableOpacity onPress={onPressMessage}>
+            <CommentIcon width={18} height={18} />
+          </TouchableOpacity>
+        </Box>
+        <Box padding="m" backgroundColor="lightGrey" borderRadius="mplus">
+          <Text variant="regularGrey16" color="black">
+            {messageContent}
+          </Text>
+        </Box>
       </Box>
-    </Box>
-  </TouchableOpacity>
-)
+    </TouchableOpacity>
+  )
+}
+
+const useStyles = mkUseStyles((theme) => ({
+  container: {
+    position: 'relative',
+    overflow: 'visible',
+    paddingHorizontal: 10,
+  },
+  cornerAddComment: {
+    position: 'absolute',
+    top: -17,
+    left: -13,
+    background: theme.colors.grey,
+    zIndex: 4,
+    borderWidth: 3,
+    borderColor: theme.colors.disabledText,
+    padding: 8,
+    margin: 0,
+  },
+}))
