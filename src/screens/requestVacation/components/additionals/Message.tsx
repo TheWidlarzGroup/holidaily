4ff2fa1dcc9859f63785bcff4d5216ad79afbd45
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-26 21:15:26 +0200

    reskin message

--- src/screens/requestVacation/components/additionals/Message.tsx
+++ src/screens/requestVacation/components/additionals/Message.tsx
@@ -1,49 +1,28 @@
 import React from 'react'
-import { Box, mkUseStyles, Text } from 'utils/theme/index'
-import CommentIcon from 'assets/icons/icon-message.svg'
+import { Box, Text } from 'utils/theme/index'
 import { TouchableOpacity } from 'react-native'
+import { useTranslation } from 'react-i18next'
 
 type MessageProps = {
-  onPressMessage: F0
-  messageContent: string
+  onPress: F0
+  content: string
+  maxLen: number
 }
 
-export const Message = ({ onPressMessage, messageContent }: MessageProps) => {
-  const styles = useStyles()
-
+export const Message = (p: MessageProps) => {
+  const { t } = useTranslation('requestVacation')
   return (
-    <TouchableOpacity onPress={onPressMessage} style={{ marginTop: 35 }}>
-      <Box position="relative" style={styles.container}>
-        <Box backgroundColor="lightGrey" borderRadius="l" style={styles.cornerAddComment}>
-          <TouchableOpacity onPress={onPressMessage}>
-            <CommentIcon width={18} height={18} />
-          </TouchableOpacity>
-        </Box>
-        <Box padding="m" backgroundColor="lightGrey" borderRadius="mplus">
-          <Text variant="regularGrey16" color="black">
-            {messageContent}
-          </Text>
-        </Box>
+    <TouchableOpacity onPress={p.onPress}>
+      <Box padding="m" backgroundColor="lightGrey" borderRadius="l1min" marginTop="xs">
+        <Text variant="regularGrey16" color="black">
+          {p.content}
+        </Text>
+      </Box>
+      <Box alignSelf="flex-end" marginRight="xs">
+        <Text variant="textXS" color="darkGrey">
+          {t('messageCharactes', { count: p.content.length, max: p.maxLen })}
+        </Text>
       </Box>
     </TouchableOpacity>
   )
 }
-
-const useStyles = mkUseStyles((theme) => ({
-  container: {
-    position: 'relative',
-    overflow: 'visible',
-    paddingHorizontal: 10,
-  },
-  cornerAddComment: {
-    position: 'absolute',
-    top: -17,
-    left: -13,
-    background: theme.colors.grey,
-    zIndex: 4,
-    borderWidth: 3,
-    borderColor: theme.colors.disabledText,
-    padding: 8,
-    margin: 0,
-  },
-}))
