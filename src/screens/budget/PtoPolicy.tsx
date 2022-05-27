2f1c3361b98d2f5b9faf345d691b84dc1ed9cd17
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-27 13:13:29 +0200

    fix

--- src/screens/budget/PtoPolicy.tsx
+++ src/screens/budget/PtoPolicy.tsx
@@ -1,27 +1,11 @@
 import React from 'react'
-import { mkUseStyles, Theme } from 'utils/theme'
-import FastImage from 'react-native-fast-image'
 import { SwipeableScreen } from 'navigation/SwipeableScreen'
 import { Policies } from './components/Policies'
 import { PolicyHeader } from './components/PolicyHeader'
 
-const Background = require('assets/policy_modal_background.png')
-
-export const PtoPolicy = () => {
-  const styles = useStyles()
-  return (
-    <SwipeableScreen>
-      <PolicyHeader />
-      <Policies />
-      <FastImage style={[styles.background]} source={Background} />
-    </SwipeableScreen>
-  )
-}
-
-const useStyles = mkUseStyles((theme: Theme) => ({
-  background: {
-    flex: 1,
-    overflow: 'visible',
-    zIndex: theme.zIndices['-1'],
-  },
-}))
+export const PtoPolicy = () => (
+  <SwipeableScreen>
+    <PolicyHeader />
+    <Policies />
+  </SwipeableScreen>
+)
