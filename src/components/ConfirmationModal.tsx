990de792d6dc7aedeeb6cf973cc0fb6324df10da
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-29 12:38:49 +0200

    refactor confirmation modal styles usage

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { ModalProps } from 'react-native-modal'
 import { useTranslation } from 'react-i18next'
-import { mkUseStyles, Theme, Text, Box, BaseOpacity } from 'utils/theme'
+import { Text, Box, BaseOpacity } from 'utils/theme'
 import { ConfirmationModalProps } from 'types/confirmationModalProps'
+import { useModalStyles } from 'hooks/style-hooks/useModalStyles'
 import { CustomButton } from './CustomButton'
 import { SwipeableModal } from './SwipeableModal'
 
@@ -16,14 +17,14 @@ export const ConfirmationModal = ({
   declineBtnText,
   acceptBtnText,
 }: ConfirmationModalProps & Pick<ModalProps, 'isVisible'>) => {
-  const styles = useStyles()
+  const styles = useModalStyles()
   const { t } = useTranslation('confirmationModal')
 
   // FIXME: Something in the drawer menu is intercepting the touch gestures, so a hack with wrapping CustomButton in BaseOpacity is needed for buttons to work
   // and a hack with full width & height BaseOpacity for backdrop press to work
   return (
     <SwipeableModal isOpen={isVisible} onHide={onDismiss ?? onDecline}>
-      <Box style={styles.modal}>
+      <Box style={styles.bottomModal}>
         {header !== null && <Text variant="displayBoldSM">{header || t('areYouSure')}</Text>}
         {content !== null && (
           <Text variant="textSM" textAlign="center">
@@ -46,20 +47,3 @@ export const ConfirmationModal = ({
     </SwipeableModal>
   )
 }
-
-const useStyles = mkUseStyles((theme: Theme) => ({
-  modal: {
-    paddingVertical: theme.spacing.l,
-    backgroundColor: theme.colors.white,
-    paddingHorizontal: theme.spacing.l2plus,
-    marginTop: 'auto',
-    borderTopLeftRadius: theme.borderRadii.l2min,
-    borderTopRightRadius: theme.borderRadii.l2min,
-    alignItems: 'center',
-    shadowOffset: { width: -2, height: 0 },
-    shadowColor: theme.colors.black,
-    shadowOpacity: 0.04,
-    shadowRadius: 2,
-    elevation: 20,
-  },
-}))
