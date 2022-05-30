cbbeb86f39ef0a2a639cb94de6a93db866795b7f
Jan Koziński <jan.kozinski.02@gmail.com>
2022-05-30 15:08:30 +0200

    Reskin/add attachment (#627)
    * add translations and default description
    
    * add translations
    
    * reskin add request

--- src/screens/requestVacation/components/additionals/Attachments.tsx
+++ src/screens/requestVacation/components/additionals/Attachments.tsx
@@ -12,8 +12,6 @@ type AttachmentProps = {
   displayAddMore?: boolean
 }
 
-type Side = 'right' | 'left' | 'top'
-
 export const Attachments = ({
   attachments,
   addMore,
@@ -21,27 +19,12 @@ export const Attachments = ({
   removeAttachment,
 }: AttachmentProps) => {
   const styles = useStyles()
-  const getPadding = (index: number, side: Side) => {
-    const n = index % 3
-    const paddingSize = 4
-    if (side === 'top') return 3 * paddingSize
-    if (n === 0) return side === 'left' ? 0 : 2 * paddingSize
-    if (n === 1) return paddingSize
-    if (n === 2) return side === 'left' ? 2 * paddingSize : 0
-  }
-
+  const isRowFull = attachments.length % 4 === 0
   return (
     <Box alignSelf="stretch" style={styles.container}>
       <Box flexDirection="row" flexWrap="wrap">
-        {attachments.map((attachment, uriIndex) => (
-          <Box
-            key={attachment.id}
-            style={{
-              paddingTop: getPadding(0, 'top'),
-              paddingLeft: getPadding(uriIndex, 'left'),
-              paddingRight: getPadding(uriIndex, 'right'),
-              width: '33.33%',
-            }}>
+        {attachments.map((attachment) => (
+          <Box key={attachment.id} paddingRight="s" width="25%">
             {'name' in attachment ? (
               <Box alignItems="center" justifyContent="center">
                 <BaseOpacity
@@ -54,6 +37,8 @@ export const Attachments = ({
               </Box>
             ) : (
               <Photo
+                width={80}
+                height={80}
                 src={attachment.uri}
                 onClose={() => removeAttachment(attachment.id)}
                 displayClose
@@ -61,13 +46,17 @@ export const Attachments = ({
             )}
           </Box>
         ))}
-        {attachments.length % 3 !== 0 && displayAddMore && (
+        {!isRowFull && displayAddMore && (
           <Box flexDirection="row" justifyContent="flex-end" flex={1}>
             <AddMore onPress={addMore} />
           </Box>
         )}
       </Box>
-      {attachments.length % 3 === 0 && displayAddMore && <AddMore onPress={addMore} />}
+      {isRowFull && displayAddMore && (
+        <Box justifyContent="flex-end" flexDirection="row">
+          <AddMore onPress={addMore} />
+        </Box>
+      )}
     </Box>
   )
 }
