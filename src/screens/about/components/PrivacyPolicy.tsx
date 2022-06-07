f8cdf4cafdd5370a10ea65867f3279e00d122ef9
Lukasz Baranowski <lukaszb48@gmail.com>
2022-06-07 15:05:59 +0200

    add navigate to privacy policy

---
+++ src/screens/about/components/PrivacyPolicy.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { BaseOpacity, Box, mkUseStyles, Text, useTheme } from 'utils/theme'
+import { ModalHeader } from 'components/ModalHeader'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import IconBack from 'assets/icons/icon-back2.svg'
+import { useTranslation } from 'react-i18next'
+import { useNavigation } from '@react-navigation/native'
+import { PrivacyPolicyContent } from './PrivacyPolicyContent'
+
+export const PrivacyPolicy = () => {
+  const theme = useTheme()
+  const styles = useStyles()
+  const { t } = useTranslation('about')
+  const navigation = useNavigation()
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.dashboardBackground,
+      }}>
+      <ModalHeader>
+        <BaseOpacity
+          onPress={() => navigation.goBack()}
+          hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+          paddingLeft="m">
+          <IconBack height={18} width={18} color={styles.arrow.color} />
+        </BaseOpacity>
+        <Text variant="displayBoldSM">{t('privacyPolicy')}</Text>
+        <Box paddingRight="xl" paddingVertical="lplus" />
+      </ModalHeader>
+      <PrivacyPolicyContent />
+    </SafeAreaView>
+  )
+}
+
+const useStyles = mkUseStyles((theme) => ({
+  arrow: {
+    color: theme.colors.black,
+  },
+}))
