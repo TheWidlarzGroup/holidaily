301b079470f9a67e18ad3170e39127c6da3132d1
Jan Koziński <jan.kozinski.02@gmail.com>
2022-06-07 09:26:06 +0200

    fix linting

--- src/screens/dashboard/DashboardTeam.tsx
+++ src/screens/dashboard/DashboardTeam.tsx
@@ -10,7 +10,6 @@ import { TeamHeader } from 'screens/dashboard/components/TeamHeader'
 import { sortByEndDate, sortByStartDate } from 'utils/sortByDate'
 import { User } from 'mockApi/models'
 import { SwipeableModalRegular, SwipeableModalRegularProps } from 'components/SwipeableModalRegular'
-import { useUserContext } from 'hooks/context-hooks/useUserContext'
 import { Analytics } from 'services/analytics'
 import { SWIPEABLE_MODAL_HEIGHT } from 'components/SwipeableModal'
 import { DashboardTeamMember } from './DashboardTeamMember'
@@ -27,7 +26,6 @@ export const DashboardTeam: FC<DashboardTeamProps> = ({ route }) => {
     setIsModalVisible(true)
     Analytics().track('DASHBOARD_TEAM_OPENED', { teamName: params.name })
   }
-  const { user } = useUserContext()
 
   const { matesOnHoliday, matesWithPlannedHolidays, mates } = useMemo(() => {
     const mates: User[] = params?.users ?? []
@@ -47,7 +45,7 @@ export const DashboardTeam: FC<DashboardTeamProps> = ({ route }) => {
     matesWithPlannedHolidays.sort(sortByStartDate)
 
     return { matesOnHoliday, matesWithPlannedHolidays, mates }
-  }, [params?.users, user])
+  }, [params?.users])
 
   return (
     <>
