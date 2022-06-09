220790e9b4912e2294c2ef6b8e061dedd9f4c493
Jan Koziński <jan.kozinski.02@gmail.com>
2022-06-09 17:10:41 +0200

    fix commented function

--- src/utils/dayOffUtils.ts
+++ src/utils/dayOffUtils.ts
@@ -6,14 +6,12 @@ export const drawnDayoffInAlreadyScheduledTime = (
   req: Pick<DayOffRequest, 'startDate' | 'endDate'>,
   requests: Omit<DayOffRequest, 'id'>[]
 ) => {
-  console.log('why', req.startDate, req.endDate)
-  // const start = new Date(req.startDate)
-  // const end = new Date(req.endDate)
-  // console.log('rly?', start, end)
-  return false
-  //   console.log(eachDayOfInterval({ start, end }))
-  //   return requests.some((existingReq) => {
-  //     const days = eachDayOfInterval({ start, end })
-  //     return days.some((day) => isDateBetween(day, existingReq.startDate, existingReq.endDate))
-  //   })
+  console.log(req.startDate, req.endDate)
+  const start = new Date(req.startDate)
+  const end = new Date(req.endDate)
+  console.log(eachDayOfInterval({ start, end }))
+  return requests.some((existingReq) => {
+    const days = eachDayOfInterval({ start, end })
+    return days.some((day) => isDateBetween(day, existingReq.startDate, existingReq.endDate))
+  })
 }
