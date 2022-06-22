7370b5d809cb1df37bf6fabfb56b5f91b8256139
Lukasz Baranowski <lukaszb48@gmail.com>
2022-06-22 11:24:42 +0200

    Fix/ptopolicy smooth slide (#738)
    * adjusted arrow hitslop
    
    * removed gesture recognizer

--- src/screens/budget/PtoPolicy.tsx
+++ src/screens/budget/PtoPolicy.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
-import GestureRecognizer from 'react-native-swipe-gestures'
-import { useNavigation } from '@react-navigation/native'
 import { Policies } from './components/Policies'
 import { PolicyHeader } from './components/PolicyHeader'
 
-export const PtoPolicy = () => {
-  const { goBack } = useNavigation()
-  return (
-    <SafeAreaWrapper>
-      <GestureRecognizer onSwipeRight={goBack}>
-        <PolicyHeader />
-        <Policies />
-      </GestureRecognizer>
-    </SafeAreaWrapper>
-  )
-}
+export const PtoPolicy = () => (
+  <SafeAreaWrapper>
+    <PolicyHeader />
+    <Policies />
+  </SafeAreaWrapper>
+)
