0eba4a5bf4e338edf72c0badae173145260aa9ce
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-01 13:28:59 +0200

    hook adjustments

--- src/hooks/useKeyboard.ts
+++ src/hooks/useKeyboard.ts
@@ -1,40 +1,26 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
-import { EmitterSubscription, Keyboard, KeyboardEvent } from 'react-native'
+import { Keyboard, KeyboardEvent } from 'react-native'
 
 export const useKeyboard = () => {
   const [keyboardOpen, setKeyboardOpen] = useState(false)
   const [keyboardEvent, setKeyboardEvent] = useState<KeyboardEvent | null>(null)
   const [keyboardHeight, setKeyboardHeight] = useState(0)
-
-  function onKeyboardDidShow(e: KeyboardEvent) {
-    setKeyboardHeight(e.endCoordinates.height)
-  }
-
-  function onKeyboardDidHide() {
-    setKeyboardHeight(0)
-  }
-
   const willMount = useRef(true)
 
   const onOpenKeyboard = useCallback((event: KeyboardEvent) => {
     setKeyboardEvent(event)
     setKeyboardOpen(true)
+    setKeyboardHeight(event.endCoordinates.height)
   }, [])
 
   const onHideKeyboard = useCallback((event: KeyboardEvent) => {
     setKeyboardEvent(event)
     setKeyboardOpen(false)
+    setKeyboardHeight(0)
   }, [])
 
   useEffect(() => {
-    let keyboardDidShow
-    let keyboardDidHide
-
-    if (willMount.current) {
-      keyboardDidShow = Keyboard.addListener('keyboardDidShow', onKeyboardDidShow)
-      keyboardDidHide = Keyboard.addListener('keyboardDidHide', onKeyboardDidHide)
-      willMount.current = false
-    }
+    if (willMount.current) willMount.current = false
     const listener1 = Keyboard.addListener('keyboardDidShow', onOpenKeyboard)
     const listener2 = Keyboard.addListener('keyboardWillShow', onOpenKeyboard)
     const listener3 = Keyboard.addListener('keyboardDidHide', onHideKeyboard)
@@ -45,9 +31,6 @@ export const useKeyboard = () => {
       listener2.remove()
       listener3.remove()
       listener4.remove()
-
-      keyboardDidShow?.remove('keyboardDidShow', onKeyboardDidShow)
-      keyboardDidHide?.remove('keyboardDidHide', onKeyboardDidHide)
     }
   }, [onOpenKeyboard, onHideKeyboard])
 
