610f7f0ca73c65c8eb202ea240e51419f90cdae5
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-01 10:09:19 +0200

    fix

--- src/hooks/useRecognizeSwipe.ts
+++ src/hooks/useRecognizeSwipe.ts
@@ -5,9 +5,7 @@ import { HandlerStateChangeEvent } from 'react-native-gesture-handler'
 export const useRecognizeSwipe = (actionOnSwipe: F0) => {
   const [touchStart, setTouchStart] = useState(0)
 
-  type NewType = HandlerStateChangeEvent<Record<string, any>>
-
-  const onTouchStart = (e: NewType) => {
+  const onTouchStart = (e: HandlerStateChangeEvent<Record<string, any>>) => {
     setTouchStart(0)
     setTouchStart(e.nativeEvent.x)
   }
