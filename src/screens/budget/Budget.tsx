c846bd21e5c05c26b23ff98eeb5b374c67e2c7a7
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-01 10:11:36 +0200

    fix swipe back to drawer ios & android (#765)
    * fix swipe back to drawer
    
    * fix

--- src/screens/budget/Budget.tsx
+++ src/screens/budget/Budget.tsx
@@ -1,7 +1,6 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { DrawerActions, useNavigation } from '@react-navigation/native'
 import { BaseOpacity, Box } from 'utils/theme'
-import GestureRecognizer from 'react-native-swipe-gestures'
 import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { AppNavigationType, BudgetNavigationType } from 'navigation/types'
 import { DrawerBackArrow } from 'components/DrawerBackArrow'
@@ -9,7 +8,12 @@ import { useTranslation } from 'react-i18next'
 import { useUserContext } from 'hooks/context-hooks/useUserContext'
 import { useFetchUserStats } from 'dataAccess/queries/useFetchUserStats'
 import { LoadingModal } from 'components/LoadingModal'
+import { useRecognizeSwipe } from 'hooks/useRecognizeSwipe'
+import Animated from 'react-native-reanimated'
+import { PanGestureHandler } from 'react-native-gesture-handler'
 import { Section } from './components/Section'
+
+const AnimatedBox = Animated.createAnimatedComponent(Box)
 
 export const Budget = () => {
   const navigation = useNavigation<AppNavigationType<'DRAWER_NAVIGATOR'>>()
@@ -18,23 +22,28 @@ export const Budget = () => {
   const { user } = useUserContext()
   const { navigate } = useNavigation<BudgetNavigationType<'BUDGET'>>()
 
-  const handleGoBack = useCallback(() => {
+  const handleGoBack = () => {
     navigation.goBack()
     navigation.dispatch(DrawerActions.openDrawer())
-  }, [navigation])
+  }
+  const { onTouchStart, onTouchMove } = useRecognizeSwipe(handleGoBack)
+
   const isLoading = loadingStats || !user || !stats
   if (isLoading) return <LoadingModal show />
   return (
     <SafeAreaWrapper>
-      <GestureRecognizer onSwipeRight={handleGoBack} style={{ flex: 1 }}>
-        <DrawerBackArrow goBack={handleGoBack} title={t('budget')} />
-        <Box paddingHorizontal="m" paddingTop="lplus">
-          <BaseOpacity activeOpacity={0.8} onPress={() => navigate('PTO_POLICY')}>
+      <PanGestureHandler onBegan={onTouchStart} onActivated={onTouchMove}>
+        <AnimatedBox flex={1}>
+          <DrawerBackArrow goBack={handleGoBack} title={t('budget')} />
+          <BaseOpacity
+            activeOpacity={0.8}
+            onPress={() => navigate('PTO_POLICY')}
+            paddingTop="lplus">
             <Section variant="left" duration={user.availablePto ?? 0} />
           </BaseOpacity>
           <Section variant="sick" duration={+stats.sickdaysTaken ?? 0} />
-        </Box>
-      </GestureRecognizer>
+        </AnimatedBox>
+      </PanGestureHandler>
     </SafeAreaWrapper>
   )
 }
