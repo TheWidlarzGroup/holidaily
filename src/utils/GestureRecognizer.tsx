efc22f45d7bf97415a63e3a5f11708e9512ef160
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-02 14:38:41 +0200

    fix

--- src/utils/GestureRecognizer.tsx
+++ src/utils/GestureRecognizer.tsx
@@ -9,33 +9,33 @@ import Animated from 'react-native-reanimated'
 import { Box } from 'utils/theme'
 
 type GestureRecognizerProps = {
-  children: ReactNode
-  component: 'Box' | 'ScrollView'
+  children?: ReactNode
+  component?: 'Box' | 'ScrollView'
   onSwipeLeft?: F0
   onSwipeRight?: F0
-  onSwipeTop?: F0
+  onSwipeUp?: F0
   onSwipeDown?: F0
 }
 
 export const GestureRecognizer = ({
   children,
-  component,
+  component = 'Box',
   onSwipeLeft,
   onSwipeRight,
-  onSwipeTop,
+  onSwipeUp,
   onSwipeDown,
 }: GestureRecognizerProps) => {
   const touchStartX = useRef(0)
   const touchStartY = useRef(0)
 
   const AnimatedComponent =
-    component === 'Box'
-      ? Animated.createAnimatedComponent(Box)
-      : Animated.createAnimatedComponent(ScrollView)
+    component === 'ScrollView'
+      ? Animated.createAnimatedComponent(ScrollView)
+      : Animated.createAnimatedComponent(Box)
 
   const onTouchStart = (e: HandlerStateChangeEvent<Record<string, any>>) => {
     if (onSwipeLeft || onSwipeRight) touchStartX.current = e.nativeEvent.x
-    if (onSwipeTop || onSwipeDown) touchStartY.current = e.nativeEvent.y
+    if (onSwipeUp || onSwipeDown) touchStartY.current = e.nativeEvent.y
   }
 
   const onTouchMove = (e: HandlerStateChangeEvent<Record<string, any>>) => {
@@ -46,7 +46,7 @@ export const GestureRecognizer = ({
     if (onSwipeRight && touchMoveX - touchStartX.current > minSwipeDistance) onSwipeRight()
     if (onSwipeLeft && touchStartX.current - touchMoveX > minSwipeDistance) onSwipeLeft()
     if (onSwipeDown && touchMoveY - touchStartY.current > minSwipeDistance) onSwipeDown()
-    if (onSwipeTop && touchStartY.current - touchMoveY > minSwipeDistance) onSwipeTop()
+    if (onSwipeUp && touchStartY.current - touchMoveY > minSwipeDistance) onSwipeUp()
   }
 
   return (
