7c71f0eb1814628b2c5653ee7719ef5b50d1e0ea
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-08 11:20:25 +0200

    adjust fonts & calculate elements width

--- src/screens/feed/components/Bubble/ReactionBubble.tsx
+++ src/screens/feed/components/Bubble/ReactionBubble.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
-
+import React, { Dispatch, SetStateAction } from 'react'
 import { Text } from 'utils/theme'
 import { useUserContext } from 'hooks/context-hooks/useUserContext'
 import { Reaction } from 'mock-api/models/miragePostTypes'
 import { Bubble } from './Bubble'
 
+type BubbleSizesType = {
+  addCommentBtn: number
+  addEmojiBtn: number
+  singleEmoji: number
+}
+
 type ReactionBubbleProps = {
   reaction: Reaction
   handlePressReaction: F1<string>
+  setBubblesSize: Dispatch<SetStateAction<BubbleSizesType>>
+  marginsWidth: number
+  reactionBubbleSize: number
 }
 
-export const ReactionBubble = ({ reaction, handlePressReaction }: ReactionBubbleProps) => {
+export const ReactionBubble = ({
+  reaction,
+  handlePressReaction,
+  setBubblesSize,
+  marginsWidth,
+  reactionBubbleSize,
+}: ReactionBubbleProps) => {
   const { user } = useUserContext()
 
   const hasUserAddedReaction = reaction.users?.includes(user?.id || '')
@@ -19,12 +33,19 @@ export const ReactionBubble = ({ reaction, handlePressReaction }: ReactionBubble
     <Bubble
       margin="xs"
       onPress={() => handlePressReaction(reaction.type)}
+      onLayout={({ nativeEvent }) => {
+        if (reactionBubbleSize) return
+        setBubblesSize((prev) => ({
+          ...prev,
+          singleEmoji: nativeEvent.layout.width + marginsWidth,
+        }))
+      }}
       borderColor={hasUserAddedReaction ? 'black' : 'transparent'}
       borderWidth={1.2}
       height={42}>
       <Text padding="s">{reaction.type}</Text>
       {reaction.users?.length > 0 && (
-        <Text paddingEnd="m" variant="primaryBold12" color="black">
+        <Text paddingEnd="m" variant="buttonXS">
           {reaction.users?.length}
         </Text>
       )}
