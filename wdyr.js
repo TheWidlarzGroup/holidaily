42b84b9f70f59af5eb95c2a8e64226902eff9e1f
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2022-07-11 08:35:23 +0200

    disabled wdyr hooks track (#805)

--- wdyr.js
+++ wdyr.js
@@ -4,5 +4,6 @@ import whyDidYouRender from '@welldone-software/why-did-you-render'
 if (process.env.NODE_ENV === 'development') {
   whyDidYouRender(React, {
     trackAllPureComponents: false, // enable when testing
+    trackHooks: false, // enable when testing !IMPORTANT: ENABLED CAUSING CRASH IN LOGOUT!
   })
 }
