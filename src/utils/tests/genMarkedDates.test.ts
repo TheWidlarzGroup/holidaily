c5547788ac0fb11351904374bff140389f657737
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-12 12:02:03 +0200

    fix marked dates test

--- src/utils/tests/genMarkedDates.test.ts
+++ src/utils/tests/genMarkedDates.test.ts
@@ -8,8 +8,26 @@ describe('genMarkedDates', () => {
 
   it('properly generates marked dates object', () => {
     const result = genMarkedDates('2021-06-17', '2021-06-19')
-    expect(result['2021-06-17']).toMatchObject({ selected: true, startingDay: true })
-    expect(result['2021-06-18']).toMatchObject({ selected: true })
-    expect(result['2021-06-19']).toMatchObject({ selected: true, endingDay: true })
+    expect(result['2021-06-17']).toMatchObject({
+      period: true,
+      startingDay: true,
+      endingDay: false,
+      isInvalid: undefined,
+      dots: undefined,
+    })
+    expect(result['2021-06-18']).toMatchObject({
+      period: true,
+      startingDay: false,
+      endingDay: false,
+      isInvalid: undefined,
+      dots: undefined,
+    })
+    expect(result['2021-06-19']).toMatchObject({
+      period: true,
+      startingDay: false,
+      endingDay: true,
+      isInvalid: undefined,
+      dots: undefined,
+    })
   })
 })
