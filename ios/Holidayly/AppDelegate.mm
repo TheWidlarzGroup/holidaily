61aebc4849bf3d948176bae6eaae6cd687e28a9c
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-13 14:04:04 +0200

    downgrade RN to 0.68.2 & upgrade expo to 45 & downgrade gradle to 6.9

--- ios/Holidayly/AppDelegate.mm
+++ ios/Holidayly/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"RnDiffApp", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Holidayly", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
