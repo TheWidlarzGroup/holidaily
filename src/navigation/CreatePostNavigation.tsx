0b1e1b8b552bde8fd9863a72a7616d79bd72fa29
mateki0 <mateki0@interia.pl>
2022-07-14 08:50:28 +0200

    Fixed createPost screen not being transparent

--- src/navigation/CreatePostNavigation.tsx
+++ src/navigation/CreatePostNavigation.tsx
@@ -3,22 +3,31 @@ import { createStackNavigator, TransitionPresets } from '@react-navigation/stack
 import { LocationForm } from 'screens/createPost/CreatePostForm/LocationForm'
 import { CreatePost } from 'screens/createPost/CreatePost'
 import { CreatePostProvider } from 'contexts/CreatePostProvider'
+import { useTheme } from '@react-navigation/native'
 import { CreatePostRoutes } from './types'
 import { StackNavigatorPresets } from './Presets/StackNavigatorPresets'
 
 const CreatePostStack = createStackNavigator<CreatePostRoutes>()
 
-export const CreatePostNavigation = () => (
-  <CreatePostProvider>
-    <CreatePostStack.Navigator
-      initialRouteName="CREATE_POST"
-      {...StackNavigatorPresets.modalNavigator}>
-      <CreatePostStack.Screen name="CREATE_POST" component={CreatePost} />
-      <CreatePostStack.Screen
-        name="LOCATION_FORM"
-        component={LocationForm}
-        options={TransitionPresets.SlideFromRightIOS}
-      />
-    </CreatePostStack.Navigator>
-  </CreatePostProvider>
-)
+export const CreatePostNavigation = () => {
+  // Nested navigators transparentModal are bugged in navigation V6,
+  // that's why we need to hard code background color
+
+  const { colors } = useTheme()
+  colors.background = 'transparent'
+
+  return (
+    <CreatePostProvider>
+      <CreatePostStack.Navigator
+        initialRouteName="CREATE_POST"
+        {...StackNavigatorPresets.modalNavigator}>
+        <CreatePostStack.Screen name="CREATE_POST" component={CreatePost} />
+        <CreatePostStack.Screen
+          name="LOCATION_FORM"
+          component={LocationForm}
+          options={TransitionPresets.SlideFromRightIOS}
+        />
+      </CreatePostStack.Navigator>
+    </CreatePostProvider>
+  )
+}
