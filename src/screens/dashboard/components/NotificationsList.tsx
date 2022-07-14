16bc6e1a56634dbcd4f543872bb1e007bcf0a3d0
asgalecki <a.s.galecki@gmail.com>
2022-07-14 22:27:54 +0200

    Add ScrollView in NotificationsList

--- src/screens/dashboard/components/NotificationsList.tsx
+++ src/screens/dashboard/components/NotificationsList.tsx
@@ -1,57 +1,69 @@
 import React, { useMemo } from 'react'
 import { BaseOpacity, Text } from 'utils/theme'
-import { SectionList } from 'react-native'
 import { Notification as NotificationModel } from 'mockApi/models'
 import { useTranslation } from 'react-i18next'
 import { useMarkNotificationAsSeen } from 'dataAccess/mutations/useMarkNotificationAsSeen'
+import { ScrollView } from 'react-native-gesture-handler'
 import { Notification } from './Notification'
 import { SwipeableNotification } from './SwipeableNotification'
 
 const style = { width: '100%' }
-const keyExtractor = ({ id }: { id: string }) => id
 
 export const NotificationsList = ({ data }: { data: NotificationModel[] }) => {
   const { t } = useTranslation('notifications')
-  const { seenNotifications, unseenNotifications } = useMemo(
+  const { unseenNotifications } = useMemo(
     () => ({
-      seenNotifications: processNotifications(data, 'seen'),
       unseenNotifications: processNotifications(data, 'unseen'),
     }),
     [data]
   )
-  const sections = [
-    {
-      title: t('unseen'),
-      data: unseenNotifications,
-    },
-    {
-      title: t('seen'),
-      data: seenNotifications,
-    },
-  ]
+
+  const seenNotificationsList = data.filter(
+    (notification) => notification.wasSeenByHolder === false
+  )
+  const unseenNotificationsList = data.filter(
+    (notification) => notification.wasSeenByHolder === true
+  )
+
+  const isSeenNotificationsList = seenNotificationsList.length > 0
+  const isUnseenNotificationsList = unseenNotificationsList.length > 0
 
   return (
-    <SectionList
-      style={style}
-      sections={sections}
-      keyExtractor={keyExtractor}
-      showsVerticalScrollIndicator={false}
-      stickySectionHeadersEnabled={false}
-      ListHeaderComponent={<MarkAllAsSeen unseen={unseenNotifications} />}
-      renderSectionHeader={({ section: { title, data } }) => {
-        if (!data.length) return null
-        return (
-          <Text variant="inputLabel" marginBottom="s" color="darkGreyBrighter">
-            {title}
-          </Text>
-        )
-      }}
-      renderItem={({ item }) => (
-        <SwipeableNotification notificationId={item.id} isSeen={item.wasSeenByHolder}>
-          <Notification {...item} />
-        </SwipeableNotification>
-      )}
-    />
+    <>
+      {isSeenNotificationsList && <MarkAllAsSeen unseen={unseenNotifications} />}
+      <ScrollView style={style} showsVerticalScrollIndicator={false}>
+        {isSeenNotificationsList && (
+          <>
+            <Text variant="inputLabel" marginBottom="s" color="darkGreyBrighter">
+              {t('unseen')}
+            </Text>
+            {seenNotificationsList.map((item: NotificationModel) => (
+              <SwipeableNotification
+                key={item.id}
+                notificationId={item.id}
+                isSeen={item.wasSeenByHolder}>
+                <Notification {...item} />
+              </SwipeableNotification>
+            ))}
+          </>
+        )}
+        {isUnseenNotificationsList && (
+          <>
+            <Text variant="inputLabel" marginBottom="s" color="darkGreyBrighter">
+              {t('seen')}
+            </Text>
+            {unseenNotificationsList.map((item: NotificationModel) => (
+              <SwipeableNotification
+                key={item.id}
+                notificationId={item.id}
+                isSeen={item.wasSeenByHolder}>
+                <Notification {...item} />
+              </SwipeableNotification>
+            ))}
+          </>
+        )}
+      </ScrollView>
+    </>
   )
 }
 
