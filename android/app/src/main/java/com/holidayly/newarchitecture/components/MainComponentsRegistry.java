2eeb263ec05f39bd8743c4a80bd73da1922928d6
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-14 17:18:04 +0200

    update adjustments

--- android/app/src/main/java/com/holidayly/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/holidayly/newarchitecture/components/MainComponentsRegistry.java
@@ -1,30 +1,48 @@
-package com.holidaily.newarchitecture.components;
+package com.rndiffapp.newarchitecture.modules;
+
 import com.facebook.jni.HybridData;
-import com.facebook.proguard.annotations.DoNotStrip;
-import com.facebook.react.fabric.ComponentFactory;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactPackageTurboModuleManagerDelegate;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.soloader.SoLoader;
+import java.util.List;
+
 /**
- * Class responsible to load the custom Fabric Components. This class has native methods and needs a
+ * Class responsible to load the TurboModules. This class has native methods and needs a
  * corresponding C++ implementation/header file to work correctly (already placed inside the jni/
  * folder for you).
  *
  * <p>Please note that this class is used ONLY if you opt-in for the New Architecture (see the
  * `newArchEnabled` property). Is ignored otherwise.
  */
-@DoNotStrip
-public class MainComponentsRegistry {
-  static {
-    SoLoader.loadLibrary("fabricjni");
+public class MainApplicationTurboModuleManagerDelegate
+    extends ReactPackageTurboModuleManagerDelegate {
+
+  private static volatile boolean sIsSoLibraryLoaded;
+
+  protected MainApplicationTurboModuleManagerDelegate(
+      ReactApplicationContext reactApplicationContext, List<ReactPackage> packages) {
+    super(reactApplicationContext, packages);
   }
-  @DoNotStrip private final HybridData mHybridData;
-  @DoNotStrip
-  private native HybridData initHybrid(ComponentFactory componentFactory);
-  @DoNotStrip
-  private MainComponentsRegistry(ComponentFactory componentFactory) {
-    mHybridData = initHybrid(componentFactory);
+
+  protected native HybridData initHybrid();
+
+  native boolean canCreateTurboModule(String moduleName);
+
+  public static class Builder extends ReactPackageTurboModuleManagerDelegate.Builder {
+    protected MainApplicationTurboModuleManagerDelegate build(
+        ReactApplicationContext context, List<ReactPackage> packages) {
+      return new MainApplicationTurboModuleManagerDelegate(context, packages);
+    }
   }
-  @DoNotStrip
-  public static MainComponentsRegistry register(ComponentFactory componentFactory) {
-    return new MainComponentsRegistry(componentFactory);
+
+  @Override
+  protected synchronized void maybeLoadOtherSoLibraries() {
+    if (!sIsSoLibraryLoaded) {
+      // If you change the name of your application .so file in the Android.mk file,
+      // make sure you update the name here as well.
+      SoLoader.loadLibrary("holidaily_appmodules");
+      sIsSoLibraryLoaded = true;
+    }
   }
 }
