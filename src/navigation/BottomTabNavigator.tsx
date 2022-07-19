0826ef85411678c1649d8eb61804ea2085bc6a63
Lukasz Baranowski <lukaszb48@gmail.com>
2022-07-19 21:23:18 +0200

    Feat/drawer screen styles (#837)
    * add rounded corners to right screen in drawer
    
    * add animation to open drawer button
    
    * fix screen corner
    
    * adjust colors
    
    * adjustments
    
    * fix

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Animated, { interpolate, SharedValue, useAnimatedStyle } from 'react-native-reanimated'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { mkUseStyles, theme, Theme } from 'utils/theme'
+import { Box, mkUseStyles, Theme, theme } from 'utils/theme'
 import { TabsUi } from 'navigation/BottomNavComponents/TabsUi'
 import { Calendar } from 'screens/calendar/Calendar'
 import { Feed } from 'screens/feed/Feed'
-import { SafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { useDrawerProgress } from '@react-navigation/drawer'
 import useDimensions from '@shopify/restyle/dist/hooks/useDimensions'
+import { isIos } from 'utils/layout'
 import { BottomTabRoutes } from './types'
 import { DashboardNavigation } from './DashboardNavigation'
 import { RequestsNavigation } from './RequestsNavigation'
@@ -26,21 +26,15 @@ const tabs = [
   { name: 'FEED' },
 ]
 
-const screenStyles = {
-  shadowOffset: { width: 0, height: 0 },
-  shadowColor: theme.colors.black,
-  shadowRadius: 10,
-  borderWidth: 0,
-  backgroundColor: theme.colors.white,
-  flex: 1,
-}
+const AnimatedBox = Animated.createAnimatedComponent(Box)
+const OPEN_DRAWER_SCREEN_BORDER = theme.borderRadii.l
 
 export const BottomTabNavigator = () => {
   const styles = useStyles()
   const progress = useDrawerProgress() as Readonly<SharedValue<number>>
   const { width } = useDimensions()
 
-  const style = useAnimatedStyle(() => {
+  const animatedStyle = useAnimatedStyle(() => {
     const screenScale = interpolate(progress.value, [0, 1], [1, 0.8])
     const screenTranslate = interpolate(progress.value, [0, 1], [0, 0.8 * width * -0.1])
     const screenShadowAndroid = interpolate(progress.value, [0, 1], [0, 10])
@@ -53,17 +47,21 @@ export const BottomTabNavigator = () => {
     }
   })
 
-  const screenOptions = {
-    headerShown: false,
-  }
+  const animatedTopBoxStyle = useAnimatedStyle(() => ({
+    borderTopLeftRadius: interpolate(
+      progress.value,
+      [0, OPEN_DRAWER_SCREEN_BORDER],
+      [OPEN_DRAWER_SCREEN_BORDER, 0]
+    ),
+  }))
 
   return (
-    <SafeAreaWrapper edges={['bottom']}>
-      <Animated.View style={[screenStyles, style]}>
-        <SafeAreaView edges={['top']} style={styles.safeAreaTop}>
+    <SafeAreaView edges={['left', 'right']} style={styles.outerSafeArea}>
+      <AnimatedBox style={[styles.animatedBox, animatedStyle]}>
+        <SafeAreaView edges={['bottom', 'top']} style={styles.innerSafeArea}>
           <Tab.Navigator
             tabBar={(props) => <TabsUi {...{ tabs, ...props }} />}
-            screenOptions={screenOptions}>
+            screenOptions={{ headerShown: false }}>
             <Tab.Screen
               name="DashboardNavigation"
               options={{ unmountOnBlur: true }}
@@ -78,15 +76,39 @@ export const BottomTabNavigator = () => {
             />
             <Tab.Screen name="FEED" component={Feed} />
           </Tab.Navigator>
+          {/* Box below covers different color of top edge of SafeAreaView, as it's not possible to add different color on top than bottom */}
+          <AnimatedBox
+            position="absolute"
+            backgroundColor="dashboardBackground"
+            height={50}
+            width="100%"
+            style={animatedTopBoxStyle}
+          />
         </SafeAreaView>
-      </Animated.View>
-    </SafeAreaWrapper>
+      </AnimatedBox>
+    </SafeAreaView>
   )
 }
 
 const useStyles = mkUseStyles((theme: Theme) => ({
-  safeAreaTop: {
+  outerSafeArea: {
     flex: 1,
-    backgroundColor: theme.colors.dashboardBackground,
+    backgroundColor: theme.colors.white,
+  },
+  innerSafeArea: {
+    flex: 1,
+    marginTop: isIos ? -6 : 0,
+    marginBottom: -7,
+    backgroundColor: theme.colors.white,
+    borderRadius: OPEN_DRAWER_SCREEN_BORDER,
+    overflow: 'hidden',
+  },
+  animatedBox: {
+    shadowOffset: { width: 0, height: 2 },
+    shadowColor: theme.colors.black,
+    shadowRadius: 10,
+    borderWidth: 0,
+    flex: 1,
+    borderRadius: OPEN_DRAWER_SCREEN_BORDER,
   },
 }))
