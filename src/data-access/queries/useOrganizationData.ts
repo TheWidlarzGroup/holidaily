11a859e40e9df8752dfaba07cb4381e32855dcfa
Lukasz Baranowski <lukaszb48@gmail.com>
2022-08-05 14:34:35 +0200

    add json to with data

--- src/data-access/queries/useOrganizationData.ts
+++ src/data-access/queries/useOrganizationData.ts
@@ -2,10 +2,9 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 import { Organization } from 'mock-api/models/mirageTypes'
 import { QueryKeys } from 'dataAccess/QueryKeys'
-import { API } from 'dataAccess/API'
 
 export const getOrganization = async () => {
-  const response = await axios.get(API.GET.getOrganization)
+  const response = await axios.get('../mockedOrg.json')
   return response.data.organizations[0]
 }
 
