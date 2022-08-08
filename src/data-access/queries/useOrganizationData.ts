ca01e2429123915f51538e01cfce8a32dd01a67f
Lukasz Baranowski <lukaszb48@gmail.com>
2022-08-08 09:09:18 +0200

    fix

--- src/data-access/queries/useOrganizationData.ts
+++ src/data-access/queries/useOrganizationData.ts
@@ -2,9 +2,10 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 import { Organization } from 'mock-api/models/mirageTypes'
 import { QueryKeys } from 'dataAccess/QueryKeys'
+import { API } from 'dataAccess/API'
 
 export const getOrganization = async () => {
-  const response = await axios.get('../mockedOrg.json')
+  const response = await axios.get(API.GET.getOrganization)
   return response.data.organizations[0]
 }
 
--- src/screens/stats/StatsAndRequests.tsx
+++ src/screens/stats/StatsAndRequests.tsx
@@ -19,8 +19,6 @@ type NavigationType = RequestsNavigatorType<'STATS_AND_REQUESTS'> & typeof Drawe
 export const StatsAndRequests = () => {
   const { isLoading, data: stats } = useFetchUserStats()
 
-  console.log('xx', JSON.stringify(stats, null, 2))
-
   const { t } = useTranslation('stats')
   const { user } = useUserContext()
   const navigation = useNavigation<NavigationType>()
