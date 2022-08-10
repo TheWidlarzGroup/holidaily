3f89a8a79c1bdd24d20bee6f8955a40648bf015b
Lukasz Baranowski <lukaszb48@gmail.com>
2022-08-10 11:50:17 +0200

    add first test

--- src/utils/tests/editProfile/userData.test.tsx
+++ src/utils/tests/editProfile/userData.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react-native'
+import { fireEvent, render, screen } from '@testing-library/react-native'
 import { MockedNavigator } from 'utils/tests/MockedNavigator'
 import { MockGlobalProviders } from 'utils/tests/MockGlobalProviders'
 import { EditProfile } from 'screens/editProfile/EditProfile'
@@ -8,14 +8,23 @@ import { useTranslation } from 'react-i18next'
 describe('test user edit inputs', () => {
   const { t } = useTranslation('userProfile')
   const navigationSpy = jest.fn()
-  const renderEditProfile = render(
-    <MockedNavigator stateChangeSpy={navigationSpy} component={EditProfile} />,
-    { wrapper: MockGlobalProviders }
-  )
+  render(<MockedNavigator stateChangeSpy={navigationSpy} component={EditProfile} />, {
+    wrapper: MockGlobalProviders,
+  })
 
   // it('should show confirm modal when user data is changed', () => {})
 
   it('should show confirm modal when user data is changed', () => {
-    const saveChangesBtn = renderEditProfile.getByText(t('userFirstName'))
+    screen.getByText(t('userFirstName'))
+    const firstNameInput = screen.getByPlaceholderText(t('firstNamePlaceholder'))
+
+    fireEvent.changeText(firstNameInput, 'Jan')
+    const modalSaveButton = screen.getByText(t('saveChanges'))
+    fireEvent.press(modalSaveButton)
+    fireEvent.changeText(firstNameInput, 'Luk')
+    const modalDiscardButton = screen.getByText(t('discardChanges'))
+    fireEvent.press(modalDiscardButton)
+    // expect(firstNameInput.value).toBe('Luk')
+    expect(firstNameInput).toHaveLength(3)
   })
 })
