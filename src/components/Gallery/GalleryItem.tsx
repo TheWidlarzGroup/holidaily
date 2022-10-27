0cc673b21008c33f5138fe9b01429003fa7a38af
mateki0 <mateki0@interia.pl>
2022-10-27 15:51:02 +0200

    add skeleton loader for images in feed

--- src/components/Gallery/GalleryItem.tsx
+++ src/components/Gallery/GalleryItem.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FastImage from 'react-native-fast-image'
 import { BaseOpacity } from 'utils/theme'
 import { ViewStyle } from 'react-native'
 import { AttachmentDataType } from 'mockApi/models/miragePostTypes'
+import { SkeletonLoader } from 'components/SkeletonLoader'
+import { sleep } from 'utils/sleep'
 
 type GalleryItemProps = AttachmentDataType & {
   width: number
@@ -10,8 +12,24 @@ type GalleryItemProps = AttachmentDataType & {
   style: ViewStyle
 }
 
-export const GalleryItem = ({ uri, width, onPress, style }: GalleryItemProps) => (
-  <BaseOpacity onPress={onPress} activeOpacity={1} style={style}>
-    <FastImage style={{ aspectRatio: 4 / 5, width }} source={{ uri }} resizeMode="cover" />
-  </BaseOpacity>
-)
+export const GalleryItem = ({ uri, width, onPress, style }: GalleryItemProps) => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const onImageLoadEnd = async () => {
+    await sleep(500)
+    setIsLoading(false)
+  }
+
+  return (
+    <BaseOpacity onPress={onPress} activeOpacity={1} style={style}>
+      <SkeletonLoader width={width} isLoading={isLoading}>
+        <FastImage
+          style={{ aspectRatio: 4 / 5, width }}
+          source={{ uri }}
+          resizeMode="cover"
+          onLoadEnd={onImageLoadEnd}
+        />
+      </SkeletonLoader>
+    </BaseOpacity>
+  )
+}
---
+++ src/components/SkeletonLoader.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react'
+import { StyleSheet } from 'react-native'
+import MaskedView from '@react-native-masked-view/masked-view'
+import {
+  useSharedValue,
+  withRepeat,
+  withTiming,
+  useAnimatedStyle,
+  interpolate,
+} from 'react-native-reanimated'
+import { Box, theme } from 'utils/theme'
+import LinearGradient from 'react-native-linear-gradient'
+import { AnimatedBox } from './AnimatedBox'
+
+type Props = {
+  children: React.ReactElement
+  width: number
+  isLoading: boolean
+}
+
+export const SkeletonLoader = (props: Props) => {
+  const loaderValue = useSharedValue(0)
+
+  const IMAGE_HEIGHT = (props.width * 4) / 3
+
+  useEffect(() => {
+    loaderValue.value = withRepeat(withTiming(1, { duration: 1500 }), Infinity)
+
+    return () => {
+      loaderValue.value = 0
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: interpolate(loaderValue.value, [0, 1], [-props.width, props.width]),
+      },
+    ],
+  }))
+
+  if (!props.isLoading) {
+    return props.children
+  }
+
+  return (
+    <MaskedView maskElement={props.children} style={{ height: IMAGE_HEIGHT, width: props.width }}>
+      <AnimatedBox style={[StyleSheet.absoluteFill, animatedStyle]} width={props.width}>
+        <MaskedView
+          style={StyleSheet.absoluteFill}
+          maskElement={
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={StyleSheet.absoluteFill}
+              colors={['transparent', theme.colors.grey, 'transparent']}
+            />
+          }>
+          <Box style={[StyleSheet.absoluteFill, { backgroundColor: theme.colors.grey }]} />
+        </MaskedView>
+      </AnimatedBox>
+    </MaskedView>
+  )
+}
